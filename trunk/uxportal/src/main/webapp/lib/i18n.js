9b7eb78401f56b0d6124a5555cd085c0a784f899
nicolaas.matthijs@caret.cam.ac.uk <nicolaas.matthijs@caret.cam.ac.uk@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2009-01-15 15:21:22 +0000

    SAK-15389

--- trunk/uxportal/src/main/webapp/lib/i18n.js
+++ trunk/uxportal/src/main/webapp/lib/i18n.js
@@ -9,39 +9,28 @@ sdata.events.Listener.onLoad(function() {
 	} else {
 		sdataMeUrl = "/sdata/me?sid=" + Math.random();
 	}
+	
 	sdata.Ajax.request({
-		url : sdataMeUrl,
+		url : "/direct/batch?_refs=" + sdataMeUrl + "," + "/dev/bundle/default.json",
 		httpMethod : "GET",
 		onSuccess : function(data) {
-			var mejson = eval('(' + data + ')');
-			sdata.Ajax.request({
-				url : "bundle/default.json",
-				httpMethod : "GET",
-				onSuccess : function(data) {
-					var defaultjson = eval('(' + data + ')');
-					if (mejson.items.userLocale) {
-						sdata.Ajax.request({
-							url: "bundle/" + mejson.items.userLocale.language + "_" + mejson.items.userLocale.country + ".json",
-							httpMethod: "GET",
-							onSuccess: function(data){
-								var localjson = eval('(' + data + ')');
-								var newstring = sdata.i18n.processhtml(tostring, localjson, defaultjson);
-								document.body.innerHTML = newstring;
-								document.body.style.display = "block";
-								sdata.readyToLoad = true;
-								sdata.performLoad();
-								sdata.widgets.WidgetLoader.insertWidgets(null);
-							},
-							onFail: function(status){
-								var newstring = sdata.i18n.processhtml(tostring, null, defaultjson);
-								document.body.innerHTML = newstring;
-								document.body.style.display = "block";
-								sdata.readyToLoad = true;
-								sdata.performLoad();
-								sdata.widgets.WidgetLoader.insertWidgets(null);
-							}
-						});
-					} else {
+			var data = eval('(' + data + ')');
+			var mejson = data["ref0"].data;
+			var defaultjson = data["ref1"].data;
+			if (mejson.items.userLocale) {
+				sdata.Ajax.request({
+					url: "bundle/" + mejson.items.userLocale.language + "_" + mejson.items.userLocale.country + ".json",
+					httpMethod: "GET",
+					onSuccess: function(data){
+						var localjson = eval('(' + data + ')');
+						var newstring = sdata.i18n.processhtml(tostring, localjson, defaultjson);
+						document.body.innerHTML = newstring;
+						document.body.style.display = "block";
+						sdata.readyToLoad = true;
+						sdata.performLoad();
+						sdata.widgets.WidgetLoader.insertWidgets(null);
+					},
+					onFail: function(status){
 						var newstring = sdata.i18n.processhtml(tostring, null, defaultjson);
 						document.body.innerHTML = newstring;
 						document.body.style.display = "block";
@@ -49,14 +38,15 @@ sdata.events.Listener.onLoad(function() {
 						sdata.performLoad();
 						sdata.widgets.WidgetLoader.insertWidgets(null);
 					}
-				},
-				onFail : function(status) {
-					document.body.style.display = "block";
-					sdata.readyToLoad = true;
-					sdata.performLoad();
-					sdata.widgets.WidgetLoader.insertWidgets(null); 
-				}
-			});	
+				});
+			} else {
+				var newstring = sdata.i18n.processhtml(tostring, null, defaultjson);
+				document.body.innerHTML = newstring;
+				document.body.style.display = "block";
+				sdata.readyToLoad = true;
+				sdata.performLoad();
+				sdata.widgets.WidgetLoader.insertWidgets(null);
+			}
 		},
 		onFail : function(status) {
 			document.body.style.display = "block";
