d115569a6b7503548d10722d7f01b16fbf85dd73
nicolaas.matthijs@caret.cam.ac.uk <nicolaas.matthijs@caret.cam.ac.uk@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2009-02-11 15:59:03 +0000

    SAK-15667

--- branches/K2/trunk/uxwidgets/src/main/webapp/myfriends/javascript/myfriends.js
+++ branches/K2/trunk/uxwidgets/src/main/webapp/myfriends/javascript/myfriends.js
@@ -12,7 +12,7 @@ sakai.myfriends = function(tuid,placement,showSettings){
 	
 	sdata.Ajax.request({
 		httpMethod: "GET",
-		url: "/sdata/connection?show=accepted&page=1&count=100&sid=" + Math.random(),
+		url: "/rest/friend/status?sid=" + Math.random(),
 		onSuccess: function(data){
 			friends = eval('(' + data + ')');
 			getOnline();
@@ -24,16 +24,16 @@ sakai.myfriends = function(tuid,placement,showSettings){
 	
 	var getOnline = function(){
 		
-		sdata.Ajax.request({
-			url: "/sdata/presence/?sid=" + Math.random(),
-			httpMethod: "GET",
-			onSuccess: function(data){
-				online = eval('(' + data + ')');
+		//sdata.Ajax.request({
+		//	url: "/sdata/presence/?sid=" + Math.random(),
+		//	httpMethod: "GET",
+		//	onSuccess: function(data){
+		//		online = eval('(' + data + ')');
 				doProcessing();
-			},
-			onFail: function(status){
-			}
-		});
+		//	},
+		//	onFail: function(status){
+		//	}
+		//});
 		
 	}
 	
@@ -44,68 +44,70 @@ sakai.myfriends = function(tuid,placement,showSettings){
 		var total = 0;
 		pOnline.showMore = false;
 		
-		for (var i = 0; i < online.items.length; i++){
-			if (total < 3) {
-				var item = online.items[i];
-				if (item.firstName && item.lastName) {
-					item.name = item.firstName + " " + item.lastName;
-				}
-				else {
-					item.name = item.userid;
-				}
-				if (item.picture) {
-					var pict = eval('(' + item.picture + ')');
-					item.photo = "/sdata/f/public/" + item.userid + "/" + pict.name;
-				}
-				item.online = true;
-				if (item.basic){
-					var basic = eval('(' + item.basic + ')');
-					if (basic.status){
-						item.status = basic.status;
-					}
-				}
-				pOnline.items[pOnline.items.length] = item;
-				total++;
-			} else {
-				pOnline.showMore = true;
-			}
-		}
+		//for (var i = 0; i < online.items.length; i++){
+		//	if (total < 3) {
+		//		var item = online.items[i];
+		//		if (item.firstName && item.lastName) {
+		//			item.name = item.firstName + " " + item.lastName;
+		//		}
+		//		else {
+		//			item.name = item.userid;
+		//		}
+		//		if (item.picture) {
+		//			var pict = eval('(' + item.picture + ')');
+		//			item.photo = "/sdata/f/public/" + item.userid + "/" + pict.name;
+		//		}
+		//		item.online = true;
+		//		if (item.basic){
+		//			var basic = eval('(' + item.basic + ')');
+		//			if (basic.status){
+		//				item.status = basic.status;
+		//			}
+		//		}
+		//		pOnline.items[pOnline.items.length] = item;
+		//		total++;
+		//	} else {
+		//		pOnline.showMore = true;
+		//	}
+		//}
 		
-		for (var i = 0; i < friends.items.length; i++) {
-			var isOnline = false;
-			
-			for (var ii = 0; ii < online.items.length; ii++){
-				if (online.items[ii].userid == friends.items[i].userid){
-					isOnline = true;
-				}
-			}
-			
-			if (!isOnline && total < 3) {
-				var item = friends.items[i];
-				if (item.firstName && item.lastName) {
-					item.name = item.firstName + " " + item.lastName;
-				}
-				else {
-					item.name = item.userid;
-				}
-				if (item.picture) {
-					var pict = eval('(' + item.picture + ')');
-					item.photo = "/sdata/f/public/" + item.userid + "/" + pict.name;
-				}
-				item.online = false;
-				if (item.basic){
-					var basic = eval('(' + item.basic + ')');
-					if (basic.status){
-						item.status = basic.status;
+		if (friends.status.friends) {
+			for (var i = 0; i < friends.status.friends; i++) {
+				var isOnline = false;
+				
+				for (var ii = 0; ii < online.items.length; ii++) {
+					if (online.items[ii].userid == friends.items[i].userid) {
+						isOnline = true;
 					}
 				}
-				pOnline.items[pOnline.items.length] = item;
-				total++;
-			}
-			else 
-				if (total >= 3 && !isOnline) {
-					pOnline.showMore = true;
+				
+				if (!isOnline && total < 3) {
+					var item = friends.status.friends[i];
+					//if (item.firstName && item.lastName) {
+					//	item.name = item.firstName + " " + item.lastName;
+					//}
+					//else {
+					//	item.name = item.userid;
+					//}
+					//if (item.picture) {
+					//	var pict = eval('(' + item.picture + ')');
+					//	item.photo = "/sdata/f/public/" + item.userid + "/" + pict.name;
+					//}
+					//item.online = false;
+					//if (item.basic) {
+					//	var basic = eval('(' + item.basic + ')');
+					//	if (basic.status) {
+					//		item.status = basic.status;
+					//	}
+					//}
+					//pOnline.items[pOnline.items.length] = item;
+					//total++;
 				}
+				else 
+					if (total >= 3 && !isOnline) {
+						pOnline.showMore = true;
+					}
+			}
 		}
 		
 		$("#friends_list").html(sdata.html.Template.render("friends_list_template", pOnline));
