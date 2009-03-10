22551b359cd4e1493a0b5a62812c1c7e2fbaddc8
oszkar@caret.cam.ac.uk <oszkar@caret.cam.ac.uk@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2009-03-10 18:21:21 +0000

    Added 2nd variant of search page SAK-15773

---
+++ branches/K2/trunk/uxportal/src/main/webapp/redesign/jscripts/search_b.js
@@ -0,0 +1,24 @@
+//Search page JS
+
+$(document).ready(function() { 
+
+// Fix small arrow horizontal position
+$('.explore_nav_selected_arrow').css('right', $('.explore_nav_selected').width() / 2 + 10);
+
+// Round cornners for elements with '.rounded_corners' class
+$('.rounded_corners').corners("2px");
+
+// IE Fixes
+if (($.browser.msie) && ($.browser.version < 8)) {
+	// Tab fix
+	$('.fl-tabs li a').css('bottom','-1px');
+	$('.fl-tabs .fl-activeTab a').css('bottom','-1px');
+	
+	//Search button fix
+	$('.search_button').css('top','4px');
+	
+	// Small Arrow Fix
+	$('.explore_nav_selected_arrow').css('bottom','-11px');
+}
+
+});  
