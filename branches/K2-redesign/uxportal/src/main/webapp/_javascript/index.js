047563f9da88cd1596cf2edb96022e427c63bd64
nicolaas.matthijs@caret.cam.ac.uk <nicolaas.matthijs@caret.cam.ac.uk@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2009-04-24 16:04:51 +0000

    SAK-16161

--- branches/K2-redesign/uxportal/src/main/webapp/_javascript/index.js
+++ branches/K2-redesign/uxportal/src/main/webapp/_javascript/index.js
@@ -1,82 +1,122 @@
+/*
+ * Licensed to the Sakai Foundation (SF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The SF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+var Querystring = Querystring || function(){ throw "Querystring not available"; };
+var Config = Config || function(){ throw "Config file not available"; };
+var $ = $ || function(){ throw "JQuery not available"; };
+var sdata = sdata || function(){ throw "SData.js not available"; };
+var json_parse = json_parse || function(){ throw "SData.js not available"; };
+var set_cookie = set_cookie || function(){ throw "JQuery cookie is not available"; };
+
 var sakai = sakai || {};
+
 sakai.index = function(){
 
-	var isLoggingIn = false;
-	var redirecturl = "/dev/redesign/my_sakai.html";
-
-	var doInit = function(){
-
-		var qs = new Querystring();
-		var red = qs.get("url", "false");
-		if (red != "false"){
-			redirecturl = sdata.util.URL.decode(red);
-		}
-		$("input").bind("keydown", function(e){
-			var code = e.keyCode;
-			switch (code)
-			{
-			case 13:
-				performLogIn();
-				break			
-			}
-		});
-		$("#loginbutton").bind("click", function(){
-			performLogIn();
-		});
+	
+	/////////////////////////////
+	// Configuration variables //
+	/////////////////////////////
+	
+	var redirecturl = Config.URL.MY_DASHBOARD;
+	var loginButton = "#loginbutton";
+	var usernameField = "#username";
+	var passwordField = "#password";
+	var failMessage = "#failed";
+	var loadingMessage = "#loginloader";
+	var registerLink = "#register_here";
+	
+	
+	/////////////////////
+	// Login functions //
+	/////////////////////
+	
+	/*
+	 * This will determine whether there is a valid session. If there is, we'll
+	 * redirect to the URL requested or the personal dashboard if nothing has been provided. 
+	 */
+	var decideLoggedIn = function(data){
+		var mejson = (data === undefined ? sdata.me : json_parse(data));
+		if (mejson.preferences.uuid !== "anon" && mejson.preferences.uuid !== null){
+			document.location = redirecturl;
+		} else {
+			$(loadingMessage).hide();
+			$(loginButton).show();
+			$(registerLink).show();
+			data !== undefined ? function(){
+				$(failMessage).show();
+			} : function(){};
+		}	
+	};
+	
+	/*
+	 * This will be executed after the post to the login service has finished. 
+	 * We send a new request to the Me service, explicity disabling cache by
+	 * adding a random number behind the URL, becasue otherwise it would get
+	 * the cached version of the me object which would still say I'm not logged
+	 * in.
+	 */
+	var checkLogInSuccess = function(response, exists){
 		
 		sdata.Ajax.request({
-			url : "/rest/me?sid=" + Math.random(),
+			url : Config.URL.ME_SERVICE + "?sid=" + Math.random(),
 			httpMethod : "GET",
 			onSuccess : function(data) {
-				sakai.index.decideLoggedIn(data,true);
+				decideLoggedIn(data,true);
 			},
 			onFail : function(data){
-				sakai.index.decideLoggedIn(data,false);
+				throw "Me service has failed";
 			}
 		});
+	
+	};
+	
+	var performLogIn = function(){
 
-	}
+		var username = $(usernameField).val();
+		var password = $(passwordField).val();
 
-	sakai.index.decideLoggedIn = function(response, exists){
-		if (exists){
-			var mejson = eval('(' + response + ')');
-			if (mejson.preferences.uuid != "anon" && mejson.preferences.uuid != null){
-				document.location = redirecturl;
-			} else {
-				$("#username").focus();
-			}
-		} else {
-			// An error has occured
-		}	
+		/*
+		 * We check whether the username and password are empty (including trimming). If one of them is,
+		 * we ignore this request to login and wait unti something has been filled out in both fields.
+		 */
+		if (!(!username || !password || username.replace(/ /g,"") === "" || password.replace(/ /g,"") === "")){
 
-	}
+			$(failMessage).hide();
+			$(loadingMessage).show();
+			$(loginButton).hide();
+			$(registerLink).hide();
 
-	performLogIn = function(){
-
-		var username = $('#username').attr("value");
-		var password = $('#password').attr("value");
-
-		if (!username || !password || username.replace(/ /g,"") == "" || password.replace(/ /g,"") == ""){
-
-		} else {
-
-			isLoggingIn = true;
-			$("#failed").hide();
-			$("#loginloader").show();
-			$("#loginbutton").hide();
-			$("#register_here").hide();
-
-			var url= "/rest/login";
-			var requestbody = {"l":1, "a":"FORM", "u" : $('#username').attr("value"), "p" : $('#password').attr("value")};
+			/*
+			 * u : the username entered in the username textfield
+			 * p : the password entered in the password textfield
+			 * l : set to 1 because we want to perform a login action
+			 * a : the login method set to FORM because we are doing a FORM-based login
+			 */
+			var requestbody = {"l":1, "a":"FORM", "u" : username, "p" : password};
 	
 			sdata.Ajax.request({
-				url :url,
+				url :Config.URL.LOGIN_SERVICE,
 				httpMethod : "POST",
 				onSuccess : function(data) {
-					sakai.index.checkLogInSuccess(data,true);
+					checkLogInSuccess(data,true);
 				},
 				onFail : function(status) {
-					sakai.index.checkLogInSuccess(status,false);
+					checkLogInSuccess(status,false);
 				},
 				postData : requestbody,
 				contentType : "application/x-www-form-urlencoded"
@@ -84,65 +124,61 @@ sakai.index = function(){
 
 		}
 
-	}
-
-	sakai.index.retryLogin = function(){
-
-		var url= "/rest/login";
-		var requestbody = {"l":1, "a":"FORM", "u" : $('#username').attr("value"), "p" : $('#password').attr("value")};
-		
-		sdata.Ajax.request({
-			url :url,
-			httpMethod : "POST",
-			onSuccess : function(data) {
-				sakai.index.checkLogInSuccess(data,true);
-			},
-			onFail : function(status) {
-				sakai.index.checkLogInSuccess(status,false);
-			},
-			postData : requestbody,
-			contentType : "application/x-www-form-urlencoded"
-		});
-	}
-
-	sakai.index.checkLogInSuccess = function(response, exists){
-		
-		sdata.Ajax.request({
-			url : "/rest/me?sid=" + Math.random(),
-			httpMethod : "GET",
-			onSuccess : function(data) {
-				sakai.index.decideLoggedIn2(data,true);
-			},
-			onFail : function(data){
-				sakai.index.decideLoggedIn2(data,false);
-			}
-		});
+	};
 	
-		if (isLoggingIn){
-			setTimeout("sakai.index.retryLogin()",1000);
-		}
-	}
-
-	sakai.index.decideLoggedIn2 = function(response, exists){
-
-		if (exists){
-			var mejson = eval('(' + response + ')');
-			if (mejson.preferences.uuid != "anon" && mejson.preferences.uuid != null){
-				document.location = redirecturl;
-			} else {
-				$("#failed").show();
-				$("#loginloader").hide();
-				$("#loginbutton").show();
-				$("#register_here").show();
-				isLoggingIn = false;
-			}
-		} else {
-			// An error has occured
-		}	
-
-	}
 	
+	//////////
+	// Chat //
+	//////////
+	
+	/*
+	 * The chat bar keeps a cookie during your session that remembers which
+	 * chat windows were open or active, so that state can be restored across
+	 * all of the pages. When we have reached this page, it means that our
+	 * session has ended and we can remove this cookie.
+	 */
 	set_cookie('sakai_chat','', null, null, null, "/", null, null );
+	
+	
+	////////////////////
+	// Event Handlers //
+	////////////////////
+	
+	/*
+	 * Check on every keypress whether the enter key has been pressed or not. If so,
+	 * we initiate the login function
+	 */
+	$("input").bind("keydown", function(e){
+		e.keyCode === 13 ? performLogIn() : function(){}();
+	});
+	
+	$(loginButton).bind("click", performLogIn);
+	
+
+	/////////////////////////////
+	// Initialisation function //
+	/////////////////////////////
+
+	var doInit = function(){
+
+		/*
+		 * If you were logged in to the system, and the session has expired,
+		 * it will redirect you to the login page with the URL of the page you
+		 * were on encoded into a "url" querystring variable. We check whether
+		 * the "url" querystring parameter is there, and if it is, we'll decode
+		 * that URL and redirect to that location on successful login. If it isn't
+		 * set, we'll redirect to the personal dashboard 
+		 */
+		var qs = new Querystring();
+		var red = qs.get("url", false);
+		if (red !== false){
+			redirecturl = sdata.util.URL.decode(red);
+		}
+		
+		$(usernameField).focus();
+		decideLoggedIn();
+
+	};
 
 	doInit();
 
