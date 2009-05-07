d0fc8a4b417f21c8c3467765fd5571506719d341
nicolaas.matthijs@caret.cam.ac.uk <nicolaas.matthijs@caret.cam.ac.uk@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2009-05-07 15:35:01 +0000

    Removed agnostic code

--- branches/K2-Sling/uxbackend/bootstrap/drop.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE group_membership
-DROP TABLE site
-DROP TABLE user_eid
-DROP TABLE friends
-DROP TABLE subject_permission
-DROP TABLE aclIndex
--- branches/K2-Sling/uxbackend/bootstrap/start.bat
+++
@@ -1,7 +0,0 @@
-echo on
-
-set CATALINA_HOME=%cd%\target\runtime
-
-set SAKAI_KERNEL_PROPERTIES=%cd%\localkernel.properties
-
-"%cd%\target\runtime\bin\catalina.bat" %1
