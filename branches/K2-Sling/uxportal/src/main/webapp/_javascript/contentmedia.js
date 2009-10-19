da4e347f1d470a57564adfb4461b2a5b3d607b86
oszkar@caret.cam.ac.uk <oszkar@caret.cam.ac.uk@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2009-10-19 09:31:21 +0000

    Content / media bugfix
    NOJIRA

--- branches/K2-Sling/uxportal/src/main/webapp/_javascript/contentmedia.js
+++ branches/K2-Sling/uxportal/src/main/webapp/_javascript/contentmedia.js
@@ -1388,25 +1388,21 @@ sakai.contentmedia = function(){
 		// Check if the querystring contains the site id of a site
 		if(querystring.contains("siteid")){
 			var siteid = querystring.get("siteid");
-			setSiteFilter(Config.URL.SITE_CONFIGFOLDER.replace("__SITEID__", siteid), siteid);
-			doFileSearch(options);
 			
-			// Why do we need this?
-			//
-			//$.ajax({
-			//	url: Config.URL.SITE_CONFIGFOLDER.replace("__SITEID__", siteid) + ".json",
-			//	cache: false,
-			//	success: function(data){
-			//		var parsedData = $.evalJSON(data);
-			//		setSiteFilter(Config.URL.SITE_FILES_URL.replace("__SITEID__", siteid), parsedData.name);
-			//		
-			//		// Fetch the initial list of files
-			//		doFileSearch(options);
-			//	},
-			//	error: function(status){
-			//		//alert("An error has occured");
-			//	}
-			//});
+			$.ajax({
+				url: Config.URL.SITE_CONFIGFOLDER.replace("__SITEID__", siteid) + ".json",
+				cache: false,
+				success: function(data){
+					var parsedData = $.evalJSON(data);
+					setSiteFilter(Config.URL.SITE_FILES_URL.replace("__SITEID__", siteid), parsedData.name);
+					
+					// Fetch the initial list of files
+					doFileSearch(options);
+				},
+				error: function(status){
+					//alert("An error has occured");
+				}
+			});
 		}else{
 			// Fetch the initial list of files
 			doFileSearch(options);
