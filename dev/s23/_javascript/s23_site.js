81609e8e35121c5b4e48488de5da48524171ddea
christianv <vueringschristian@gmail.com>
2009-12-03 20:41:57 +0000

    s23 - changed the init to the registerForLoad function to not use the document.ready

--- dev/s23/_javascript/s23_site.js
+++ dev/s23/_javascript/s23_site.js
@@ -18,106 +18,103 @@
 
 /*global sakai, Config, $, QueryString */
 
-sakai = sakai || {};
 sakai.s23_site = sakai.s23_site || {};
 
-$(document).ready(function(){
-	sakai.s23_site = function(){
+sakai.s23_site = function(){
+
+
+	/////////////////////////////
+	// Configuration variables //
+	/////////////////////////////
+	
+	// Get the current query string
+	var qs = new Querystring();
+	
+	var s23Site = "#s23_site";
+	var s23SiteTitle = $(s23Site + "_title");
+	var s23SiteMenuContainer = $(s23Site + "_menu_container");
+	
+	// Templates
+	var s23SiteMenuContainerTemplate = "s23_site_menu_container_template";
 	
 	
-		/////////////////////////////
-		// Configuration variables //
-		/////////////////////////////
+	///////////////////////
+	// General functions //
+	///////////////////////
+	
+	/**
+	 * Parse the site info that is in a JSON format
+	 * to show it on the page
+	 * @param {Object} json A JSON object containing the site information
+	 */
+	var parseSakai2SiteInfo = function(json){
+	
+		// Check if the title and the pages attribute exist
+		if (json && json.site && json.site.title && json.site.pages) {
 		
-		// Get the current query string
-		var qs = new Querystring();
-		
-		var s23Site = "#s23_site";
-		var s23SiteTitle = $(s23Site + "_title");
-		var s23SiteMenuContainer = $(s23Site + "_menu_container");
-		
-		// Templates
-		var s23SiteMenuContainerTemplate = "s23_site_menu_container_template";
-		
-		
-		///////////////////////
-		// General functions //
-		///////////////////////
-		
-		/**
-		 * Parse the site info that is in a JSON format
-		 * to show it on the page
-		 * @param {Object} json A JSON object containing the site information
-		 */
-		var parseSakai2SiteInfo = function(json){
-		
-			// Check if the title and the pages attribute exist
-			if (json && json.site && json.site.title && json.site.pages) {
+			// Set the title of the page
+			s23SiteTitle.text(json.site.title);
 			
-				// Set the title of the page
-				s23SiteTitle.text(json.site.title);
-				
-				// Render the tools of the site
-				s23SiteMenuContainer.html($.Template.render(s23SiteMenuContainerTemplate, json));
-			}
-			else {
-				fluid.log("s23_site: An error occured when parsing the Sakai 2 site information");
-			}
-			
-		};
+			// Render the tools of the site
+			s23SiteMenuContainer.html($.Template.render(s23SiteMenuContainerTemplate, json));
+		}
+		else {
+			fluid.log("s23_site: An error occured when parsing the Sakai 2 site information");
+		}
 		
-		/**
-		 * Get the information of a Sakai2 site
-		 * @param {String} siteid The id of the Sakai2 site you want to
-		 */
-		var getSakai2SiteInfo = function(siteid){
-			$.ajax({
-				url: Config.URL.SAKAI2_TOOLS_SERVICE.replace(/__SITEID__/, siteid),
-				success: function(data){
-				
-					// Evaluate the data to JSON
-					var json = $.evalJSON(data);
-					
-					// Parse the Sakai2 info
-					parseSakai2SiteInfo(json);
-				},
-				error: function(status){
-					fluid.log("s23_site: It was not possible to get the information the Sakai 2 site with the id: " + siteid + " the error code is: " + status);
-				}
-			});
-		};
-		
-		
-		/////////////////////////////
-		// Initialisation function //
-		/////////////////////////////
-		
-		/**
-		 * Function that get executed when the DOM is completely loaded
-		 */
-		var init = function(){
-		
-			// Check if the query string contains the parameter id
-			if (qs.contains("id")) {
-			
-				// Get the value for the id parameter
-				var siteid = qs.get("id");
-				
-				// Send an ajax request to the user
-				getSakai2SiteInfo(siteid);
-				
-				/*
-				 $("#s23_site_iframe_container").html('<iframe id="s23_site_iframe" frameborder="0"></iframe>');
-				 $("#s23_site_iframe").attr("src", v1);
-				 */
-			}
-			else {
-			
-				// Log an error message for the user
-				fluid.log("s23site: This site needs to have an id parameter for a sakai2 site");
-			}
-		};
-		init();
 	};
-	sakai.s23_site();
-});
+	
+	/**
+	 * Get the information of a Sakai2 site
+	 * @param {String} siteid The id of the Sakai2 site you want to
+	 */
+	var getSakai2SiteInfo = function(siteid){
+		$.ajax({
+			url: Config.URL.SAKAI2_TOOLS_SERVICE.replace(/__SITEID__/, siteid),
+			success: function(data){
+			
+				// Evaluate the data to JSON
+				var json = $.evalJSON(data);
+				
+				// Parse the Sakai2 info
+				parseSakai2SiteInfo(json);
+			},
+			error: function(status){
+				fluid.log("s23_site: It was not possible to get the information the Sakai 2 site with the id: " + siteid + " the error code is: " + status);
+			}
+		});
+	};
+	
+	
+	/////////////////////////////
+	// Initialisation function //
+	/////////////////////////////
+	
+	/**
+	 * Function that get executed when the DOM is completely loaded
+	 */
+	var init = function(){
+	
+		// Check if the query string contains the parameter id
+		if (qs.contains("id")) {
+		
+			// Get the value for the id parameter
+			var siteid = qs.get("id");
+			
+			// Send an ajax request to the user
+			getSakai2SiteInfo(siteid);
+			
+			/*
+			 $("#s23_site_iframe_container").html('<iframe id="s23_site_iframe" frameborder="0"></iframe>');
+			 $("#s23_site_iframe").attr("src", v1);
+			 */
+		}
+		else {
+		
+			// Log an error message for the user
+			fluid.log("s23site: This site needs to have an id parameter for a sakai2 site");
+		}
+	};
+	init();
+};
+sdata.container.registerForLoad("sakai.s23_site");
