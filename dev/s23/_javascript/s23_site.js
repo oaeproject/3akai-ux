8ae0e33a5cc791005ded8d500a863d6745fdf917
christianv <vueringschristian@gmail.com>
2009-12-04 11:39:36 +0000

    s23 - show the tools of the first page if it has any tools

--- dev/s23/_javascript/s23_site.js
+++ dev/s23/_javascript/s23_site.js
@@ -26,15 +26,18 @@ sakai.s23_site = function(){
 	/////////////////////////////
 	// Configuration variables //
 	/////////////////////////////
-	
-	// Get the current query string
-	var qs = new Querystring();
-	
+
+	var qs = new Querystring(); // Get the current query string
+	var completeJSON;
+
+	// CSS selectors
 	var s23Site = "#s23_site";
 	var s23SiteTitle = $(s23Site + "_title");
+	var s23SiteIframeContainer = $(s23Site + "_iframe_container");
 	var s23SiteMenuContainer = $(s23Site + "_menu_container");
 	
 	// Templates
+	var s23SiteIframeContainerTemplate = "s23_site_iframe_container_template";
 	var s23SiteMenuContainerTemplate = "s23_site_menu_container_template";
 	
 	
@@ -43,20 +46,87 @@ sakai.s23_site = function(){
 	///////////////////////
 	
 	/**
-	 * Parse the site info that is in a JSON format
-	 * to show it on the page
-	 * @param {Object} json A JSON object containing the site information
+	 * Get all the information about a certain page
+	 * @param {String} pageid The id of the page you want information for
+	 * @return {Object} A page object containing the info about the page
 	 */
-	var parseSakai2SiteInfo = function(json){
-	
-		// Check if the title and the pages attribute exist
-		if (json && json.site && json.site.title && json.site.pages) {
+	var getPageInfo = function(pageid){
 		
-			// Set the title of the page
-			s23SiteTitle.text(json.site.title);
+		// Run over all the pages and return the page object that has the same
+		// id as the pageid argument
+		for (var i = 0, len = completeJSON.site.pages.length; i < len; i++) {
+			if (completeJSON.site.pages[i].id === pageid) {
+				return completeJSON.site.pages[i];
+			}
+		}
+
+		// Log a message if the page with the given pageid was not found
+		fluid.log("s23_site: the page with id'" + pageid + "' was not found in the json object");
+
+	};
+	
+	/**
+	 * Load the tools for a certain page
+	 * @param {Object} pageid The id of the page you want the tools loaded for
+	 */
+	var loadPageTools = function(pageid){
+		
+		// Get the page info for a certain page and store it in a variable
+		var page = getPageInfo(pageid);
+		
+		// Check if the page actually exists
+		if(page){
 			
 			// Render the tools of the site
-			s23SiteMenuContainer.html($.Template.render(s23SiteMenuContainerTemplate, json));
+			s23SiteIframeContainer.html($.Template.render(s23SiteIframeContainerTemplate, page));
+		}
+		
+	};
+	
+	/**
+	 * Create an xid for every tool that exists
+	 * We need this because the iframe looks at the parent frame with the xid
+	 * An id could be 	0175d73d-741f-4fb3-94dc-9f8c1d4925a9
+	 * An xid is		0175d73dx741fx4fb3x94dcx9f8c1d4925a9
+	 */
+	var createxids = function(){
+		
+		// Run over all the pages and then all the tools inside a page to modify the id
+		for (var i=0, len=completeJSON.site.pages.length; i<len; i++) {
+			if (completeJSON.site.pages[i].tools && completeJSON.site.pages[i].tools.length>0) {
+				for (var j = 0, toolslen = completeJSON.site.pages[i].tools.length; j < toolslen; j++) {
+					completeJSON.site.pages[i].tools[j].xid = completeJSON.site.pages[i].tools[j].id.replace(/-/, "x");
+				}
+			}
+		}
+		
+	};
+	
+	/**
+	 * Parse the site info that is in a JSON format
+	 * to show it on the page
+	 */
+	var parseSakai2SiteInfo = function(){
+	
+		// Check if the title and the pages attribute exist
+		if (completeJSON && completeJSON.site && completeJSON.site.title && completeJSON.site.pages) {
+		
+			// Set the title of the page
+			s23SiteTitle.text(completeJSON.site.title);
+			
+			// Render the menu of the workspace
+			s23SiteMenuContainer.html($.Template.render(s23SiteMenuContainerTemplate, completeJSON));
+			
+			// Create xid's
+			createxids();
+			
+			// Check if there are any tools in the first page, if so parse them
+			if(completeJSON.site.pages[0].tools && completeJSON.site.pages.tools[0].length > 0){
+				
+				// Load the tools for a specific page
+				loadPageTools(completeJSON.site.pages[0].id);
+				
+			}
 		}
 		else {
 			fluid.log("s23_site: An error occured when parsing the Sakai 2 site information");
@@ -69,15 +139,17 @@ sakai.s23_site = function(){
 	 * @param {String} siteid The id of the Sakai2 site you want to
 	 */
 	var getSakai2SiteInfo = function(siteid){
+		
+		// Send an Ajax request to the sakai2 tools service
 		$.ajax({
 			url: Config.URL.SAKAI2_TOOLS_SERVICE.replace(/__SITEID__/, siteid),
 			success: function(data){
 			
 				// Evaluate the data to JSON
-				var json = $.evalJSON(data);
+				completeJSON = $.evalJSON(data);
 				
 				// Parse the Sakai2 info
-				parseSakai2SiteInfo(json);
+				parseSakai2SiteInfo();
 			},
 			error: function(status){
 				fluid.log("s23_site: It was not possible to get the information the Sakai 2 site with the id: " + siteid + " the error code is: " + status);
@@ -103,11 +175,6 @@ sakai.s23_site = function(){
 			
 			// Send an ajax request to the user
 			getSakai2SiteInfo(siteid);
-			
-			/*
-			 $("#s23_site_iframe_container").html('<iframe id="s23_site_iframe" frameborder="0"></iframe>');
-			 $("#s23_site_iframe").attr("src", v1);
-			 */
 		}
 		else {
 		
