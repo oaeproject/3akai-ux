20c91289a7f138c085e7352849912073d0d76db2
christianv <vueringschristian@gmail.com>
2009-12-07 16:12:10 +0000

    s23 - Make the loading of pages faster by adding them in a div - camtools way

--- dev/s23/_javascript/s23_site.js
+++ dev/s23/_javascript/s23_site.js
@@ -32,29 +32,31 @@ sakai.s23_site = function(){
 
 	// CSS selectors
 	var s23Site = "#s23_site";
-	var s23SiteTitle = $(s23Site + "_title");
 	var s23SiteIframeContainer = $(s23Site + "_iframe_container");
-	var s23SiteMenuContainer = $(s23Site + "_menu_container");
 	var s23SiteMenuActive = "s23_site_menu_item_active";
+	var s23SiteMenuContainer = $(s23Site + "_menu_container");
 	var s23SiteMenuItemTag = "s23_site_menu_item_";
 	var s23SiteMenuItems = s23Site + "_menu_container ul li a";
-	
+	var s23SitePageContainerClass = ".s23_site_page_container";
+	var s23SitePageContainerTag = "s23_site_page_container_";
+	var s23SiteTitle = $(s23Site + "_title");
+
 	// Templates
 	var s23SiteIframeContainerTemplate = "s23_site_iframe_container_template";
 	var s23SiteMenuContainerTemplate = "s23_site_menu_container_template";
-	
-	
+
+
 	///////////////////////
 	// General functions //
 	///////////////////////
-	
+
 	/**
 	 * Get all the information about a certain page
 	 * @param {String} pageid The id of the page you want information for
 	 * @return {Object} A page object containing the info about the page
 	 */
 	var getPageInfo = function(pageid){
-		
+
 		// Run over all the pages and return the page object that has the same
 		// id as the pageid argument
 		for (var i = 0, len = completeJSON.site.pages.length; i < len; i++) {
@@ -65,25 +67,37 @@ sakai.s23_site = function(){
 
 		// Log a message if the page with the given pageid was not found
 		fluid.log("s23_site: the page with id'" + pageid + "' was not found in the json object");
-
 	};
-	
+
 	/**
 	 * Load the tools for a certain page
 	 * @param {Object} pageid The id of the page you want the tools loaded for
 	 */
 	var loadPageTools = function(pageid){
-		
+
 		// Get the page info for a certain page and store it in a variable
 		var page = getPageInfo(pageid);
-		
+
 		// Check if the page actually exists
 		if(page){
+
+			// Get the complete id for a page container
+			var completexid = s23SitePageContainerTag + page.xid;
 			
-			// Render the tools of the site
-			s23SiteIframeContainer.append($.Template.render(s23SiteIframeContainerTemplate, page));
+			// Check if the page was already loaded before
+			if($("#"+completexid).length > 0){
+				
+				// Show the page container
+				$("#"+completexid).show();
+			}else{
+				
+				// Hide the content & tools from the other pages
+				$(s23SitePageContainerClass, s23SiteIframeContainer).hide();
+				
+				// Render the tools of the site and add them to the page container
+				s23SiteIframeContainer.append($.Template.render(s23SiteIframeContainerTemplate, page));
+			}
 		}
-		
 	};
 
 	/**
@@ -91,12 +105,11 @@ sakai.s23_site = function(){
 	 * @param {String} id The id you want to transform to an xid
 	 */
 	var toxid = function(id){
-		
+
 		// Filter out all the ! and - characters and replace them by x
 		return id.replace(/-|\!/g, "x");
-		
 	};
-	
+
 	/**
 	 * Create an xid for every page & tool that exists
 	 * We need this because the iframe looks at the parent frame with the xid (tool)
@@ -105,7 +118,7 @@ sakai.s23_site = function(){
 	 * An xid is		0175d73dx741fx4fb3x94dcx9f8c1d4925a9
 	 */
 	var createxids = function(){
-		
+
 		// Run over all the pages and then all the tools inside a page to modify the xid
 		for (var i=0, len=completeJSON.site.pages.length; i<len; i++) {
 			completeJSON.site.pages[i].xid = toxid(completeJSON.site.pages[i].id);
@@ -116,7 +129,7 @@ sakai.s23_site = function(){
 			}
 		}
 	};
-	
+
 	/**
 	 * Add binding to several items
 	 */
@@ -126,53 +139,51 @@ sakai.s23_site = function(){
 		 * Bind a click handler to every site menu item
 		 */
 		$(s23SiteMenuItems).click(function(ev){
-			
+
 			// Prevent going to the actual page
 			ev.preventDefault();
-			
+
 			// Get the id of the tool you clicked on
 			var id = this.id.replace(s23SiteMenuItemTag, "");
-			
+
 			// Remove the active class from the previous selected item
 			$(s23SiteMenuItems).removeClass(s23SiteMenuActive);
-			
+
 			// Set the active class to the item you just clicked on
 			$(this).addClass(s23SiteMenuActive);
-			
+
 			// Load the tools for a specific page
 			loadPageTools(id);
 		});
 	};
-	
+
 	/**
 	 * Parse the site info that is in a JSON format
 	 * to show it on the page
 	 */
 	var parseSakai2SiteInfo = function(){
-	
+
 		// Check if the title and the pages attribute exist
 		if (completeJSON && completeJSON.site && completeJSON.site.title && completeJSON.site.pages) {
-		
+
 			// Set the title of the page
 			s23SiteTitle.text(completeJSON.site.title);
 
 			// Render the menu of the workspace
 			s23SiteMenuContainer.html($.Template.render(s23SiteMenuContainerTemplate, completeJSON));
-			
+
 			// Create xid's
 			createxids();
-			
+
 			// Add binding to the tools links
 			addBinding();
-			
+
 			// Pretend like you clicked on the first page
 			$(s23SiteMenuItems + ":first").trigger("click");
-			
 		}
 		else {
 			fluid.log("s23_site: An error occured when parsing the Sakai 2 site information");
 		}
-		
 	};
 	
 	/**
@@ -197,28 +208,28 @@ sakai.s23_site = function(){
 			}
 		});
 	};
-	
-	
+
+
 	/////////////////////////////
 	// Initialisation function //
 	/////////////////////////////
-	
+
 	/**
 	 * Function that get executed when the DOM is completely loaded
 	 */
 	var init = function(){
-	
+
 		// Check if the query string contains the parameter id
 		if (qs.contains("id")) {
-		
+
 			// Get the value for the id parameter
 			var siteid = qs.get("id");
-			
+
 			// Send an ajax request to the user
 			getSakai2SiteInfo(siteid);
 		}
 		else {
-		
+
 			// Log an error message for the user
 			fluid.log("s23site: This site needs to have an id parameter for a sakai2 site");
 		}
