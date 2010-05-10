b23023a57470b25580b61bc304a196e73a3ee876
Christian Vuerings <vueringschristian@gmail.com>
2010-05-10 20:32:21 +0100

    SAKIII-371 - Added initial files for the entity widget

---
+++ devwidgets/entity/javascript/entity.js
@@ -0,0 +1,40 @@
+/*
+ * Licensed to the Sakai Foundation (SF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The SF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+/*global $, sdata, Config */
+
+var sakai = sakai || {};
+
+/**
+ * Initialize the entity widget - this widget provides person / space and content information
+ * http://jira.sakaiproject.org/browse/SAKIII-371
+ * @param {String} tuid Unique id of the widget
+ * @param {Boolean} showSettings Show the settings of the widget or not
+ */
+sakai.entity = function(tuid, showSettings){
+
+
+    /**
+     * Public function that can be called anywhere this widget is loaded.
+     * @param {String} mode The mode of how you would like to load the entity widget ["profile","space","content"]
+     */
+    sakai.entity.init = function(mode){
+        alert(mode);
+    };
+
+};
