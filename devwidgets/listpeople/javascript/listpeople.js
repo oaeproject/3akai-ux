0e97f42643044bff0ae63973436728f57a77d3bc
oszkarnagy <oszkar@caret.cam.ac.uk>
2010-05-11 18:56:53 +0100

    SAKIII-373 First go at loading people dynamically

--- devwidgets/listpeople/javascript/listpeople.js
+++ devwidgets/listpeople/javascript/listpeople.js
@@ -32,7 +32,8 @@ sakai.api.UI.listPeople = {};
 
 /**
  * Initialize the listpeople widget
- * This is the widget loader's default callback
+ * This is the widget loader's default callback, executing when the widget
+ * is loaded on a page
  * @param tuid {String} Unique id of the widget
  * @param showSettings {Boolean} Show the settings of the widget or not
  */
@@ -41,10 +42,9 @@ sakai.listpeople = function(tuid, showSettings){
     // Config defaults
     var default_config = {
 
-        "maxResults": 20
-
-    }
-
+        "items": 25,
+        "currentPage": 0
+    };
 
     // Create a config object for this instance
     sakai.config.widgets.listpeople = sakai.config.widgets.listpeople || {};
@@ -52,6 +52,11 @@ sakai.listpeople = function(tuid, showSettings){
 
     // Reset to defaults
     sakai.api.UI.listPeople.reset(tuid);
+
+    // Send out an event that says the widget is ready to
+    // accept a search query to process and display. This event can be picked up
+    // in a page JS code
+    $(window).trigger("listpeople_ready", [tuid]);
 };
 
 /**
@@ -62,23 +67,114 @@ sakai.listpeople = function(tuid, showSettings){
  */
 sakai.api.UI.listPeople.reset = function(tuid) {
 
+    $("#" + tuid + " .listpeople_content").html("");
+    $("#" + tuid + " .listpeople_count").html("");
+
 };
 
 /**
  * Render
- * Renders the people lister with a specified set of data. Usually this data is
- * a result JSON coming from a search servlet.
+ * Renders the people lister with a specified set of data. The function uses
+ * a search query initially, then does the paginating and subsequent requests
+ * for data automatically
  * @param tuid {String} Unique id of the widget
- * @param iData {Array} An array of objects, containing the to be displayed
- * user's name, picture etc...
+ * @param iSearchQuery {String} A Sakai search query
+ * @param iConfig {Object} Optional config overrides
  * @returns void
  */
-sakai.api.UI.listPeople.render = function(tuid, iData) {
+sakai.api.UI.listPeople.render = function(tuid, iSearchQuery, iConfig) {
+
+    // Init
+    var $pl_container = $("#" + tuid + " .listpeople_content");
+
+    // Merge user defined config with defaults
+    for (var element in iConfig) {
+        if (iConfig.hasOwnProperty(element)) {
+            sakai.config.widgets.listpeople[tuid][element] = iConfig[element];
+        }
+    }
+
+    // Parse search query
+    var searchQuery = {};
+    var main_parts = iSearchQuery.split("?");
+    searchQuery.url = main_parts[0];
+    var arguments = main_parts[1].split("&");
+    for (var i=0, il = arguments.length; i < il; i++) {
+        var kv_pair = arguments[i].split("=");
+        searchQuery[kv_pair[0]] = kv_pair[1];
+    }
+
+    // Alter search query according to config
+    searchQuery.items = sakai.config.widgets.listpeople[tuid].items;
+
+    // Add hash to search query in case it's not there to prevent caching
+    if (!searchQuery["_"]) {
+        searchQuery["_"] = (Math.random() * 100000000000000000);
+    }
+
+    // Render the first page of results
+    sakai.api.UI.listPeople.addPage(tuid, 0, searchQuery);
 
 };
 
 
+sakai.api.UI.listPeople.addPage = function(tuid, pageNumber, searchQuery) {
 
+    // Create new container for the bit we load. This is then appended to the
+    // main container
+    var $pl_pageContainer = $("<ul id=\"listpeople_page_" + pageNumber + "\" class=\"listpeople_page loadinganim\"></ul>");
+    var $pl_container = $("#" + tuid + " .listpeople_content");
+
+    // Alter page number to the search query
+    searchQuery.page = pageNumber;
+
+    // Construct search query
+    var sq = searchQuery.url + "?";
+    for (var e in searchQuery) {
+        if (searchQuery.hasOwnProperty(e) && e !== "url") {
+            sq += e + "=" + searchQuery[e] + "&";
+        }
+    }
+
+    // Display empty new container with loading anim
+    $pl_container.append($pl_pageContainer);
+
+    // Make the request
+    $.ajax({
+        url: sq,
+        type: "GET",
+        success: function(rawData) {
+
+            var searchResults = $.evalJSON(rawData);
+
+            // Render the data template
+            var pageHTML = $.TemplateRenderer("#" + tuid + " .listpeople_content_pagetemplate", searchResults);
+
+            // Remove loading animation
+            $pl_pageContainer.removeClass("loadinganim");
+
+            // Inject results into DOM
+            $pl_pageContainer.html(pageHTML);
+
+            // Wire scroll
+            if (searchResults.total > (searchQuery.items * (pageNumber + 1))) {
+                $pl_container.bind("scroll", function(e){
+
+                    if ((e.target.scrollHeight - e.target.scrollTop - $(e.target).height() ) < 20) {
+                        $pl_container.unbind("scroll");
+                        console.log("calling", (pageNumber + 1));
+                        sakai.api.UI.listPeople.addPage(tuid, (pageNumber + 1), searchQuery);
+                    }
+                });
+            }
+
+
+        },
+        error: function() {
+            $pl_container.html($("#" + tuid + " .listpeople_error_template").html());
+        }
+    });
+};
 
 
 
