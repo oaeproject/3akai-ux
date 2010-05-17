4f171b7899d53d8b54c4405c3c37a2c700d74f83
hh354 <henryhoudmont@hotmail.com>
2010-05-17 15:38:42 +0100

    Fixed accidental removal of line in s.a.comm.sendMessage

--- dev/_lib/sakai_util/sakai_magic.js
+++ dev/_lib/sakai_util/sakai_magic.js
@@ -96,6 +96,7 @@ sakai.api.Communication = sakai.api.Communication || {};
 sakai.api.Communication.sendMessage = function(to, subject, body, category, reply, callback) {
 
 	var toUsers = "";
+	if(typeof(to) == "string"){
 		toUsers = "internal:"+to;
 	}else{
 		for(var i=0,il=to.length;i<il;i++){
