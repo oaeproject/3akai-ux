ccc093686053fd8e74e3e2368233f5fd8de27c73
hh354 <henryhoudmont@hotmail.com>
2010-06-08 12:35:35 +0100

    SAKIII-439 - removed the redirect link from the button once we're sure the user is logged in.

--- dev/_javascript/site_admin.js
+++ dev/_javascript/site_admin.js
@@ -1477,6 +1477,7 @@ sakai.site.site_admin = function(){
 
     // Bind Insert Link click event
     $("#more_link").html("More");
+    $("#more_link").attr("href","javascript:;");
     $("#more_link").addClass("more_link");
     $("#more_link").bind("click", function(ev){
         showHideMoreMenu(false);
