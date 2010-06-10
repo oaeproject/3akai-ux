748206cc94a910f7f5893ec1ab7f97ea8d1becbe
oszkarnagy <oszkar@caret.cam.ac.uk>
2010-06-10 17:47:06 +0100

    First working version of activity stream widget

--- devwidgets/activitystream/javascript/activitystream.js
+++ devwidgets/activitystream/javascript/activitystream.js
@@ -32,15 +32,81 @@ sakai.activitystream = function(tuid, showSettings) {
     // Configuration variables //
     /////////////////////////////
 
-    var activityData = [];
+    var activityData = {};
     var connectionData = {};
+    var activityitemsPerPage = 10;
+    var activityitemsSortOrder = "descending";
 
     // HTML elements
     var $rootel = $("#" + tuid);
     var $contentContainer = $("#as_content_container", $rootel);
     var $mainScreen = $("#as_main", $rootel);
     var $settingsScreen = $("#as_settings", $rootel);
+    var $feedContainer = $("#as_feed");
+    var $feedTitle = $("#as_feed_title");
 
+
+    /**
+     * getActivityData
+     * Fetches activity data from the back-end
+     * @param callback {Function} Optional callback function which is executed at when the data is returned from the server
+     * @returns void
+     */
+    var getActivityData = function(callback) {
+
+        $.ajax({
+            url: sakai.config.URL.ACTIVITY_PERSONAL,
+            traditional: true,
+            data: {
+                "items": activityitemsPerPage,
+                "sortOrder": activityitemsSortOrder
+            },
+            success: function(rawReturnedData) {
+
+                // Store fetched data in place where it is accesible for other widget functions
+                activityData = rawReturnedData;
+
+                if (typeof callback === "function") {
+                    callback(true);
+                }
+
+            },
+            error: function(xhr, status, thrownError) {
+
+                if (typeof callback === "function") {
+                    callback(false, xhr);
+                }
+
+            }
+        });
+
+    };
+
+    /**
+     * renderActivity
+     * Renders the activity feed on screen based on the feed data JSON
+     * @returns void
+     */
+    var renderActivity = function() {
+
+        // Create an unordered list container element for the results
+        var $resultContainer = $("<ul class=\"feed_list\"></ul>");
+
+        // Go through the feed data
+        for (var i = 0, il = activityData.results.length; i < il; i++) {
+
+            // Create a list item element for each one of the feed items
+            var $feedItem = $("<li>" + activityData.results[i]["sakai:activityMessage"] + "<li>");
+
+            // Append the list item to the list container
+            $resultContainer.append($feedItem);
+        }
+
+        // Add result container to the DOM
+        $feedContainer.html($resultContainer);
+
+
+    }
 
     /**
      * Init function
@@ -49,11 +115,17 @@ sakai.activitystream = function(tuid, showSettings) {
 
         // Which screen to show
         if (showSettings) {
+
             $settingsScreen.show();
             $mainScreen.hide();
+
         } else {
+
             $settingsScreen.hide();
             $mainScreen.show();
+
+            // Get the activity feed data, when ready call the rendering function
+            getActivityData(renderActivity);
         }
     };
 
