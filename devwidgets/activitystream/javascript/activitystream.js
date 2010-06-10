b7ffadefb1349317b7715b863e22b5117c08d736
oszkarnagy <oszkar@caret.cam.ac.uk>
2010-06-10 14:19:20 +0100

    Added activity stream widget skeleton

---
+++ devwidgets/activitystream/javascript/activitystream.js
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Sakai Foundation (SF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The SF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+/*global $, sdata, fluid, doPaging, saveDeliciousSettings */
+
+var sakai = sakai || {};
+
+/**
+ * Initialize the Activitystream widget
+ * @param {String} tuid unique id of the widget
+ * @param {Boolean} showSettings show the settings of the widget or not
+ */
+sakai.activitystream = function(tuid, showSettings) {
+
+
+    /////////////////////////////
+    // Configuration variables //
+    /////////////////////////////
+
+    var rootel = $("#" + tuid);
+
+    // URLs
+    sakai.config.URL.DELICIOUS_PROXY = "/var/proxy/delicious/bookmarks.json";
+
+    // HTML elements
+    var $contentContainer = $("#as_content_container", rootel);
+    var $mainScreen = $("#as_main", rootel);
+    var $settingsScreen = $("#as_settings", rootel);
+
+    /**
+     * Init function
+     */
+    var doInit = function(){
+
+        console.log("hello from activitystream widget");
+    };
+
+    doInit();
+};
+
+sakai.api.Widgets.widgetLoader.informOnLoad("activitystream");
