dea82e7cad5cac6c98b9f17653e259d4551583a0
Nicolaas Matthijs <nicolaas.matthijs@caret.cam.ac.uk>
2010-07-28 16:22:37 +0100

    NOJIRA - Whitespace fixes

--- dev/_tests/qunit/unit/security.js
+++ dev/_tests/qunit/unit/security.js
@@ -7,13 +7,13 @@ module("Security");
  */
 
 test("Escape HTML", function(){
-    
+
     var htmlString = "<a href='http://www.google.com'>Advertising my script enabled site with redirect</a>";
     var escapedExpected = "&lt;a href='http://www.google.com'&gt;Advertising my script enabled site with redirect&lt;/a&gt;";
-    
+
     htmlString = sakai.api.Security.escapeHTML(htmlString);
     equals(htmlString, escapedExpected, "The escaped string is correct");
-    
+
 });
 
 })();
