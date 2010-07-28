d9f9e7cdd05890550c553053af73c672124adf05
Nicolaas Matthijs <nicolaas.matthijs@caret.cam.ac.uk>
2010-07-28 12:41:11 +0100

    SAKIII-537 - Adding qUnit test for the new escapeHTML sakai_magic function

---
+++ dev/_tests/qunit/unit/security.js
@@ -0,0 +1,19 @@
+module("Security");
+
+(function(){
+
+/*
+ * Test the escape HTML function
+ */
+
+test("Escape HTML", function(){
+    
+    var htmlString = "<a href='http://www.google.com'>Advertising my script enabled site with redirect</a>";
+    var escapedExpected = "&lt;a href='http://www.google.com'&gt;Advertising my script enabled site with redirect&lt;/a&gt;";
+    
+    htmlString = sakai.api.Security.escapeHTML(htmlString);
+    equals(htmlString, escapedExpected, "The escaped string is correct");
+    
+});
+
+})();
