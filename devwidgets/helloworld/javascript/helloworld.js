b08f9fe770bf7b4c530c67521daae0b7c5b74d52
Nicolaas Matthijs <nicolaas.matthijs@caret.cam.ac.uk>
2010-08-14 23:20:06 +0100

    SAKIII-562 - Some final fixes to the HelloWorld widget

--- devwidgets/helloworld/javascript/helloworld.js
+++ devwidgets/helloworld/javascript/helloworld.js
@@ -73,13 +73,7 @@ sakai.helloworld = function(tuid,showSettings){
      * @param {Object} color
      */
     var selectCurrentColor = function(color){
-        var select = $(colorPicker,rootel).get(0);
-        var toSelect = 0;
-        for (var i = 0; i < select.options.length; i++){
-            var option = select.options[i];
-            toSelect = option.value === color.color ? i : 0;
-        }
-        select.selectedIndex = toSelect;
+        $(colorPicker,rootel).val(color.color);
     };
 
 
@@ -110,7 +104,9 @@ sakai.helloworld = function(tuid,showSettings){
             if (success) {
                 callback(data);
             } else {
-                callback(defaultColor);
+                callback({
+                    "color": defaultColor
+                });
             }
 
         });
@@ -123,7 +119,7 @@ sakai.helloworld = function(tuid,showSettings){
             $(settingsContainer, rootel).show();
         } else {
             var me = sakai.data.me;
-            $(usernameContainer, rootel).text(sakai.api.Security.saneHTML(sakai.api.User.getProfileBasicElementValue(me, "firstName")));
+            $(usernameContainer, rootel).text(sakai.api.Security.saneHTML(sakai.api.User.getProfileBasicElementValue(me.profile, "firstName")));
             getPreferedColor(showHelloWorld);
         }
     };
