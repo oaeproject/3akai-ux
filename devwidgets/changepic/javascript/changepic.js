a6dcc7e562d0c830b151184cd83b4509eadbae62
unknown <KuKu@.(none)>
2010-08-31 21:28:54 +1000

    SAKIII-594 Can't select region of new profile picture in IE8 fixed

--- devwidgets/changepic/javascript/changepic.js
+++ devwidgets/changepic/javascript/changepic.js
@@ -147,8 +147,8 @@ sakai.changepic = function(tuid, showSettings){
         userSelection = selection;
 
         // How much has the user scaled down the image?
-        var scaleX = thumbnailWidth / selection.width;
-        var scaleY = thumbnailHeight / selection.height;
+        var scaleX = thumbnailWidth / (selection.width || 1);
+        var scaleY = thumbnailHeight / (selection.height || 1);
 
         // Change the thumbnail according to the user his selection via CSS.
         $(thumbnail).css({
@@ -437,32 +437,8 @@ sakai.changepic.startCallback = function(){
  */
 sakai.changepic.completeCallback = function(response){
 
-    var tosave = {
-        "_name": "profilepicture"
-    };
-
-    // We edit the profile.json file with the new profile picture.
-    var stringtosave = $.toJSON(tosave);
-
-    // We edit the me object.
-    // This saves a request and will be checked in the doInit function later on.
-    sakai.data.me.profile.picture = stringtosave;
-
-    // the object we wish to insert into the profile.json file.
-    var data = {"picture":stringtosave,"_charset_":"utf-8"};
-
-    $.ajax({
-        url: "/~" + sakai.data.me.user.userid + "/public/authprofile.json",
-        type : "POST",
-        data : data,
-        success : function(data) {
-            // we have saved the profile, now do the widgets other stuff.
-            sakai.changepic.doInit(true);
-        },
-        error: function(xhr, textStatus, thrownError) {
-            alert("An error has occured");
-        }
-    });
+    // we have saved the profile, now do the widgets other stuff.
+    sakai.changepic.doInit(true);
 };
 
 
