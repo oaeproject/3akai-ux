e2bb5bf49f14f22f127e5e3af6f9819817f0c33b
Chris Roby <christopher.roby@gmail.com>
2010-09-05 19:14:53 -0700

    SAKIII-600: Adding file upload support to the embed content widget

--- devwidgets/embedcontent/javascript/embedcontent.js
+++ devwidgets/embedcontent/javascript/embedcontent.js
@@ -43,6 +43,8 @@ sakai.embedcontent = function(tuid, showSettings) {
     var $embedcontent_display_options = $("#embedcontent_display_options", $rootel);
     var $embedcontent_display_options_select = $("#embedcontent_display_options_select", $rootel);
     var $embedcontent_metadata_container = $("#embedcontent_metadata_container", $rootel);
+    var $embedcontent_search_for_content = $("#embedcontent_search_for_content", $rootel);
+    var $embedcontent_add_content = $("#embedcontent_add_content", $rootel);
 
     var $embedcontent_alternative_display_name_value = $("#embedcontent_alternative_display_name_value", $rootel);
     var $embedcontent_description_value = $("#embedcontent_description_value", $rootel);
@@ -50,6 +52,9 @@ sakai.embedcontent = function(tuid, showSettings) {
     var $embedcontent_page_name_template = $("#embedcontent_page_name_template", $rootel);
     var $embedcontent_display_options_select_template = $("#embedcontent_display_options_select_template", $rootel);
     var $embedcontent_content_html_template = $("#embedcontent_content_html_template", $rootel);
+    var $embedcontent_new_item_template = $("#embedcontent_new_item_template", $rootel);
+
+    var $fileuploadContainer = $("#fileupload_container", $rootel);
 
     var selectedItems = [];
     var firstTime = true;
@@ -82,6 +87,22 @@ sakai.embedcontent = function(tuid, showSettings) {
         return mimetype;
     };
 
+    var createDataObject = function(result, name) {
+        var mimetype = getMimeType(result);
+        var dataObj = {
+            "value": name || result['jcr:name'],
+            "name": result['sakai:pooled-content-file-name'],
+            "type": "file",
+            "filetype": mimetype.split("/")[0],
+            "mimetype": mimetype,
+            "description": result["sakai:description"] || "",
+            "path": "/p/" + (name || result['jcr:name']),
+            "fileSize": sakai.api.Util.convertToHumanReadableFileSize(result["jcr:content"][":jcr:data"]),
+            "link": "/p/" + (name || result['jcr:name']) + "/" + result['sakai:pooled-content-file-name']
+        };
+        return dataObj;
+    };
+
     var setupAutoSuggest = function() {
         $embedcontent_content_input.autoSuggest("",{
             source: function(query, add) {
@@ -90,18 +111,7 @@ sakai.embedcontent = function(tuid, showSettings) {
                     if (success) {
                         var suggestions = [];
                         $.each(data.results, function(i) {
-                            var mimetype = getMimeType(data.results[i]);
-                            var dataObj = {
-                                "value": data.results[i]['jcr:name'], 
-                                "name": data.results[i]['sakai:pooled-content-file-name'], 
-                                "type": "file", 
-                                "filetype": mimetype.split("/")[0], 
-                                "mimetype": mimetype, 
-                                "description": data.results[i]["sakai:description"],
-                                "path": "/p/" + data.results[i]['jcr:name'],
-                                "fileSize": sakai.api.Util.convertToHumanReadableFileSize(data.results[i]["jcr:content"][":jcr:data"]),
-                                "link": "/p/" + data.results[i]['jcr:name'] + "/" + data.results[i]['sakai:pooled-content-file-name']
-                            };
+                            var dataObj = createDataObject(data.results[i]);
                             suggestions.push(dataObj);
                         });
                         add(suggestions);
@@ -138,6 +148,33 @@ sakai.embedcontent = function(tuid, showSettings) {
         selectedItems = newItems;
     };
 
+    var addChoicesFromPickeradvanced = function(data) {
+      $.each(data, function(i,val) {
+
+          var itemHTML = '<li id="as-selection-' + id + '" class="as-selection-item"><a class="as-close">×</a>' + name + '</li>';
+          itemHTML = sakai.api.Security.saneHTML(itemHTML);
+          $("#as-values-" + tuid).val(id + "," + $("#as-values-" + tuid).val());
+          $("#as-original-" + tuid).before(itemHTML);
+      });
+      $("input#" + tuid).val('').focus();
+    };
+
+    var addChoicesFromFileUpload = function(data) {
+      $.each(data, function(i,val) {
+          $.ajax({
+             url: val.url + ".infinity.json",
+             success: function(data) {
+                 var newObj = createDataObject(data, val.url.split("/p/")[1]);
+                 selectedItems.push(newObj);
+                 var itemHTML = $.TemplateRenderer($embedcontent_new_item_template, {"name": newObj.name, "value": newObj.value});
+                 $("#as-values-" + tuid).val(newObj.value + "," + $("#as-values-" + tuid).val());
+                 $("#as-original-" + tuid).before(itemHTML);
+             }
+          });
+      });
+      $("input#" + tuid).val('').focus();
+    };
+
     var doEmbed = function() {
         var embedContentHTML = "";
         var objectData = {
@@ -147,7 +184,6 @@ sakai.embedcontent = function(tuid, showSettings) {
             "items": selectedItems
         };
         embedContentHTML = $.TemplateRenderer($embedcontent_content_html_template, objectData);
-        console.log(embedContentHTML);
         tinyMCE.get('elm1').execCommand("mceInsertContent", true, embedContentHTML);
     };
 
@@ -160,7 +196,22 @@ sakai.embedcontent = function(tuid, showSettings) {
     $embedcontent_cancel.bind("click", function() {
         $embedcontent_dialog.jqmHide();
     });
-    
+
+    $embedcontent_search_for_content.bind("click", function() {
+
+    });
+
+    $embedcontent_add_content.bind("click", function() {
+        sakai.fileupload.initialise();
+    });
+
+    $(window).bind("sakai-fileupload-complete", function(e, data) {
+        console.log(data);
+        var files = data.files;
+        addChoicesFromFileUpload(files);
+        $embedcontent_display_options.show();
+    });
+
     $embedcontent_display_options_select.bind("change", function(e) {
         if ($embedcontent_display_options_select.find("option:selected").val() === "show_content_and_description") {
             $embedcontent_metadata_container.show();
@@ -177,12 +228,14 @@ sakai.embedcontent = function(tuid, showSettings) {
     $embedcontent_dialog.jqm({
         modal: true,
         overlay: 20,
+        zIndex: 3000,
         toTop: true
     });
 
 
     var doInit = function() {
         $(window).trigger("sakai-embedcontent-ready");
+        sakai.api.Widgets.widgetLoader.insertWidgets("#"+tuid);
     };
 
     doInit();
--- devwidgets/fileupload/javascript/fileupload.js
+++ devwidgets/fileupload/javascript/fileupload.js
@@ -42,6 +42,7 @@ sakai.fileupload = function(tuid, showSettings){
     // All files that need to have been uploaded
     var uploadedFiles;
     var tags = [];
+    var dataResponse = false;
 
     var setDescriptionandName = false;
     var filesUploaded = false;
@@ -279,7 +280,7 @@ sakai.fileupload = function(tuid, showSettings){
 
         // Close the jqm box
         $(fileUploadContainer).jqmHide();
-
+        $(window).trigger("sakai-fileupload-complete", {"files": dataResponse});
         // Show notification
         if (context !== "new_version") {
             if (uploadedLink) {
@@ -364,6 +365,7 @@ sakai.fileupload = function(tuid, showSettings){
                 requests: $.toJSON(batchDescriptionData)
             },
             success: function(data){
+                dataResponse = batchDescriptionData;
                 // Description and name set
                 setDescriptionandName = true;
                 // When this is a new revision of a file no more operations are executed
@@ -586,10 +588,12 @@ sakai.fileupload = function(tuid, showSettings){
             url: path,
             data: body,
             type: "POST",
+            dataType: "json",
             beforeSend : function(xmlReq){
                 xmlReq.setRequestHeader("Content-type", "multipart/form-data; boundary=AAAAA");
             },
             success: function(data){
+                dataResponse = data;
                 newVersionIsLink = false;
                 uploadedLink = true;
                 resetFields();
@@ -723,17 +727,17 @@ sakai.fileupload = function(tuid, showSettings){
         $(fileUploadProgressId).addClass(fileUploadProgress);
         // Disable input fields
         if (context !== "new_version") {
-            $(fileUploadAddTags).attr("disabled", true);
-            $(fileUploadAddDescription).attr("disabled", true);
-            $(fileUploadPermissionsSelect).attr("disabled", true);
-            $(fileUploadLinkBoxInput).attr("disabled", true);
-            $(fileUploadAddLinkButton).attr("disabled", true);
+            $(fileUploadAddTags).attr("disabled", "disabled");
+            $(fileUploadAddDescription).attr("disabled", "disabled");
+            $(fileUploadPermissionsSelect).attr("disabled", "disabled");
+            $(fileUploadLinkBoxInput).attr("disabled", "disabled");
+            $(fileUploadAddLinkButton).attr("disabled", "disabled");
             // Initiate the tagging process
             formatTags($(fileUploadAddTags).val());
         } else {
-            $(fileUploadAddVersionDescription).attr("disabled", true);
+            $(fileUploadAddVersionDescription).attr("disabled", "disabled");
         }
-        $(".fileupload_file_name input").attr("disabled", true);
+        $(".fileupload_file_name input").attr("disabled", "disabled");
         $(".MultiFile-remove").addClass("hide_remove_link");
     });
 
@@ -757,6 +761,7 @@ sakai.fileupload = function(tuid, showSettings){
     $(fileUploadContainer).jqm({
         modal: true,
         overlay: 20,
+        zIndex: 4000,
         toTop: true,
         onHide: closeUploadBox
     });
