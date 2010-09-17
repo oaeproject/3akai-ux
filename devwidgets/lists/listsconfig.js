999efd06536bb7d48215f1702c5448afd3e38e40
Chris Roby <christopher.roby@gmail.com>
2010-09-17 12:10:09 -0700

    SAKIII-871: Lists widget

---
+++ devwidgets/lists/listsconfig.js
@@ -0,0 +1,153 @@
+sakai.Lists = {
+    "Label": "School",
+    "list": [
+    {
+        "Label": "Wagner",
+        "list": [
+        {
+            "Label": "Program",
+            "multiple": true,
+            "list": [
+            "PNP",
+            "HPAM",
+            "UP",
+            "EMPA",
+            "EMPA Nurse Leaders",
+            "PhD"
+            ]
+        },
+        {
+            "Label": "Specialization",
+            "multiple": true,            
+            "list": [
+            "Finance",
+            "International",
+            "Management",
+            "Policy",
+            "Environment, Infrastructure, and Transportation",
+            "Economic Development and Housing Policy",
+            "International Planning"
+            ]
+        },
+        {
+            "Label": "Enrollment Status",
+            "multiple": false,            
+            "list": [
+            "Full-Time",
+            "Part-Time"
+            ]
+        },
+        {
+            "Label": "Gradutation Date (Month)",
+            "multiple": false,            
+            "list": [
+            "May ",
+            "January",
+            "September"
+            ]
+        },
+        {
+            "Label": "Graduation Date (Year)",
+            "multiple": false,            
+            "list": [
+            "2010",
+            "2011",
+            "2012",
+            "2013",
+            "2020"
+            ]
+        },
+        {
+            "Label": "Interest Area(s)",
+            "multiple": true,            
+            "list": [
+            "Aging",
+            "Arts and Culture",
+            "Children and Families",
+            "Civil Rights/Human Rights",
+            "Coalition Building",
+            "Community Development",
+            "Corporate Accountability",
+            "Identity and Diversity",
+            "Economic Development",
+            "Education",
+            "Elections",
+            "Environment",
+            "Finance",
+            "Food & Nutrition",
+            "Global Health",
+            "Governance",
+            "Health",
+            "Housing",
+            "Immigration",
+            "Infrastructure",
+            "International Development",
+            "Justice System",
+            "Labor and Employment",
+            "Leadership Development",
+            "Local and/or State Government",
+            "Microfinance",
+            "Nonprofit Management",
+            "NYC Government",
+            "Organizational Development",
+            "Philanthropy",
+            "Politics",
+            "Poverty/Hunger",
+            "Public Policy",
+            "Region - Africa: North",
+            "Region - Africa: Sub-Saharan",
+            "Region - Asia: Central",
+            "Region - Asia: East",
+            "Region - Asia: South",
+            "Region - Asia: Southeast",
+            "Region - Central America",
+            "Region - Europe: Eastern",
+            "Region - Europe: Western",
+            "Region - Former Soviet Republics",
+            "Region - Middle East",
+            "Region - North America",
+            "Region – Russia",
+            "Region - South America",
+            "Region - United States",
+            "Social Entrepreneurship",
+            "Transportation",
+            "Urban Planning",
+            "US Federal Government",
+            "US National Politics",
+            "Water"
+            ]
+        },
+        {
+            "Label": "Student Groups",
+            "multiple": true,            
+            "list": [
+            "Wagner Student Association (WSA)",
+            "Alliance of Latin American Students (ALAS)",
+            "Asian Pacific American Student Alliance (APASA)",
+            "Black Student Alliance (BSA)",
+            "Jewish Wagner Professional Association (JeWPA)",
+            "Non Profit Network (NPN)",
+            "Stonewall Policy Alliance (SPA)",
+            "Students for Criminal Justice Reform (SCJR)",
+            "Students Exploring Arts and Culture (SNEAC)",
+            "Students for Innovation (BRIDGE)",
+            "Urban Planning Student Association (UPSA)",
+            "Wagner Review",
+            "Wagner Economic and Finance Association (WEFA)",
+            "Wagner Environmental Policy Association (WEPA)",
+            "Wagner Education Policy Studies Association (WEPSA)",
+            "Wagner Health Network (WHN)",
+            "Wagner International Student Society (WISS)",
+            "Wagner Policy Alliance (WPA)",
+            "Wagner Student Association for Africa(WSAFA)",
+            "Wagner Transportation Association (WTA)",
+            "Wagner Volunteer Corps (WVC)",
+            "Wagner Women’s Caucus (WWC)"
+            ]
+        }
+        ]
+    }
+    ]
+};
+
+sakai.api.Widgets.widgetLoader.informOnLoad("lists");
