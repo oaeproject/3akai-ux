919987227615e79b3c4e064eca5ff94b5a4bed57
Chris Roby <christopher.roby@gmail.com>
2010-11-01 17:01:58 -0700

    Manually merging sitespages_admin.js change from master into Q2 sitespages

--- devwidgets/sitespages/javascript/sitespages.js
+++ devwidgets/sitespages/javascript/sitespages.js
@@ -1340,11 +1340,10 @@ sakai.sitespages = function(tuid,showSettings){
      * @param {String} content The content of the WYSIWYG editor
      */
     var checkContent = function(content){
-        if(!content.replace(/ /g,"%20")) {
-            alert(sakai.api.i18n.General.getValueForKey("PLEASE_ENTER_SOME_CONTENT"));
-            return false;
-        }else{
+        if (content) {
             return true;
+        } else {
+            return false;
         }
     };
 
@@ -1415,7 +1414,7 @@ sakai.sitespages = function(tuid,showSettings){
                 $("#title-input").focus();
                 return;
             }
-            var newcontent = getContent();  // Get the content from tinyMCE
+            var newcontent = getContent() || "";  // Get the content from tinyMCE
             if (!checkContent(newcontent)) {
                 alert(sakai.api.i18n.General.getValueForKey("PLEASE_ENTER_SOME_CONTENT"));
                 return;
