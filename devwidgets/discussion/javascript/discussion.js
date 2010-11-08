e035abd2fe65fbda9950388fac8d86fb2ff559d7
Zach A. Thomas <zach@aeroplanesoftware.com>
2010-11-08 13:14:11 -0600

    KERN-1331 the answer to non-ascii characters in discussions is to add _charset_=utf-8 to the POST

--- devwidgets/discussion/javascript/discussion.js
+++ devwidgets/discussion/javascript/discussion.js
@@ -257,7 +257,8 @@ sakai.discussion = function(tuid, showSettings){
         var post = {
             'sakai:subject': subject,
             'sakai:body': body,
-            'sakai:editedby': me.user.userid
+            'sakai:editedby': me.user.userid,
+            '_charset_':'utf-8'
         };
 
         $.ajax({
@@ -682,7 +683,8 @@ sakai.discussion = function(tuid, showSettings){
                 'sakai:replyon': id,
                 'sakai:messagebox': 'outbox',
                 'sakai:sendstate': 'pending',
-                'sakai:to': "discussion:w-" + store
+                'sakai:to': "discussion:w-" + store,
+                '_charset_':'utf-8'
             };
 /*            sakai.api.Widgets.saveWidgetData(tuid, object, function(success, data){
                 alert("I seem to have saved a discussion topic.");
@@ -798,7 +800,8 @@ sakai.discussion = function(tuid, showSettings){
                 'sakai:initialpost': true,
                 'sakai:messagebox': 'outbox',
                 'sakai:sendstate': 'pending',
-                'sakai:to': "discussion:w-" + store
+                'sakai:to': "discussion:w-" + store,
+                '_charset_':"utf-8"
             };
 /*            sakai.api.Widgets.saveWidgetData(tuid, object, function(success, data){
                 alert("I seem to have saved a discussion topic.");
@@ -870,6 +873,7 @@ sakai.discussion = function(tuid, showSettings){
         post['sakai:marker'] = tuid;
         post['sakai:messagebox'] = "outbox";
         post['sakai:sendstate'] = "pending";
+        post['_charset_'] = "utf-8";
         return post;
     };
 
