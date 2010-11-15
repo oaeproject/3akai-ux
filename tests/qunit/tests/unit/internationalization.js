e7c68bf304cdaabe8a3772e845797e05f0928637
Chris Roby <christopher.roby@gmail.com>
2010-11-15 09:38:04 -0800

    NOJIRA changing i18n test to use sakai.widgets namespace

--- tests/qunit/tests/unit/internationalization.js
+++ tests/qunit/tests/unit/internationalization.js
@@ -108,9 +108,9 @@ var testInternationalization = function(){
         "/dev/admin/widgets.html"
     ];
 
-    for (var i in Widgets.widgets) {
-        if (Widgets.widgets.hasOwnProperty(i) && Widgets.widgets[i].url) {
-            pageArray.push(Widgets.widgets[i].url);
+    for (var i in sakai.widgets.widgets) {
+        if (sakai.widgets.widgets.hasOwnProperty(i) && sakai.widgets.widgets[i].url) {
+            pageArray.push(sakai.widgets.widgets[i].url);
         }
     }
 
