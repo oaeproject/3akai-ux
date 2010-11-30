d5f1e9c2aec0f78cd8ddcacb7fc55b6748b07f94
Chris Roby <christopher.roby@gmail.com>
2010-11-29 18:16:08 -0800

    SAKIII-1867 Split tests into unit and integration. Significant overhaul of testing infrastructure to include mockjax, mocked results, relative paths, and more.

--- tests/qunit/js/sakai_qunit_runner.js
+++ tests/qunit/js/sakai_qunit_runner.js
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Sakai Foundation (SF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The SF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+
 (function($) {
 
 // Tracking variables
@@ -34,9 +53,13 @@ var runTest = function(test) {
  * runAllTests populates the tests array with any link in the index.html file
  * that contains a test class. It will then kick off the first test.
  */
-var runAllTests = function() {
+var runAllTests = function(which) {
     tests = [];
-    var $tests = $('a.test');
+    var selector = "a.test";
+    if (which) {
+        selector += "." + which;
+    }
+    var $tests = $(selector);
     $.each($tests, function(i,val) {
         tests.push({url: $(val).attr("href"), title: $(val).text()});
     });
@@ -116,6 +139,16 @@ $("#show_list a").bind("click", function() {
 $("#run_all").bind("click", function() {
     $("#show_list a").click();
     runAllTests();
+});
+
+$("#run_all_unit").bind("click", function() {
+    $("#show_list a").click();
+    runAllTests("unit");
+});
+
+$("#run_all_integration").bind("click", function() {
+    $("#show_list a").click();
+    runAllTests("integration");
 });
 
 // Handle clicks on the result rows to show the iframe with the results
