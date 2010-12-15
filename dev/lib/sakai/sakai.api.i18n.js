d1902e65a5e6ef56a471c9a3a6aaebee404b3014
soe soe <khinsoesoekyaw@gmail.com>
2010-12-15 14:24:14 +1100

    SAKIII-1939 Handle for equal sign in value of the key in properties file.

--- dev/lib/sakai/sakai.api.i18n.js
+++ dev/lib/sakai/sakai.api.i18n.js
@@ -244,9 +244,9 @@ sakai.api.i18n.init = function(){
         for (var i in inputLine) {
             // IE 8 i has indexof as well which breaks the page.
             if (inputLine.hasOwnProperty(i)) {
-                var keyValuePair = inputLine[i].split(/ \s*\=\s*/);
-                var key = keyValuePair[0];
-                var value = keyValuePair[1];
+                var keyValuePair = inputLine[i].split(/\s*\=\s*/);
+                var key = keyValuePair.shift();
+                var value = keyValuePair.join(" = ");
                 json[key] = value;
             }
         }
