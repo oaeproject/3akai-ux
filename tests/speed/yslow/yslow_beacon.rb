a486075d706250ffacbe5346d0f7f2a271ce2162
Chris Roby <christopher.roby@gmail.com>
2011-01-13 14:03:57 -0800

    NOJIRA Updated YSlow! beacon to include cached results

--- tests/speed/yslow/yslow_beacon.rb
+++ tests/speed/yslow/yslow_beacon.rb
@@ -8,21 +8,33 @@ post '/' do
   # structure of request.body is documented at 
   # http://developer.yahoo.com/yslow/help/beacons.html#version2
   pageSize = (body["w"]/1000.0).to_f.to_s + "kb"
+  cachePageSize = (body["w_c"]/1000.0).to_f.to_s + "kb"
   yslowScore = body["o"].to_s
   url = CGI::unescape(body["u"])
   totalNumRequests = body["r"].to_s
+  cahceRequests = body["r_c"].to_s
   pageLoadTime = (body["lt"]/1000.0).to_f.to_s + "s"
-  individualStats = body["stats"]
+  stats = body["stats"]
+  cacheStats = body["stats_c"]
   out = ""
   out << "--------------------------\n"
   out << "URL: #{url}\n"
   out << "YSlow! Score: #{yslowScore}/100\n"
-  out << "HTTP Requests: #{totalNumRequests}\n"
   out << "Page Load Time: #{pageLoadTime}\n"
+  out << "\nFirst visit, no cache:\n"
+  out << "HTTP Requests: #{totalNumRequests}\n"
   out << "Page Size: #{pageSize}\n"
   out << "Page components:\n"
   out << "\t|Requests\t|Component\t|Size\t|\n"
-  individualStats.each do |stat|
+  stats.each do |stat|
+    out << "\t|" << stat[1]["r"].to_s << "\t|" << stat[0] << "\t|" << (stat[1]["w"]/1000.0).to_f.to_s << "kb\t|\n"
+  end
+  out << "\nWith a primed cache (already visited the page):\n"
+  out << "HTTP Requests: #{cahceRequests}\n"
+  out << "Page Size: #{cachePageSize}\n"
+  out << "Page components:\n"
+  out << "\t|Requests\t|Component\t|Size\t|\n"
+  cacheStats.each do |stat|
     out << "\t|" << stat[1]["r"].to_s << "\t|" << stat[0] << "\t|" << (stat[1]["w"]/1000.0).to_f.to_s << "kb\t|\n"
   end
   out << "--------------------------\n"
