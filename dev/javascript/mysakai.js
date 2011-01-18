790064059454b842979e49077ec6f34123692503
jsloane <jsloane@csu.edu.au>
2011-01-18 16:29:22 +1100

    NOJIRA seeing how requirejs works

--- dev/javascript/mysakai.js
+++ dev/javascript/mysakai.js
@@ -19,6 +19,25 @@
 
 /*global Config, $ */
 
+require({
+        baseUrl: "/dev",
+        paths: {
+            "jquery": "lib/jquery",
+            "jqueryPlugins": "lib/jquery/plugins",
+            "imgAreaSelect": "lib/jquery/plugins/imgareaselect"
+        },
+        waitSeconds: 15
+    },
+    ["/dev/lib/jquery/plugins/imgareaselect/jquery.imgareaselect.js"],
+    /*["imgAreaSelect/jquery.imgareaselect.js"],*/
+    function(someModule, myModule) {
+        //This function will be called when all the dependencies
+        //listed above are loaded. Note that this function could
+        //be called before the page is loaded.
+        //This callback is optional.
+    }
+);
+
 var sakai = sakai || {};
 
 sakai.mysakai = function(){
