c2bd305d8cd5b519430bc74ddde897de01848d1c
jsloane <jsloane@csu.edu.au>
2011-02-04 15:10:31 +1100

    SAKIII-1827 change to mygroups widget to only trim group titles if widget is not hidden

--- devwidgets/mygroups/javascript/mygroups.js
+++ devwidgets/mygroups/javascript/mygroups.js
@@ -130,15 +130,16 @@ require(["jquery", "sakai/sakai.api.core"], function($, sakai) {
 
                 // make sure the newly added content is properly styled with
                 // threedots truncation
-                $(ellipsisContainer).css("width", $(ellipsisContainer).width() + "px");
-                $(ellipsisContainer, rootel).ThreeDots({
-                    max_rows: 1,
-                    text_span_class: "mygroups_ellipsis_text",
-                    e_span_class: "mygroups_e_span_class",
-                    whole_word: false,
-                    alt_text_t: true
-                });
-
+                if (rootel.is(":visible")) {
+                    $(ellipsisContainer).css("width", $(ellipsisContainer).width() + "px");
+                    $(ellipsisContainer, rootel).ThreeDots({
+                        max_rows: 1,
+                        text_span_class: "mygroups_ellipsis_text",
+                        e_span_class: "mygroups_e_span_class",
+                        whole_word: false,
+                        alt_text_t: true
+                    });
+                }
             }
         };
 
