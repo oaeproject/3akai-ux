8f77199d0b0f6f6d89855bd843731ea6905406f9
Gaurav Cedric Bhatnagar <gcb@gauravb.org>
2011-02-09 15:26:11 -0800

    SAK-2238 to avoid code duplication, the public add_selected_item function calls the newly created internal one.

--- dev/lib/jquery/plugins/jquery.autoSuggest.sakai-edited.js
+++ dev/lib/jquery/plugins/jquery.autoSuggest.sakai-edited.js
@@ -366,22 +366,8 @@
                     opts.selectionAdded.call(this, org_li.prev());
                 }
                 
-                $.fn.autoSuggest.add_selected_item = function(data, num){
-                    values_input.val(values_input.val()+data[opts.selectedValuesProp]+",");
-                    var item = $('<li class="as-selection-item" id="as-selection-'+num+'"></li>').click(function(){
-                            opts.selectionClick.call(this, $(this));
-                            selections_holder.children().removeClass("selected");
-                            $(this).addClass("selected");
-                        }).mousedown(function(){ input_focus = false; });
-                    var close = $('<a class="as-close">&times;</a>').click(function(){
-                            values_input.val(values_input.val().replace(data[opts.selectedValuesProp]+",",""));
-                            opts.selectionRemoved.call(this, item);
-                            input_focus = true;
-                            input.focus();
-                            return false;
-                        });
-                    org_li.before(item.html(data[opts.selectedItemProp]).prepend(close));
-                    opts.selectionAdded.call(this, org_li.prev());
+                $.fn.autoSuggest.add_selected_item = function (data, num) {
+                    add_selected_item(data, num);
                 };
                 
                 function moveSelection(direction){
