dcc3ba39c12a4fa071e09acbfdadb6144ce09548
Chris Roby <christopher.roby@gmail.com>
2011-02-17 13:21:34 -0800

    KERN-1554 changing the path to the pages data

--- dev/javascript/show.js
+++ dev/javascript/show.js
@@ -235,10 +235,10 @@ require(["jquery","sakai/sakai.api.core"], function($, sakai) {
         var setEntityPath = function() {
             switch (entityType) {
                 case "user":
-                    entityPath = entityPrefix + sakai_global.profile.main.data.path;
+                    entityPath = "/" + sakai_global.profile.main.data.homePath;
                     break;
                 case "group":
-                    entityPath = entityPrefix + sakai_global.currentgroup.data.authprofile.path;
+                    entityPath = "/" + sakai_global.currentgroup.data.authprofile.homePath;
                     break;
             }
         };
