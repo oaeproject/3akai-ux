9bb1dc83b06de573803f5fa9d430e54facd1c058
Bert Pareyn <bp323@caret.cam.ac.uk>
2011-03-29 16:32:11 +0100

    SAKIII-2636 Finish Carousel animation, next/prev buttons, circular buttons in the middle, add in content previews for images and videos

--- devwidgets/carousel/javascript/carousel.js
+++ devwidgets/carousel/javascript/carousel.js
@@ -60,6 +60,40 @@ require(["jquery", "sakai/sakai.api.core"], function($, sakai) {
             return "<img src=\"" + json.contentURL + "\" style=\"max-width:" + json.maxwidth + "; height:" + json.height + ";\"/>";
         };
 
+        var isJwPlayerSupportedVideo = function(mimeType) {
+            supported = false;
+            if (mimeType.substring(0, 6) === "video/" ){
+                var mimeSuffix = mimeType.substring(6);
+                if (mimeSuffix === "x-flv" || mimeSuffix === "mp4" || mimeSuffix === "3gpp" || mimeSuffix === "quicktime") {
+                    supported = true;
+                }
+            }
+            return supported;
+        };
+
+        var createSWFObject = function(url, params, flashvars){
+            if (!url){
+                url = "/devwidgets/video/jwplayer/player-licensed.swf";
+            }
+            var so = new SWFObject(url,'ply', '350', '197','9','#ffffff');
+            so.addParam('allowfullscreen','true');
+            if (params.allowscriptaccess) {
+                so.addParam('allowscriptaccess', params.allowscriptaccess);
+            } else {
+                so.addParam('allowscriptaccess', 'always');
+            }
+            so.addParam('wmode','opaque');
+            return so;
+        };
+
+        var renderVideoPlayer = function(url){
+            var so = createSWFObject(false, {}, {});
+            so.addVariable('file', "/p/" + url);
+            so.addVariable('stretching','uniform');
+            so.write("carousel_video");
+            return $("#carousel_video_holder").html();
+        };
+
 
         /////////////////////
         // RENDER CAROUSEL //
@@ -107,13 +141,13 @@ require(["jquery", "sakai/sakai.api.core"], function($, sakai) {
         }
 
         var renderCarousel = function(dataArr){
-            $(carouselContainer).html(sakai.api.Util.TemplateRenderer(carouselSingleColumnTemplate, {"data":dataArr}));
+            $(carouselContainer).html(sakai.api.Util.TemplateRenderer(carouselSingleColumnTemplate, {"data":dataArr},false,false));
             $(carouselContainer).jcarousel({
                 auto: 5,
                 animation: "slow",
                 scroll: 1,
                 easing: "swing",
-                size: 4,
+                size: dataArr.length,
                 initCallback: carouselBinding,
                 buttonNextHTML: "<div id=\"carousel_next_button\"></div>",
                 buttonPrevHTML: "<div id=\"carousel_prev_button\"></div>",
@@ -130,8 +164,12 @@ require(["jquery", "sakai/sakai.api.core"], function($, sakai) {
             for (var item in data.results) {
                 var obj = {}
 
-                if (data.results[item]["_mimeType"].substring(0, 6) === "image/") {
+                if (data.results[item]["_mimeType"] && data.results[item]["_mimeType"].substring(0, 6) === "image/") {
                     obj.preview = renderImagePreview(data.results[item]["jcr:name"], data.results[item]["_lastModified"]);
+                } else if (isJwPlayerSupportedVideo(data.results[item]["_mimeType"] || "")) {
+                    obj.preview = renderVideoPlayer(data.results[item]["jcr:name"]);
+                } else {
+                    obj.preview = "<img src=\"background:url(\"/devwidgets/carousel/images/carousel_video_image.png\") no-repeat top left transparent;\"/>";
                 }
 
                 obj.title = data.results[item]["sakai:pooled-content-file-name"];
@@ -149,11 +187,12 @@ require(["jquery", "sakai/sakai.api.core"], function($, sakai) {
                         }
                     }
                 }
-                obj.mimeType = data.results[item]["_mimeType"];
+                obj.mimeType = data.results[item]["_mimeType"] || "";
                 obj.created = sakai.api.l10n.transformDate(sakai.api.l10n.fromEpoch(data.results[item]["_created"]), sakai.data.me)
                 obj.createdBy = data.results[item]["sakai:pool-content-created-for"];
                 obj.lastModified = sakai.api.l10n.transformDate(sakai.api.l10n.fromEpoch(data.results[item]["_lastModified"]), sakai.data.me);
                 obj.lastModifiedBy = data.results[item]["_lastModifiedBy"];
+                obj.url = "/content#content_path=/p/" + data.results[item]["jcr:name"];
 
                 dataArr.push(obj);
             }
