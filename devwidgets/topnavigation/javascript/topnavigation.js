79a23b503a0d2191f22536f14a065e848dcd8a9b
Nicolaas Matthijs <nicolaas.matthijs@caret.cam.ac.uk>
2011-04-06 12:11:57 -0700

    SAKIII-2716 - Move search pages to new structure

--- devwidgets/topnavigation/javascript/topnavigation.js
+++ devwidgets/topnavigation/javascript/topnavigation.js
@@ -394,7 +394,9 @@ require(["jquery", "sakai/sakai.api.core"], function($, sakai) {
 
             $("#topnavigation_search_input").keyup(function(evt){
                 var val = $.trim($(this).val());
-                if (val && evt.keyCode != 16) {
+                if (evt.keyCode == 13) {
+                    document.location = "/dev/search2.html#q=" + val;
+                } else if (val && evt.keyCode != 16) {
                     doSearch(val);
                 }else if(!val){
                     $("#topnavigation_search_results").hide();
