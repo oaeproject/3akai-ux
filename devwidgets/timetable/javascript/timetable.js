70686de06762ad9e32a33cf9f2f4779ff83ea388
Ayrton De Craene <ayrton.decraene@gmail.com>
2011-04-18 14:25:47 +0100

    SAKIII-2759 further optimization

--- devwidgets/timetable/javascript/timetable.js
+++ devwidgets/timetable/javascript/timetable.js
@@ -39,6 +39,7 @@ require(["jquery", "sakai/sakai.api.core", "/dev/lib/jquery/jquery-ui-datepicker
      */
     sakai_global.timetable = function(tuid, showSettings){
     
+    
         /////////////////////////////
         // Configuration variables //
         /////////////////////////////
@@ -46,19 +47,14 @@ require(["jquery", "sakai/sakai.api.core", "/dev/lib/jquery/jquery-ui-datepicker
         var $rootel = $("#" + tuid);
         var timetableTemplateContainer = "timetable_main_template";
         
-        var $settingsContainer = $("#timetable_settings", $rootel);
         var $settingsForm = $("#timetable_settings_form", $rootel);
         
         var $addEventContainer = $("#new_event_entry", $rootel);
         var $addEventForm = $('form', $addEventContainer);
-        var $addEventFormDateInput = $('.n_e_date', $addEventForm);
-        var $addEventFormNuds = $('.n_e_time', $addEventForm);
         var $addEventFormSelect = $("select#n_e_type", $addEventForm);
         
         var $mainContainer = $("#timetable_main", $rootel);
-        var $addEventLinks;
-        
-        var $eventList, $eventInfo;
+        var $addEventLinks, $sortEventsList, $eventList, $eventInfo;
         
         var data = {};
         var startDate, endDate;
@@ -79,6 +75,11 @@ require(["jquery", "sakai/sakai.api.core", "/dev/lib/jquery/jquery-ui-datepicker
             hash.w.hide();
         };
         
+        /**
+         * Counts how many days are left untill an event is due.
+         *
+         * @param {Date} event_start_day
+         */
         var countDaysLeft = function(event_start_day){
             var today = new Date();
             var one_day = 1000 * 60 * 60 * 24;
@@ -86,8 +87,9 @@ require(["jquery", "sakai/sakai.api.core", "/dev/lib/jquery/jquery-ui-datepicker
         };
         
         /**
+         * Reload the template after we have the most updated data.
          *
-         * @param {Object} success
+         * @param {Boolean} success
          * @param {Object} loadedData
          */
         var hideAndRemoveOldEventsAndRenderTemplate = function(success, loadedData){
@@ -101,7 +103,8 @@ require(["jquery", "sakai/sakai.api.core", "/dev/lib/jquery/jquery-ui-datepicker
                         data.events.push(event);
                     }
                     else {
-                        //TODO delete the event
+                        // TODO delete past events, the reason this is not implemented yet is because we don't save
+                        // any data to an API, DB yet...
                     }
                 }
             }
@@ -117,7 +120,7 @@ require(["jquery", "sakai/sakai.api.core", "/dev/lib/jquery/jquery-ui-datepicker
             if ($eventInfo.height() < $eventList.height() - 63) {
                 $eventInfo.height($eventList.height() - 63);
             }
-            $(".sort_events_container select", $mainContainer).change(sortEventsChangeEvent).change();
+            $sortEventsList.change(sortEventsChangeEvent).change();
             
             
             $addEventLinks = $('a.add_event_link', $mainContainer);
@@ -129,11 +132,13 @@ require(["jquery", "sakai/sakai.api.core", "/dev/lib/jquery/jquery-ui-datepicker
         };
         
         /**
+         * Convert an event start and end date to a human readable string such as:
+         * Wednesday February 12, 2012, 14:00-15:00 or Wednesday February 12, 2012, 14:00 - Thursday February 12, 2012, 15:00
          *
          * @param {Time} start
          * @param {Time} end
          */
-        var convertTwoTimeObjectToAReadableString = function(start, end){
+        var convertToAHumanReadableString = function(start, end){
             // date
             var start_date = start.getDate();
             var start_month = start.getMonth();
@@ -176,10 +181,11 @@ require(["jquery", "sakai/sakai.api.core", "/dev/lib/jquery/jquery-ui-datepicker
         }
         
         /**
+         * Convert time into a 12 hour time format.
          *
          * @param {String} date
          */
-        var fromDateToHourInAMorPM = function(date){
+        var convertToHourInAMorPM = function(date){
             date = new Date(date);
             var hour = date.getHours();
             
@@ -204,16 +210,25 @@ require(["jquery", "sakai/sakai.api.core", "/dev/lib/jquery/jquery-ui-datepicker
             return hour + ap;
         }
         
+        
         ////////////////////
         // Event Handlers //
         ////////////////////  
         
+        /**
+         * Loads the widget once we submitted the settingsform.
+         *
+         * @param {Object} ev
+         */
         $settingsForm.bind("submit", function(ev){
             // Settings finished, switch to Main view
             sakai.api.Widgets.Container.informFinish(tuid, "timetable");
             return false;
         });
         
+        /**
+         * When we click an event, we want to show some more details.
+         */
         var eventListClickEvent = function(){
             if ($(this).hasClass('selected')) {
                 return;
@@ -230,13 +245,16 @@ require(["jquery", "sakai/sakai.api.core", "/dev/lib/jquery/jquery-ui-datepicker
                 if (e.id == id) {
                     //fill in details
                     $('#event_entry_info_details h3', $eventInfo).html(e.title);
-                    $('#event_entry_info_details strong', $eventInfo).html(fromDateToHourInAMorPM(e.start) + ' - ' + fromDateToHourInAMorPM(e.end));
+                    $('#event_entry_info_details strong', $eventInfo).html(convertToHourInAMorPM(e.start) + ' - ' + convertToHourInAMorPM(e.end));
                     $('#event_entry_info_notes p', $eventInfo).html(e.description);
                 }
             }
             $eventInfo.show();
         };
         
+        /**
+         * We want to sort events based on specific attributes. (e.g. IDs, start_date)
+         */
         var sortEventsChangeEvent = function(){
             var listitems = $eventList.children('li').get();
             var selectVal = $(this).val();
@@ -254,29 +272,27 @@ require(["jquery", "sakai/sakai.api.core", "/dev/lib/jquery/jquery-ui-datepicker
                         if (e.id == $(a).attr('data-id')) {
                             compA = e.start;
                         }
-                    }
-                    for (i = 0; i < data.events.length; i++) {
-                        var e = data.events[i];
+                        
                         if (e.id == $(b).attr('data-id')) {
                             compB = e.start;
                         }
                     }
                 }
                 return (compA < compB) ? -1 : (compA > compB) ? 1 : 0;
-            })
+            });
             $.each(listitems, function(idx, itm){
                 $eventList.append(itm);
             });
             
-            // if the first event was selected and it's on a different place now, the box should have a curved border on the top left
+            // If the first event was selected and it's on a different place now, the box should have a curved border on the top left.
+            // So we assign the index of the selected list item again.
             $eventInfo.removeClass();
             $eventInfo.addClass('selected_index_' + $eventList.find('li.selected').index());
         };
         
-        /////////////////////////////
-        // Initialization function //
-        /////////////////////////////       
-        
+        /**
+         * Create an event, add it to the existing events and re-render the template.
+         */
         var doCreateEvent = function(){
             var event = {
                 'id': data.events.length, // temp created my own id system
@@ -286,7 +302,7 @@ require(["jquery", "sakai/sakai.api.core", "/dev/lib/jquery/jquery-ui-datepicker
                 'days_left': countDaysLeft(startDate),
                 'description': $.trim($('#n_e_description', $addEventForm).val())
             };
-            event.readableTime = convertTwoTimeObjectToAReadableString(event.start, event.end);
+            event.readableTime = convertToAHumanReadableString(event.start, event.end);
             data.events.push(event);
             
             sakai.api.Widgets.saveWidgetData(tuid, data, function(success){
@@ -295,14 +311,22 @@ require(["jquery", "sakai/sakai.api.core", "/dev/lib/jquery/jquery-ui-datepicker
                         hideAndRemoveOldEventsAndRenderTemplate(success, loadedData);
                     });
                 }
-                else {
-                    alert('something went wrong saving the date');
-                }
             });
+            
+            // Hide this modal view and reset each field.            
             $addEventContainer.jqmHide();
-            $('input, textarea', $addEventContainer).not('.n_e_time').val('');
+            $('input, textarea', $addEventContainer).val('');
+            $('input.n_e_time', $addEventContainer).val('12:00');
         };
         
+        
+        /////////////////////////////
+        // Initialization function //
+        /////////////////////////////  
+        
+        /**
+         * Set a width to the list to make it as wide as possible without having a horizontal scrollbar.
+         */
         var doApplyWidthToList = function(){
             var fullWidth = $('#timetable_main', $rootel).width();
             var detailsWidth = $('#event_entry_info', $rootel).outerWidth();
@@ -312,11 +336,17 @@ require(["jquery", "sakai/sakai.api.core", "/dev/lib/jquery/jquery-ui-datepicker
             $('#event_entries hr', $rootel).width(listWidth - 16);
         };
         
+        /**
+         * Retreive the start and end date from the input fields into a global variable.
+         */
         var initialiseStartAndEndDate = function(){
             startDate = new Date($('.n_e_date', '#new_event_starts').val() + ' ' + $('.n_e_time', '#new_event_starts').val());
             endDate = new Date($('.n_e_date', '#new_event_ends').val() + ' ' + $('.n_e_time', '#new_event_ends').val());
-        }
+        };
         
+        /**
+         * Validation rules for the add event form.
+         */
         var doValidateAddEventForm = function(){
             $.validator.addMethod("n_e_time", function(value, element){
                 return this.optional(element) || /^([0-1][0-9]|[2][0-3]):([0-5][0-9])$/.test(value);
@@ -345,24 +375,29 @@ require(["jquery", "sakai/sakai.api.core", "/dev/lib/jquery/jquery-ui-datepicker
                 errorPlacement: function(){
                 }
             });
-        }
+        };
         
+        /**
+         * We'll setup a datetimepicker using the jQuery UI component but also create a NumericUpDown component.
+         */
         var doSetupDateAndTimePicker = function(){
-            // datepicker               
-            $addEventFormDateInput.datepicker({
+            // Datepicker part.
+            var $dateInputFields = $('.n_e_date', $addEventForm);
+            $dateInputFields.datepicker({
                 dateFormat: 'mm/dd/yy',
                 hideIfNoPrevNext: true,
                 showOtherMonths: true
             });
             
-            // timepicker
-            $addEventFormNuds.each(function(){
+            // Timepicker part.
+            var $timeInputFields = $('.n_e_time', $rootel);
+            $timeInputFields.each(function(){
                 var $timeinput = $(this);
                 
-                // add UI elements
+                // Here we add the necessary UI elements to create a NUD-component.
                 $timeinput.after('<div class="ayrton_nud"><a class="ayrton_nud_u ayrton_nud_a">up</a><a class="ayrton_nud_d ayrton_nud_a">down</a></div>');
                 
-                // clickhandler
+                // Clickhandler.
                 var mininterval = 15;
                 $timeinput.parent().find('.ayrton_nud_a').click(function(){
                     var tval = $timeinput.val();
@@ -371,6 +406,7 @@ require(["jquery", "sakai/sakai.api.core", "/dev/lib/jquery/jquery-ui-datepicker
                     var tm = parseInt(tval.split(':')[1], 10);
                     
                     if ($(this).hasClass('ayrton_nud_u')) {
+                        // Increase the minute value with mininterval.
                         tm += mininterval;
                         if (tm >= 60) {
                             th += 1;
@@ -383,6 +419,7 @@ require(["jquery", "sakai/sakai.api.core", "/dev/lib/jquery/jquery-ui-datepicker
                     }
                     else 
                         if ($(this).hasClass('ayrton_nud_d')) {
+                            // Decrease the minute value with mininterval.
                             tm -= mininterval;
                             if (tm < 0) {
                                 th -= 1;
@@ -393,11 +430,8 @@ require(["jquery", "sakai/sakai.api.core", "/dev/lib/jquery/jquery-ui-datepicker
                                 th = 23;
                             }
                         }
-                        else {
-                            return;
-                        }
                     
-                    // make sure there's a 0x prefix
+                    // Make sure there's a 0x prefix.
                     th += '';
                     tm += '';
                     if (th.length < 2) {
@@ -406,14 +440,16 @@ require(["jquery", "sakai/sakai.api.core", "/dev/lib/jquery/jquery-ui-datepicker
                     if (tm.length < 2) {
                         tm = '0' + tm;
                     }
+                    // Update the value of the input field.
                     $timeinput.val(th + ':' + tm);
-                    
                 });
             });
         };
         
+        /**
+         * Filling in event types into the dropdown.
+         */
         var doFillInEventTypes = function(){
-            // filling in all event types in the dropdown
             var event_types_arr = ['Lecture', 'Seminar'], event_types_options = '';
             for (var i = 0; i < event_types_arr.length; i++) {
                 event_types_options += '<option value="' + event_types_arr[i] + '">' + event_types_arr[i] + '</option>';
@@ -421,15 +457,18 @@ require(["jquery", "sakai/sakai.api.core", "/dev/lib/jquery/jquery-ui-datepicker
             $addEventFormSelect.html(event_types_options);
         };
         
-        
+        /**
+         * Initialize and setup the widget.
+         */
         var doInit = function(){
             if (showSettings) {
+                var $settingsContainer = $("#timetable_settings", $rootel);
                 $settingsContainer.show();
             }
             else {
-                // Load the widget data
+                // Load the widget data.
                 sakai.api.Widgets.loadWidgetData(tuid, function(success, loadedData){
-                    // setting up the Add Event modal screen
+                    // Setting up the Add Event modal screen.
                     $addEventContainer.jqm({
                         modal: true,
                         overlay: 20,
@@ -441,10 +480,9 @@ require(["jquery", "sakai/sakai.api.core", "/dev/lib/jquery/jquery-ui-datepicker
                     
                     data.isManager = sakai_global.currentgroup.manager;
                     hideAndRemoveOldEventsAndRenderTemplate(success, loadedData);
-                    
                     doApplyWidthToList();
                     
-                    // add event stuff
+                    // Setup everything to add events properly.
                     doFillInEventTypes();
                     doSetupDateAndTimePicker();
                     doValidateAddEventForm();
