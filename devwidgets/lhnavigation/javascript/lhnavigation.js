018e4a7697a0447c5cd5304dd82dccef794569b6
Nicolaas Matthijs <nicolaas.matthijs@caret.cam.ac.uk>
2011-04-19 11:55:38 +0100

    NOJIRA - Temporarily hiding the selected arrow

--- devwidgets/lhnavigation/javascript/lhnavigation.js
+++ devwidgets/lhnavigation/javascript/lhnavigation.js
@@ -116,11 +116,11 @@ require(["jquery", "sakai/sakai.api.core"], function($, sakai) {
 
             $prevItem.removeClass(navSelectedItemClass);
             $prevItem.addClass(navHoverableItemClass);
-            $prevItem.children(navSelectedItemArrow).css("visibility","hidden");
+            //$prevItem.children(navSelectedItemArrow).css("visibility","hidden");
 
             $clickedItem.removeClass(navHoverableItemClass);
             $clickedItem.addClass(navSelectedItemClass);
-            $clickedItem.children(navSelectedItemArrow).css("visibility","visible");
+            //$clickedItem.children(navSelectedItemArrow).css("visibility","visible");
 
             showHideSubnav($clickedItem);
 
