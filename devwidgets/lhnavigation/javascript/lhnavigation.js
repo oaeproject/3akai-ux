1a803dc4a665403e1da66d3b58837a5658c3438e
Michael Manning <michaellise@gmail.com>
2011-05-19 21:14:18 -0700

    SAKIII-2963 stopped error from jcarousel by toggling start/stopAuto

--- devwidgets/lhnavigation/javascript/lhnavigation.js
+++ devwidgets/lhnavigation/javascript/lhnavigation.js
@@ -317,6 +317,10 @@ require(["jquery", "sakai/sakai.api.core"], function($, sakai) {
         var selectPage = function(newPageMode){
             var state = $.bbq.getState("l");
             var selected = state || false;
+            var jcarousel = $('#carousel_container');
+            if(jcarousel.data('jcarousel')){
+                jcarousel.data('jcarousel')[(state==="dashboard"?"start":"stop") + "Auto"]();
+            }
             // If no page is selected, select the first one from the nav
             if (!selected){
                 selected = getFirstSelectablePage(privstructure) || getFirstSelectablePage(pubstructure);
