fe3d141ebe9acf512566c734ec10695186ed64ed
Bert Pareyn <bp323@caret.cam.ac.uk>
2011-05-23 13:45:21 +0100

    SAKIII-2939 Add copyright to add content widget

--- devwidgets/newaddcontent/javascript/newaddcontent.js
+++ devwidgets/newaddcontent/javascript/newaddcontent.js
@@ -253,7 +253,7 @@ require(["jquery", "/dev/configuration/sakaidoc.js", "sakai/sakai.api.core"], fu
                         "description": $contentForm.find(newaddcontentUploadContentDescription).val(),
                         "tags": $contentForm.find(newaddcontentUploadContentTags).val(),
                         "permissions": $contentForm.find(newaddcontentUploadContentPermissions).val(),
-                        "copyright": "creativecommons",
+                        "copyright": $("#newaddcontent_upload_content_copyright").val(),
                         "css_class": sakai.config.MimeTypes[sakai.config.Extensions[(originalTitle).slice(originalTitle.lastIndexOf(".") + 1, originalTitle.length).toLowerCase()] || "other"].cssClass || "icon-unknown-sprite",
                         "type": "content"
                     };
@@ -704,6 +704,10 @@ require(["jquery", "/dev/configuration/sakaidoc.js", "sakai/sakai.api.core"], fu
                     enableAddToQueue();
                 }
             });
+            $("#newaddcontent_upload_content_copyright_container").html(sakai.api.Util.TemplateRenderer("newaddcontent_copyright_template", {
+                copyright: sakai.config.Permissions.Copyright,
+                sakai: sakai
+            }));
         };
 
         /**
