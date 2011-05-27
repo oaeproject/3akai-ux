cb300564cf0d821dd2dd5fecefcca330eebf05a3
Steven Githens <swgithen@mtu.edu>
2011-05-27 16:09:46 -0400

    SAKIII-2992 Changing == to ===

--- devwidgets/topnavigation/javascript/topnavigation.js
+++ devwidgets/topnavigation/javascript/topnavigation.js
@@ -526,7 +526,7 @@ require(["jquery", "sakai/sakai.api.core"], function($, sakai) {
             // Make up for IE8 to submit login on Enter
             // http://www.thefutureoftheweb.com/blog/submit-a-form-in-ie-with-enter
             $(topnavUserOptionsLoginForm + " input").keydown(function(e){
-                if (e.keyCode == 13) {
+                if (e.keyCode === 13) {
                     $(this).parents('form').submit();
                     return false;
                 }
