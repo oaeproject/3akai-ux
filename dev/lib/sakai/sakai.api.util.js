c8aa0d4ae346a6a369f557da8c7a077dd97a9069
Chris Roby <christopher.roby@gmail.com>
2011-06-01 14:33:06 -0700

    SAKIII-2839 Changing the polling to check /system/me instead of a static file, due to caching considerations and alternative-tab logout scenarios.

--- dev/lib/sakai/sakai.api.util.js
+++ dev/lib/sakai/sakai.api.util.js
@@ -70,7 +70,13 @@ define(["jquery",
             if (meData.user.userid) {
                 setInterval(function() {
                     $.ajax({
-                        url: "/dev/keepalive.txt"
+                        url: "/system/me",
+                        cache: false,
+                        success: function(data) {
+                            if (!data.user.userid) {
+                                document.location = "/";
+                            }
+                        }
                     });
                 }, 60000);
             }
