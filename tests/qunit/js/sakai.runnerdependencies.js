15c329aa146f9ca2c4b23ef980e81b6d2fd70788
Chris Roby <christopher.roby@gmail.com>
2011-07-05 15:25:30 -0700

    NOJIRA updating paths to work with release build - consolodated some paths, made everything easier to read

--- tests/qunit/js/sakai.runnerdependencies.js
+++ tests/qunit/js/sakai.runnerdependencies.js
@@ -1,21 +1,22 @@
 require({
     baseUrl:"../../dev/lib/",
     paths: {
+        "jquery-plugins": "jquery/plugins",
         "jquery": "jquery/jquery-1.5.2",
-        "jquery-ui": "jquery/jquery-ui-1.8.13.custom"
+        "jquery-ui": "jquery/jquery-ui-1.8.13.custom",
+        "config": "../configuration",
+        "mockjax": "../../tests/qunit/js/jquery.mockjax",
+        "qunitjs": "../../tests/qunit/js"
     },
     priority: ["jquery"]
 });
 
 require(
-    {
-        baseUrl: "../../dev/lib/"
-    },
     [
         "jquery",
         "sakai/sakai.api.core",
         "jquery-ui",
-        "../../tests/qunit/js/qunit.js"
+        "qunitjs/qunit"
     ],
     function($, sakai) {
         require.ready(function() {
