4622b151125b299c671cc041cd0f561f129176aa
Chris Roby <christopher.roby@gmail.com>
2011-07-12 14:05:49 -0700

    SAKIII-3454 Don't show the current group in the addpeople dialog - filtering was broken

--- devwidgets/addpeople/javascript/addpeople.js
+++ devwidgets/addpeople/javascript/addpeople.js
@@ -80,7 +80,9 @@ require(["jquery", "sakai/sakai.api.core"], function($, sakai) {
                 var groups = sakai.api.Groups.getMemberships(sakai.data.me.groups);
                 groups = groups.entry;
                 if (sakai_global.group && sakai_global.group.groupData && sakai_global.group.groupData["sakai:group-id"]) {
-                    groups = _.without(groups, sakai_global.group.groupData["sakai:group-id"]);
+                    groups = _.reject(groups, function(group) {
+                        return group["sakai:group-id"] === sakai_global.group.groupData["sakai:group-id"];
+                    });
                 }
                 $addpeopleContactsContainer.html(sakai.api.Util.TemplateRenderer(addpeopleContactsTemplate, {
                     "contacts": sakai.data.me.mycontacts,
