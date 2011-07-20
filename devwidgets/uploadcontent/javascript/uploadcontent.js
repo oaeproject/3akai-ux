ab5baea3c529a884f886a76ec47449cfea76de10
Steven Githens <swgithen@mtu.edu>
2011-07-20 11:38:02 -0400

    SAKIII-3376 Updating raw javascript files to use encodeURIComponentI18n.

--- devwidgets/uploadcontent/javascript/uploadcontent.js
+++ devwidgets/uploadcontent/javascript/uploadcontent.js
@@ -161,7 +161,7 @@ require(["jquery", "sakai/sakai.api.core"], function($, sakai) {
                 success: function(data){
 
                     // Redirect to the profile page of the newly updated link
-                    document.location = "/content#p=" + encodeURIComponent(this.url);
+                    document.location = "/content#p=" + sakai.api.Util.encodeURIComponentI18n(this.url);
 
                     // Reset the current form
                     resetForm();
