3c1963aa6e8554eac3b68a2459200e6506d545e7
Steven Githens <swgithen@mtu.edu>
2011-07-27 11:17:09 -0400

    SAKIII-2251 Adding Sakai 2 Search Widget, and adding conditionally to Search Page. (if sakai2 is available and you're not anonymous)

--- dev/javascript/search.js
+++ dev/javascript/search.js
@@ -19,7 +19,7 @@
 require(["jquery","sakai/sakai.api.core"], function($, sakai) {
 
     sakai_global.search = function() {
-
+        var worldsOrderIncrement = 3;
         var pubdata = {
             "structure0": {
                 "all": {
@@ -63,6 +63,24 @@ require(["jquery","sakai/sakai.api.core"], function($, sakai) {
                 "page": "<div id='widget_searchpeople' class='widget_inline'></div>"
             }
         };
+
+        if (sakai.config.showSakai2 === true && !sakai.data.me.user.anon) {
+            // Shift worlds down the side menu by 1 to put in Sakai 2 Search            
+            worldsOrderIncrement++;
+            pubdata.structure0["sakai2sites"] = {
+                "_title": "Sakai 2",
+                "_ref": "id1234567890",
+                "_order": 3,
+                "main": {
+                    "_ref": "id1234567890",
+                    "_order": 0,
+                    "_title": "Sakai 2"
+                }
+            };
+            pubdata["id1234567890"] = {
+                "page": "<div id='widget_searchsakai2' class='widget_inline'></div>"
+            };
+        }
         
         for (var c = 0; c < sakai.config.worldTemplates.length; c++) {
             var category = sakai.config.worldTemplates[c];
@@ -71,7 +89,7 @@ require(["jquery","sakai/sakai.api.core"], function($, sakai) {
             pubdata.structure0[category.id] = {
                 "_title": title,
                 "_ref": refId,
-                "_order": (c + 3),
+                "_order": (c + worldsOrderIncrement),
                 "main": {
                     "_ref": refId,
                     "_order": 0,
