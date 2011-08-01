45945035cef6208741052fa9a95b82fa0653e5d3
Bert Pareyn <bp323@caret.cam.ac.uk>
2011-08-01 18:10:57 +0100

    SAKIII-3453 Add ACLs to user pages to allow users to limit visibility of their profile

--- devwidgets/userpermissions/javascript/userpermissions.js
+++ devwidgets/userpermissions/javascript/userpermissions.js
@@ -15,9 +15,8 @@
  * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-
 // load the master sakai object to access all Sakai OAE API methods
-require(["jquery", "sakai/sakai.api.core"], function($, sakai) {
+require(["jquery", "sakai/sakai.api.core"], function($, sakai){
 
     /**
      * @name sakai.userpermissions
@@ -28,90 +27,277 @@ require(["jquery", "sakai/sakai.api.core"], function($, sakai) {
      * @param {String} tuid Unique id of the widget
      * @param {Boolean} showSettings Show the settings of the widget or not
      */
-    sakai_global.userpermissions = function (tuid, showSettings) {
+    sakai_global.userpermissions = function(tuid, showSettings){
 
-         var contextData = false;
-         
-         ///////////////////////////////////////
-         // Retrieving the current permission //
-         ///////////////////////////////////////
-         
-         var getCurrentPermission = function(){
-             var currentPath = contextData.path;
-             var page = false;
-             if (currentPath.indexOf("/") !== -1){
-                 var split = currentPath.split("/");
-                 page = sakai_global.user.pubdata.structure0[split[0]][split[1]];
-             } else {
-                 page = sakai_global.user.pubdata.structure0[currentPath];
-             }
-             var permission = page._view;
-             $("#userpermissions_area_title").text(sakai.api.i18n.General.process(page._title, sakai.api.User.data.me));
-             $("#userpermissions_content_container").html(sakai.api.Util.TemplateRenderer("userpermissions_content_template", {
-                 "permission": permission
-             }));
-         };
+        var contextData = false;
 
-         /////////////////////////////
-         // Storing new permissions //
-         /////////////////////////////
+        ///////////////////////////////////////
+        // Retrieving the current permission //
+        ///////////////////////////////////////
 
-         var applyPermissions = function(){             
-             var currentPath = contextData.path;
-             var page = false;
-             if (currentPath.indexOf("/") !== -1){
-                 var split = currentPath.split("/");
-                 page = sakai_global.user.pubdata.structure0[split[0]][split[1]];
-             } else {
-                 page = sakai_global.user.pubdata.structure0[currentPath];
-             }
+        var getCurrentPermission = function(){
+            var currentPath = contextData.path;
+            var page = false;
+            if (currentPath.indexOf("/") !== -1) {
+                var split = currentPath.split("/");
+                page = sakai_global.user.pubdata.structure0[split[0]][split[1]];
+            }
+            else {
+                page = sakai_global.user.pubdata.structure0[currentPath];
+            }
+            var permission = page._view;
+            $("#userpermissions_area_title").text(sakai.api.i18n.General.process(page._title, sakai.api.User.data.me));
+            $("#userpermissions_content_container").html(sakai.api.Util.TemplateRenderer("userpermissions_content_template", {
+                "permission": permission
+            }));
+        };
 
-             // Collect selected permission
-             var permission = $("#userpermissions_area_general_visibility").val();
-             page._view = permission
+        /////////////////////////////
+        // Storing new permissions //
+        /////////////////////////////
 
-             sakai.api.Server.saveJSON("/~" + sakai.data.me.user.userid + "/public/pubspace", {
-                 "structure0": $.toJSON(sakai_global.user.pubdata.structure0)
-             });
-             $("#userpermissions_container").jqmHide();
+        /**
+         * Notify the user that the permissions have been changed or an error has occurred
+         * @param {Boolean} success Indicates the success or failure of setting the permissions
+         */
+        var permissionsSet = function(success){
+            if (success) {
+                // Hide the dialog
+                $("#userpermissions_container").jqmHide();
+                // Show gritter notification
+                sakai.api.Util.notification.show($("#userpermissions_notification_title").text(), $("#userpermissions_notification_body").text());
+            }else{
+                // Show an error notification
+                sakai.api.Util.notification.show($("#userpermissions_notification_title").text(), $("#userpermissions_notification_error_body").text(), sakai.api.Util.notification.type.ERROR);
+            }
+        };
 
-             // Show gritter notification
-             sakai.api.Util.notification.show($("#userpermissions_notification_title").text(), $("#userpermissions_notification_body").text());
+        /**
+         * Sets ACLs on a profile section
+         * @param {String} pageRef ID of the page to be updated
+         * @param {String} permission anonymous, everyone, contacts or private determining what ACLs need to be set
+         */
+        var setACLs = function(pageRef, permission){
+            var path = "/~" + sakai.data.me.user.userid + "/public/authprofile/" + pageRef;
+            var ACLs = [];
+            switch (permission) {
+                case "anonymous":
+                    ACLs.push({
+                        "url": path + ".modifyAce.html",
+                        "method": "POST",
+                        "parameters": {
+                            "principalId": ["everyone"],
+                            "privilege@jcr:read": "granted"
+                        }
+                    });
+                    ACLs.push({
+                        "url": path + ".modifyAce.html",
+                        "method": "POST",
+                        "parameters": {
+                            "principalId": ["anonymous"],
+                            "privilege@jcr:read": "granted"
+                        }
+                    });
+                    ACLs.push({
+                        "url": path + ".modifyAce.html",
+                        "method": "POST",
+                        "parameters": {
+                            "principalId": ["g-contacts-" + sakai.data.me.user.userid],
+                            "privilege@jcr:read": "granted"
+                        }
+                    });
+                    break;
+                case "everyone":
+                    ACLs.push({
+                        "url": path + ".modifyAce.html",
+                        "method": "POST",
+                        "parameters": {
+                            "principalId": ["g-contacts-" + sakai.data.me.user.userid],
+                            "privilege@jcr:read": "granted"
+                        }
+                    });
+                    ACLs.push({
+                        "url": path + ".modifyAce.html",
+                        "method": "POST",
+                        "parameters": {
+                            "principalId": ["everyone"],
+                            "privilege@jcr:read": "granted"
+                        }
+                    });
+                    ACLs.push({
+                        "url": path + ".modifyAce.html",
+                        "method": "POST",
+                        "parameters": {
+                            "principalId": ["anonymous"],
+                            "privilege@jcr:read": "denied"
+                        }
+                    });
+                    break;
+                case "contacts":
+                    ACLs.push({
+                        "url": path + ".modifyAce.html",
+                        "method": "POST",
+                        "parameters": {
+                            "principalId": [sakai.data.me.user.userid],
+                            "privilege@jcr:write": "granted"
+                        }
+                    });
+                    ACLs.push({
+                        "url": path + ".modifyAce.html",
+                        "method": "POST",
+                        "parameters": {
+                            "principalId": [sakai.data.me.user.userid],
+                            "privilege@jcr:read": "granted"
+                        }
+                    });
+                    ACLs.push({
+                        "url": path + ".modifyAce.html",
+                        "method": "POST",
+                        "parameters": {
+                            "principalId": ["g-contacts-" + sakai.data.me.user.userid],
+                            "privilege@jcr:read": "granted"
+                        }
+                    });
+                    ACLs.push({
+                        "url": path + ".modifyAce.html",
+                        "method": "POST",
+                        "parameters": {
+                            "principalId": ["everyone"],
+                            "privilege@jcr:read": "denied"
+                        }
+                    });
+                    ACLs.push({
+                        "url": path + ".modifyAce.html",
+                        "method": "POST",
+                        "parameters": {
+                            "principalId": ["anonymous"],
+                            "privilege@jcr:read": "denied"
+                        }
+                    });
+                    break;
+                case "private":
+                    ACLs.push({
+                        "url": path + ".modifyAce.html",
+                        "method": "POST",
+                        "parameters": {
+                            "principalId": [sakai.data.me.user.userid],
+                            "privilege@jcr:write": "granted"
+                        }
+                    });
+                    ACLs.push({
+                        "url": path + ".modifyAce.html",
+                        "method": "POST",
+                        "parameters": {
+                            "principalId": [sakai.data.me.user.userid],
+                            "privilege@jcr:read": "granted"
+                        }
+                    });
+                    ACLs.push({
+                        "url": path + ".modifyAce.html",
+                        "method": "POST",
+                        "parameters": {
+                            "principalId": ["g-contacts-" + sakai.data.me.user.userid],
+                            "privilege@jcr:read": "denied"
+                        }
+                    });
+                    ACLs.push({
+                        "url": path + ".modifyAce.html",
+                        "method": "POST",
+                        "parameters": {
+                            "principalId": ["everyone"],
+                            "privilege@jcr:read": "denied"
+                        }
+                    });
+                    ACLs.push({
+                        "url": path + ".modifyAce.html",
+                        "method": "POST",
+                        "parameters": {
+                            "principalId": ["anonymous"],
+                            "privilege@jcr:read": "denied"
+                        }
+                    });
+                    break;
+            }
 
-         }
+            $.ajax({
+                url: sakai.config.URL.BATCH,
+                traditional: true,
+                type: "POST",
+                cache: false,
+                data: {
+                    requests: $.toJSON(ACLs)
+                },
+                success: function(data){
+                    permissionsSet(true);
+                },
+                error: function(xhr, textStatus, thrownError){
+                    debug.err(xhr, textStatus, thrownError);
+                    permissionsSet(false);
+                }
+            });
+        };
 
-         /////////////////////////////////
-         // Modal dialog initialization //
-         /////////////////////////////////
+        /**
+         * Apply the selected permissions to the page
+         */
+        var applyPermissions = function(){
+            var currentPath = contextData.path;
+            var page = false;
+            if (currentPath.indexOf("/") !== -1) {
+                var split = currentPath.split("/");
+                page = sakai_global.user.pubdata.structure0[split[0]][split[1]];
+            }
+            else {
+                page = sakai_global.user.pubdata.structure0[currentPath];
+            }
 
-         var initializeOverlay = function(){
-             $("#userpermissions_container").jqmShow();
-         };
+            // Collect selected permission
+            var permission = $("#userpermissions_area_general_visibility").val();
+            page._view = permission;
 
-         $("#userpermissions_container").jqm({
-             modal: true,
-             overlay: 20,
-             toTop: true,
-             zIndex: 3000
-         });
+            sakai.api.Server.saveJSON("/~" + sakai.data.me.user.userid + "/public/pubspace", {
+                "structure0": $.toJSON(sakai_global.user.pubdata.structure0)
+            });
 
-         /////////////////////
-         // Internal events //
-         /////////////////////
+            if (currentPath !== "library" && currentPath !== "memberships" && currentPath !== "contacts") {
+                setACLs(split[1], permission.toString());
+            } else {
+                permissionsSet(true);
+            }
+        };
 
-         $("#userpermissions_apply_permissions").live("click", function(){
-             applyPermissions();
-         });
+        /////////////////////////////////
+        // Modal dialog initialization //
+        /////////////////////////////////
 
-         /////////////////////
-         // External events //
-         /////////////////////
+        var initializeOverlay = function(){
+            $("#userpermissions_container").jqmShow();
+        };
 
-         $(window).bind("permissions.area.trigger", function(ev, _contextData){
-             contextData = _contextData
-             getCurrentPermission();
-             initializeOverlay();
-         });
+        $("#userpermissions_container").jqm({
+            modal: true,
+            overlay: 20,
+            toTop: true,
+            zIndex: 3000
+        });
+
+        /////////////////////
+        // Internal events //
+        /////////////////////
+
+        $("#userpermissions_apply_permissions").live("click", function(){
+            applyPermissions();
+        });
+
+        /////////////////////
+        // External events //
+        /////////////////////
+
+        $(window).bind("permissions.area.trigger", function(ev, _contextData){
+            contextData = _contextData
+            getCurrentPermission();
+            initializeOverlay();
+        });
 
     };
 
