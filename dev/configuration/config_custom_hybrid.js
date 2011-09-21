06ab928feb86d3119938522775f7d82bb3067f2a
Nicolaas Matthijs <nicolaas.matthijs@caret.cam.ac.uk>
2011-09-21 10:41:27 +0100

    SAKIII-4028 - Creating a new user in OAE automatically is associated with CLE user. CLE pages do not display

---
+++ dev/configuration/config_custom_hybrid.js
@@ -0,0 +1,11 @@
+define(["config/config"], function(config) {
+
+    // Custom CSS Files to load in
+    config.skinCSS = ["/dev/skins/default/skin.css"];
+
+    // Hybrid
+    config.showSakai2 = true;
+    config.useLiveSakai2Feeds = true;
+
+    return config;
+});
