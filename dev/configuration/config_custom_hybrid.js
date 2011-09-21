b75368317cb51b33f02b2abd54061bd5a5873f99
Nicolaas Matthijs <nicolaas.matthijs@caret.cam.ac.uk>
2011-09-21 11:05:08 +0100

    SAKIII-4026 - My Sakai 2 favourites is not on dashboard by default for new users

--- dev/configuration/config_custom_hybrid.js
+++ dev/configuration/config_custom_hybrid.js
@@ -7,5 +7,11 @@ define(["config/config"], function(config) {
     config.showSakai2 = true;
     config.useLiveSakai2Feeds = true;
 
+    config.defaultprivstructure["${refid}5"].dashboard.columns.column1.push({
+        "uid": "${refid}1234",
+        "visible": "block",
+        "name": "mysakai2"
+    });
+
     return config;
 });
