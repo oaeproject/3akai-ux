b75ae543a2ded7f1e512bb1b5f8c03f5fc411e8f
Chris Roby <christopher.roby@gmail.com>
2011-11-07 16:00:17 -0800

    SAKIII-4138 Making a little mod to jquery.MultiFile so it doesn't append a class to every form on the page

--- dev/lib/sakai/sakai.dependencies.js
+++ dev/lib/sakai/sakai.dependencies.js
@@ -126,7 +126,7 @@ require(
         "jquery-plugins/jquery.threedots",
         "jquery-plugins/jquery.form",
         "jquery-plugins/jquery.fileupload",
-        "jquery-plugins/jquery.MultiFile",
+        "jquery-plugins/jquery.MultiFile.sakai-edited",
         "jquery-plugins/jsTree/jquery.jstree.sakai-edit",
         "jquery-plugins/gritter/jquery.gritter.sakai-edit",
         "jquery-plugins/jquery.jcarousel.sakai-edit"
