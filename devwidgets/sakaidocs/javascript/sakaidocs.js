aaba9cb73f3d3dfc858d5c331e741abaf63caa63
Chris Roby <christopher.roby@gmail.com>
2011-11-10 10:48:26 -0800

    SAKIII-4265 Fix the widget loading in sakaidocs.js to pass through widgetdata after a save

--- devwidgets/sakaidocs/javascript/sakaidocs.js
+++ devwidgets/sakaidocs/javascript/sakaidocs.js
@@ -639,7 +639,7 @@ require(["jquery", "sakai/sakai.api.core"], function($, sakai) {
                         }));
                     }
                     // Insert widgets
-                    sakai.api.Widgets.widgetLoader.insertWidgets(currentPageShown.ref, false, currentPageShown.pageSavePath + "/", null, {currentPageShown:currentPageShown});
+                    sakai.api.Widgets.widgetLoader.insertWidgets(currentPageShown.ref, false, currentPageShown.pageSavePath + "/", currentPageShown.widgetData, {currentPageShown:currentPageShown});
                     // Render Math formulas in the text
                     sakai.api.Util.renderMath(currentPageShown.ref);
                     $contentEl.show();
