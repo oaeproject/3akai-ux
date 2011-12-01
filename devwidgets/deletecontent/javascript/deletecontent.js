9d393db66b22f25e095a6051e116820a9eedd62f
Chris Roby <christopher.roby@gmail.com>
2011-11-30 23:41:43 -0800

    SAKIII-4398 Switching the order of the buttons * Also fixed display of pseudo-groups in the overlay, and one jslint warning

--- devwidgets/deletecontent/javascript/deletecontent.js
+++ devwidgets/deletecontent/javascript/deletecontent.js
@@ -171,10 +171,10 @@ require(["jquery", "sakai/sakai.api.core"], function($, sakai) {
                 }
                 var viewers = contentIManage[m]["sakai:pooled-content-viewer"];
                 if (viewers){
-                    for (var i = 0; i < viewers.length; i++){
-                        if ($.inArray(viewers[i], userGroupIds) === -1 && viewers[i] !== sakai.data.me.user.userid &&
-                            viewers[i] !== context && viewers[i] !== "everyone" && viewers[i] !== "anonymous"){
-                            userGroupIds.push(viewers[i]);
+                    for (var j = 0; j < viewers.length; j++){
+                        if ($.inArray(viewers[j], userGroupIds) === -1 && viewers[j] !== sakai.data.me.user.userid &&
+                            viewers[j] !== context && viewers[j] !== "everyone" && viewers[j] !== "anonymous"){
+                            userGroupIds.push(viewers[j]);
                         }
                     }
                 }
@@ -223,6 +223,7 @@ require(["jquery", "sakai/sakai.api.core"], function($, sakai) {
                         var profile = $.parseJSON(data.results[i].body);
                         if (profile["sakai:excludeSearch"] === "true"){
                             var splitOnDash = profile.groupid.split("-");
+                            profile.title = profile["sakai:parent-group-title"] + " (" + sakai.api.i18n.getValueForKey(profile["sakai:role-title-plural"]) + ")";
                             profile.groupid = splitOnDash.splice(0, splitOnDash.length - 1).join("-");
                         }
                         profileInfo.push(profile);
