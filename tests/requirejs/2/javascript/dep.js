b677bc91e91dc872098ae2a87d6a6c8ae154d2aa
Chris Roby <christopher.roby@gmail.com>
2011-11-30 18:02:31 -0800

    SAKIII-4137 Removing unused code in tests/

--- tests/requirejs/2/javascript/dep.js
+++
@@ -1,9 +0,0 @@
-require([
-    "jquery",
-    "/tests/requirejs/2/javascript/core.js"], function() {
-        console.log("here");
-        require(["misc/domReady!"], function(doc){
-            console.log("ready");
-        });
-    }
-);
