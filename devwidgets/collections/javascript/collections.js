0379ff70cd0382d0fc13d1aec8885c4a33fd4ca6
Bert Pareyn <bp323@caret.cam.ac.uk>
2011-12-08 18:17:36 +0000

    SAKIII-4465 Add tabindex to description container to focus on when collections widget is opened

--- devwidgets/collections/javascript/collections.js
+++ devwidgets/collections/javascript/collections.js
@@ -373,6 +373,7 @@ require(["jquery", "sakai/sakai.api.core"], function($, sakai) {
             }, 400, function(){
                 if ($collectionsWidget.is(":visible")){
                     getCollections();
+                    $("#collections_leftcolumn").focus();
                 } else {
                     $(collectionsScrollArrow).hide();
                     $(collectionsCollectionsList).hide();
