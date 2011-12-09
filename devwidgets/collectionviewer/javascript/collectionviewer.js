2a6cb51ee167b7543d338461343ffc35cb39540d
Nicolaas Matthijs <nicolaas.matthijs@caret.cam.ac.uk>
2011-12-09 14:36:49 +0000

    NOJIRA - Small fix

--- devwidgets/collectionviewer/javascript/collectionviewer.js
+++ devwidgets/collectionviewer/javascript/collectionviewer.js
@@ -466,7 +466,7 @@ require(["jquery", "sakai/sakai.api.core"], function($, sakai) {
                     }, function (success) {
                         sakai.api.Util.progressIndicator.showProgressIndicator(sakai.api.i18n.getValueForKey("REMOVING_CONTENT_FROM_COLLECTION", "collectionviewer"), sakai.api.i18n.getValueForKey("PROCESSING", "collectionviewer"));
                         $(".collectionviewer_check:checked:visible").parents("li").hide("slow");
-                        setTimeout(refreshCollection, 1000);
+                        setTimeout(refreshCollection, 1500);
                     }]);
                 }
             });
@@ -479,7 +479,7 @@ require(["jquery", "sakai/sakai.api.core"], function($, sakai) {
                     context: collectionviewer.contextId
                 }, function (success) {
                     $itemToRemove.parents("li").hide("slow");
-                    setTimeout(refreshCollection, 1000);
+                    setTimeout(refreshCollection, 1500);
                 }]);
             });
 
