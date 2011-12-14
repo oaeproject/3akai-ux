9e845da2148a745fcb894d92bc073a32eb1b0d08
Christian Vuerings <vueringschristian@gmail.com>
2011-12-14 05:56:10 -0800

    SAKIII-4591 - Internationalize the Reply text

--- devwidgets/inbox/javascript/inbox.js
+++ devwidgets/inbox/javascript/inbox.js
@@ -322,14 +322,13 @@ require(["jquery", "sakai/sakai.api.core"], function($, sakai) {
          */
         var showReply = function() {
             $inbox_show_message_reply_fields = $($inbox_show_message_reply_fields.selector);
-            var replyButtonText = sakai.api.i18n.getValueForKey("REPLY", "inbox");
-            var reText = "Re: ";
+            var replyText = sakai.api.i18n.getValueForKey("REPLY", "inbox");
             var messageSubject = currentMessage.subject;
-            // Check whether "Re: " is already there. If not, add it to the subject line
-            if(currentMessage.subject.substring(0, reText.length) !== reText){
-                messageSubject = reText + currentMessage.subject;
+            // Check whether the message starts with Reply. If not, add it to the subject line
+            if(currentMessage.subject.substring(0, replyText.length) !== replyText){
+                messageSubject = replyText + ": " + currentMessage.subject;
             }
-            $(window).trigger("initialize.sendmessage.sakai", [currentMessage.replyAll, $inbox_show_message_reply_fields, handleReplyFinished, messageSubject, null, true, currentMessage.id, replyButtonText]);
+            $(window).trigger("initialize.sendmessage.sakai", [currentMessage.replyAll, $inbox_show_message_reply_fields, handleReplyFinished, messageSubject, null, true, currentMessage.id, replyText]);
             $inbox_show_message_reply_fields.show();
         };
 
