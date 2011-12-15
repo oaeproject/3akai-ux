8c6e6b57e8f505f351118b566d13bbfb79726760
Bert Pareyn <bp323@caret.cam.ac.uk>
2011-12-15 10:20:49 +0000

    SAKIII-3477 Combine if structures

--- devwidgets/versions/javascript/versions.js
+++ devwidgets/versions/javascript/versions.js
@@ -163,41 +163,39 @@ require(["jquery", "sakai/sakai.api.core"], function($, sakai){
         var restoreVersion = function(e){
             var page = versions[$(this).parent().attr("data-versionId")].page;
             sakai.api.Content.checkAutosave(false, currentPageShown.pageSavePath + "/" + currentPageShown.saveRef, function(success, data){
-                if(success){
-                    if (data.safeToEdit) {
-                        var toStore = {};
-                        toStore[currentPageShown.saveRef] = {
-                            page: page
-                        }
-                        $.ajax({
-                            url: currentPageShown.pageSavePath,
-                            type: "POST",
-                            dataType: "json",
-                            data: {
-                                ":operation": "import",
-                                ":contentType": "json",
-                                ":replace": true,
-                                ":replaceProperties": true,
-                                "_charset_":"utf-8",
-                                ":content": $.toJSON(toStore)
-                            },
-                            success: function(){
-                                $.ajax({
-                                    url: currentPageShown.pageSavePath + "/" + currentPageShown.saveRef + ".save.json",
-                                    type: "POST",
-                                    data: {
-                                        "sling:resourceType": "sakai/pagecontent",
-                                        "sakai:pagecontent": $.toJSON(toStore),
-                                        "_charset_": "utf-8"
-                                    }, success: function(){
-                                        $(window).trigger("update.versions.sakai", currentPageShown);
-                                    }
-                                });
-                            }
-                        });
-                    } else {
-                        sakai.api.Util.notification.show(sakai.api.i18n.getValueForKey("RESTORE_VERSION", "versions"), sakai.api.i18n.getValueForKey("NEW_VERSION_COULD_NOT_BE_STORED", "versions"));
+                if(success && data.safeToEdit){
+                    var toStore = {};
+                    toStore[currentPageShown.saveRef] = {
+                        page: page
                     }
+                    $.ajax({
+                        url: currentPageShown.pageSavePath,
+                        type: "POST",
+                        dataType: "json",
+                        data: {
+                            ":operation": "import",
+                            ":contentType": "json",
+                            ":replace": true,
+                            ":replaceProperties": true,
+                            "_charset_":"utf-8",
+                            ":content": $.toJSON(toStore)
+                        },
+                        success: function(){
+                            $.ajax({
+                                url: currentPageShown.pageSavePath + "/" + currentPageShown.saveRef + ".save.json",
+                                type: "POST",
+                                data: {
+                                    "sling:resourceType": "sakai/pagecontent",
+                                    "sakai:pagecontent": $.toJSON(toStore),
+                                    "_charset_": "utf-8"
+                                }, success: function(){
+                                    $(window).trigger("update.versions.sakai", currentPageShown);
+                                }
+                            });
+                        }
+                    });
+                } else {
+                    sakai.api.Util.notification.show(sakai.api.i18n.getValueForKey("RESTORE_VERSION", "versions"), sakai.api.i18n.getValueForKey("NEW_VERSION_COULD_NOT_BE_STORED", "versions"));
                 }
             });
         };
