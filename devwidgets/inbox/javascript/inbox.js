4cc6b03404c1dddc998d8792160d55ac15fc9aaa
Christian Vuerings <vueringschristian@gmail.com>
2011-12-14 19:53:07 -0800

    SAKIII-4591 - Check for "Re:" instead of "Re".

--- devwidgets/inbox/javascript/inbox.js
+++ devwidgets/inbox/javascript/inbox.js
@@ -322,13 +322,14 @@ require(["jquery", "sakai/sakai.api.core"], function($, sakai) {
          */
         var showReply = function() {
             $inbox_show_message_reply_fields = $($inbox_show_message_reply_fields.selector);
-            var replyText = sakai.api.i18n.getValueForKey("REPLY", "inbox");
+            var replyButtonText = sakai.api.i18n.getValueForKey("REPLY", "inbox");
+            var replyText = sakai.api.i18n.getValueForKey("RE", "inbox");
             var messageSubject = currentMessage.subject;
             // Check whether the message starts with Reply. If not, add it to the subject line
             if(currentMessage.subject.substring(0, replyText.length) !== replyText){
-                messageSubject = replyText + ": " + currentMessage.subject;
+                messageSubject = replyText + " " + currentMessage.subject;
             }
-            $(window).trigger("initialize.sendmessage.sakai", [currentMessage.replyAll, $inbox_show_message_reply_fields, handleReplyFinished, messageSubject, null, true, currentMessage.id, replyText]);
+            $(window).trigger("initialize.sendmessage.sakai", [currentMessage.replyAll, $inbox_show_message_reply_fields, handleReplyFinished, messageSubject, null, true, currentMessage.id, replyButtonText]);
             $inbox_show_message_reply_fields.show();
         };
 
