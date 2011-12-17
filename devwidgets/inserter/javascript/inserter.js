08ef37404dc7961212b3338ff6fa2fbcd67b81c3
Bert Pareyn <bp323@caret.cam.ac.uk>
2011-12-17 13:54:49 +0000

    SAKIII-4682 Set up inserter widget and create the 'no collections' display

---
+++ devwidgets/inserter/javascript/inserter.js
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Sakai Foundation (SF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The SF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+// load the master sakai object to access all Sakai OAE API methods
+require(["jquery", "sakai/sakai.api.core"], function($, sakai) {
+
+    /**
+     * @name sakai_global.inserter
+     *
+     * @class inserter
+     *
+     * @description
+     * The inserter makes it possible to insert content from collections and 
+     * enhances the content authoring experience.
+     *
+     * @version 0.0.1
+     * @param {String} tuid Unique id of the widget
+     * @param {Boolean} showSettings Show the settings of the widget or not
+     */
+    sakai_global.inserter = function (tuid, showSettings) {
+
+
+        /////////////////////////////
+        // Configuration variables //
+        /////////////////////////////
+        
+        var $rootel = $("#" + tuid);
+        
+        var doInit = function(){
+            $(".inserter_widget", $rootel).draggable({
+                cancel: "div#inserter_collector"
+            });
+
+            $(".inserter_widget", $rootel).show();
+        };
+        
+        doInit();
+    };
+
+    // inform Sakai OAE that this widget has loaded and is ready to run
+    sakai.api.Widgets.widgetLoader.informOnLoad("inserter");
+});
