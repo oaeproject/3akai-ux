01edb365c0585d57aa5b585e137c4536f422495b
Christian Vuerings <vueringschristian@gmail.com>
2012-01-05 13:46:42 -0800

    SAKIII-4714 - Don't add QUnit twice

--- tests/qunit/tests/unit/js/widgets.js
+++ tests/qunit/tests/unit/js/widgets.js
@@ -2,7 +2,7 @@ require(
     [
     "jquery",
     "sakai/sakai.api.core",
-    "../../../../tests/qunit/js/qunit.js",
+    "qunitjs/qunit",
     "../../../../tests/qunit/js/sakai_qunit_lib.js",
     "../../../../tests/qunit/js/dev.js",
     "../../../../tests/qunit/js/devwidgets.js"
