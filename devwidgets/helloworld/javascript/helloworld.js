bc0e8e14e98aed8a782955aed263934a9754de52
Bert Pareyn <bp323@caret.cam.ac.uk>
2012-01-20 11:18:09 +0000

    SAKIII-4794 HTML updates to the helloworld widget

--- devwidgets/helloworld/javascript/helloworld.js
+++ devwidgets/helloworld/javascript/helloworld.js
@@ -45,6 +45,7 @@ require(["jquery", "sakai/sakai.api.core"], function($, sakai) {
         var $mainContainer = $("#helloworld_main", $rootel);
         var $settingsContainer = $("#helloworld_settings", $rootel);
         var $settingsForm = $("#helloworld_settings_form", $rootel);
+        var $cancelSettings = $("#helloworld_cancel_settings", $rootel);
         var $colorPicker = $("#helloworld_color", $rootel);
         var $usernameContainer = $("#helloworld_username", $rootel);
 
@@ -138,6 +139,10 @@ require(["jquery", "sakai/sakai.api.core"], function($, sakai) {
                 }
             );
             return false;
+        });
+
+        $cancelSettings.bind("click", function(){
+            sakai.api.Widgets.Container.informFinish(tuid, "helloworld");
         });
 
 
