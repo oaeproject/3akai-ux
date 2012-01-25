64e092a312c67c26d70c3515a6b0ada0fd510835
Nicolaas Matthijs <nicolaas.matthijs@caret.cam.ac.uk>
2012-01-25 17:07:42 +0000

    SAKIII-4588 - Changed naming to match convention

--- dev/javascript/content_profile.js
+++ dev/javascript/content_profile.js
@@ -63,11 +63,11 @@ require(["jquery","sakai/sakai.api.core"], function($, sakai) {
 
                                 var collectionId = $.bbq.getState("collectionId");
                                 var collectionName = $.bbq.getState("collectionName");
-                                var currentpath = $.bbq.getState("p");
-                                if(collectionId && collectionName && currentpath){
+                                var currentPath = $.bbq.getState("p");
+                                if(collectionId && collectionName && currentPath){
                                     // Show go back to collection link
                                     $("#back_to_collection_button #collection_title").text(collectionName);
-                                    $("#back_to_collection_button").attr("href", "/content#p=" + collectionId + "/" + sakai.api.Util.safeURL(collectionName) + "&item=" + currentpath.split("/")[0]);
+                                    $("#back_to_collection_button").attr("href", "/content#p=" + collectionId + "/" + sakai.api.Util.safeURL(collectionName) + "&item=" + currentPath.split("/")[0]);
                                     $("#back_to_collection_container").show("slow");
                                 } else {
                                     $("#back_to_collection_container").hide("slow");
