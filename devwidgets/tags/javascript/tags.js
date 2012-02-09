0bd875b5dfa34869b006275b9b14741af6e0ae4b
Gonzalo Silverio <gsilver@umich.edu>
2012-02-09 15:22:43 -0500

    SAKIII-4673 - Most popular tags widget should be normalized - fix require path

--- devwidgets/tags/javascript/tags.js
+++ devwidgets/tags/javascript/tags.js
@@ -24,7 +24,7 @@
 
 /*global $ */
 
-require(["jquery", "sakai/sakai.api.core", "/dev/lib/jquery/plugins/jquery.tagcloud.js", "underscore"], function($, sakai, _) {
+require(["jquery", "sakai/sakai.api.core", "jquery-plugins/jquery.tagcloud.js", "underscore"], function($, sakai, _) {
 
     /**
      * @name sakai_global.tags
