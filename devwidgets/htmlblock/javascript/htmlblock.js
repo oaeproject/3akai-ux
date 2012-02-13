4501b52c3399393a3b8caf0e68b7020316599129
Nicolaas Matthijs <nicolaas.matthijs@caret.cam.ac.uk>
2012-02-13 13:34:10 +0000

    SAKIII-4724 - Bind to save event once

--- devwidgets/htmlblock/javascript/htmlblock.js
+++ devwidgets/htmlblock/javascript/htmlblock.js
@@ -88,15 +88,18 @@ require(["jquery", "sakai/sakai.api.core"], function($, sakai) {
                 autoSavePoll = false;
             }
             autoSavePoll = setInterval(autoSave, 5000);
-            $(window).bind("save.contentauthoring.sakai", function(){
-                if ($rootel.is(":visible")) {
-                    autoSave();
+        };
+
+        $(window).bind("save.contentauthoring.sakai", function(){
+            if ($rootel.is(":visible")) {
+                autoSave();
+                if (id && tinyMCE.get(id)) {
                     var currentText = tinyMCE.get(id).getContent();
                     $("#htmlblock_view_container", $rootel).html(currentText);
                     sakai.api.Util.renderMath($rootel);
                 }
-            });
-        };
+            }
+        });
         
         var stopAutosave = function(){
             if (autoSavePoll){
