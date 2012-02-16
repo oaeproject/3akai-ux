dc8429c4c8ecea97c7a9e118dfb0c1c11230e95d
Chris Roby <christopher.roby@gmail.com>
2012-02-15 16:04:23 -0800

    SAKIII-4869 Use the API to get a user's name - Modified the api to use safeOutput instead of saneHTML, as it wasn't doing what we wanted it to

--- dev/javascript/user.js
+++ dev/javascript/user.js
@@ -411,7 +411,7 @@ require(["jquery","sakai/sakai.api.core"], function($, sakai) {
         var showWelcomeNotification = function(){
             var querystring = new Querystring();
             if (querystring.contains("welcome") && querystring.get("welcome") === "true" && !sakai.data.me.user.anon){
-                sakai.api.Util.notification.show(sakai.api.i18n.getValueForKey("WELCOME") + " " + sakai.data.me.profile.basic.elements.firstName.value,sakai.api.i18n.getValueForKey("YOU_HAVE_CREATED_AN_ACCOUNT"));
+                sakai.api.Util.notification.show(sakai.api.i18n.getValueForKey("WELCOME") + " " + sakai.api.User.getFirstName(sakai.data.me.profile), sakai.api.i18n.getValueForKey("YOU_HAVE_CREATED_AN_ACCOUNT"));
             }
         };
 
