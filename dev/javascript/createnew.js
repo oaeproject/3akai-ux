9b570b3ccbadaa05b49280bec2e02f5f77622cbb
Nicolaas Matthijs <nicolaas.matthijs@caret.cam.ac.uk>
2012-02-23 14:53:22 +0000

    NOJIRA - Code cleanup

--- dev/javascript/createnew.js
+++ dev/javascript/createnew.js
@@ -4,43 +4,43 @@
  * distributed with this work for additional information
  * regarding copyright ownership. The SF licenses this file
  * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
+ * 'License'); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
 
-require(["jquery","sakai/sakai.api.core"], function($, sakai) {
+require(['jquery','sakai/sakai.api.core'], function($, sakai) {
 
     sakai_global.createnew = function() {
 
         var pubdata = {
-            "structure0": {}
+            'structure0': {}
         };
 
-        for (var i = 0; i < sakai.config.worldTemplates.length; i++){
+        for (var i = 0; i < sakai.config.worldTemplates.length; i++) {
             var category = sakai.config.worldTemplates[i];
             var rnd = sakai.api.Util.generateWidgetId();
             pubdata.structure0[category.id] = {
-                "_order": i,
-                "_title": sakai.api.i18n.getValueForKey(category.title),
-                "_ref": rnd
+                '_order': i,
+                '_title': sakai.api.i18n.getValueForKey(category.title),
+                '_ref': rnd
             };
             pubdata[rnd] = {
-                "rows": [{
-                    "id": sakai.api.Util.generateWidgetId(),
-                    "columns": [{
-                        "width": 1,
-                        "elements": [
+                'rows': [{
+                    'id': sakai.api.Util.generateWidgetId(),
+                    'columns': [{
+                        'width': 1,
+                        'elements': [
                             {
-                                "id": category.id,
-                                "type": "selecttemplate"
+                                'id': category.id,
+                                'type': 'selecttemplate'
                             }
                         ]
                     }]
@@ -48,25 +48,21 @@ require(["jquery","sakai/sakai.api.core"], function($, sakai) {
             };
         }
 
-        var generateNav = function(){
-            $(window).trigger("lhnav.init", [pubdata, {}, {}]);
+        var generateNav = function() {
+            $(window).trigger('lhnav.init', [pubdata, {}, {}]);
         };
 
-        var renderCreateGroup = function(){
-            $(window).trigger("sakai.newcreategroup.init");
+        var renderCreateGroup = function() {
+            $(window).trigger('sakai.newcreategroup.init');
         };
 
-        $(window).bind("lhnav.ready", function(){
-            generateNav();
-        });
-
-        $(window).bind("newcreategroup.ready", function(){
-            renderCreateGroup();
-        });
+        $(window).bind('lhnav.ready', generateNav);
+        $(window).bind('newcreategroup.ready', renderCreateGroup);
 
         generateNav();
         
     };
 
-    sakai.api.Widgets.Container.registerForLoad("createnew");
+    sakai.api.Widgets.Container.registerForLoad('createnew');
+
 });
