87b032ee64c912520de877265da3aa3f093b0561
Nicolaas Matthijs <nicolaas.matthijs@caret.cam.ac.uk>
2012-02-23 23:57:39 +0000

    NOJIRA - Following up on code review

--- dev/javascript/acknowledgements.js
+++ dev/javascript/acknowledgements.js
@@ -4,14 +4,14 @@
  * distributed with this work for additional information
  * regarding copyright ownership. The SF licenses this file
  * to you under the Apache License, Version 2.0 (the
- * 'License'); you may not use this file except in compliance
+ * "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
- * 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
@@ -126,11 +126,11 @@ require(['jquery','sakai/sakai.api.core'], function($, sakai) {
             $(window).trigger('sakai.entity.init', ['acknowledgements']);
         };
 
-        $(window).bind('lhnav.ready', function(){
+        $(window).bind('lhnav.ready', function() {
             generateNav();
         });
         
-        $(window).bind('sakai.entity.ready', function(){
+        $(window).bind('sakai.entity.ready', function() {
             renderEntity(); 
         });
         
