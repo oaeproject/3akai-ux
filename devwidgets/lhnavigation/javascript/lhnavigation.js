c49d24297806202c94b04ca0f1c59727f9068019
Nicolaas Matthijs <nicolaas.matthijs@caret.cam.ac.uk>
2012-02-24 00:35:50 +0000

    NOJIRA - Following up on code review

--- devwidgets/lhnavigation/javascript/lhnavigation.js
+++ devwidgets/lhnavigation/javascript/lhnavigation.js
@@ -491,33 +491,32 @@ require(["jquery", "sakai/sakai.api.core", "jquery-ui"], function($, sakai) {
             }
         };
 
-        var preparePageRender = function(ref, path, savePath, pageSavePath, nonEditable, canEdit, newPageMode){
+        var preparePageRender = function(ref, path, savePath, pageSavePath, nonEditable, canEdit, newPageMode) {
             var content = getPageContent(ref);
             var pageContent = content ? content : sakai.config.defaultSakaiDocContent;
             var lastModified = content && content._lastModified ? content._lastModified : null;
             var autosave = content && content.autosave ? content.autosave : null;
-            var pageTitle = $.trim($(".lhnavigation_selected_item .lhnavigation_page_title_value").text());
+            var pageTitle = $.trim($('.lhnavigation_selected_item .lhnavigation_page_title_value').text());
             var saveRef = ref;
-            if (saveRef.indexOf("-") !== -1){
-                saveRef = saveRef.substring(saveRef.indexOf("-") + 1);
+            if (saveRef.indexOf('-') !== -1) {
+                saveRef = saveRef.substring(saveRef.indexOf('-') + 1);
             }
             currentPageShown = {
-                "ref": ref,
-                "path": path,
-                "content": pageContent,
-                "savePath": savePath,
-                "pageSavePath": pageSavePath,
-                "saveRef": saveRef,
-                "canEdit": canEdit,
-                //"widgetData": [privstructure.pages, pubstructure.pages],
-                "addArea": contextData.addArea,
-                "nonEditable": nonEditable,
-                "_lastModified": lastModified,
-                "autosave": autosave,
-                "title": pageTitle
+                'ref': ref,
+                'path': path,
+                'content': pageContent,
+                'savePath': savePath,
+                'pageSavePath': pageSavePath,
+                'saveRef': saveRef,
+                'canEdit': canEdit,
+                'addArea': contextData.addArea,
+                'nonEditable': nonEditable,
+                '_lastModified': lastModified,
+                'autosave': autosave,
+                'title': pageTitle
             };
             if (newPageMode) {
-                $(window).trigger("editpage.contentauthoring.sakai", [currentPageShown, newPageMode]);
+                $(window).trigger('editpage.contentauthoring.sakai', [currentPageShown, newPageMode]);
                 contextMenuHover = {
                     path: currentPageShown.path,
                     ref: currentPageShown.ref,
@@ -527,7 +526,7 @@ require(["jquery", "sakai/sakai.api.core", "jquery-ui"], function($, sakai) {
                 };
                 editPageTitle();
             } else {
-                $(window).trigger("showpage.contentauthoring.sakai", [currentPageShown]);
+                $(window).trigger('showpage.contentauthoring.sakai', [currentPageShown]);
             }
         };
 
