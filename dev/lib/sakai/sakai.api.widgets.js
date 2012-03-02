31c1babe31356ab27c2c67a6803136e449af01db
Nicolaas Matthijs <nicolaas.matthijs@caret.cam.ac.uk>
2012-03-02 19:25:45 +0000

    SAKIII-4927 - Code styleguide cleanup

--- dev/lib/sakai/sakai.api.widgets.js
+++ dev/lib/sakai/sakai.api.widgets.js
@@ -861,11 +861,11 @@ define(
          * This function will return widget configuration for a specific widget
          * @param {Object} widgetid  id of the widget as specified in the widget's config file
          */
-        getWidget: function(widgetid){
-            if (sakai.widgets[widgetid]){
+        getWidget: function(widgetid) {
+            if (sakai.widgets[widgetid]) {
                 return sakai.widgets[widgetid];
             } else {
-                debug.error("A config file was not found for the following widget: " + widgetid);
+                debug.error('A config file was not found for the following widget: ' + widgetid);
                 return false;
             }
         },
