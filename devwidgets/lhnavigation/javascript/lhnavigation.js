44e5135dd6fa66c170a26c9803ff96c77b0a603b
Bert Pareyn <bp323@caret.cam.ac.uk>
2012-03-08 21:16:52 +0000

    SAKIII-5258 Accepting an invitation in the inbox doesn't update counts in the left hand navigation

--- devwidgets/lhnavigation/javascript/lhnavigation.js
+++ devwidgets/lhnavigation/javascript/lhnavigation.js
@@ -113,7 +113,7 @@ require(['jquery', 'underscore', 'sakai/sakai.api.core', 'jquery-ui'], function(
                     count._count = value;
                 }
                 if (listitem.length) {
-                    if (!listitem.find('lhnavigation_levelcount').length) {
+                    if (!listitem.find('.lhnavigation_levelcount').length) {
                         listitem.find('.lhnavigation_item_content').prepend(
                             sakai.api.Util.TemplateRenderer('lhnavigation_counts_template', {
                             'count': 0
