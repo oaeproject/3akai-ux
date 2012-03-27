3d5ba7ddd923a3685a38f76f606f701eab6c9b3d
James Sloane <jsloane@csu.edu.au>
2012-03-27 11:16:45 +1100

    SAKIII-5066 dont show warning when changing group access from public

--- devwidgets/worldsettings/javascript/worldsettings.js
+++ devwidgets/worldsettings/javascript/worldsettings.js
@@ -58,7 +58,7 @@ require(["jquery", "sakai/sakai.api.core"], function($, sakai) {
         var showWarning = function(){
             var newVisibility = $(worldsettingsCanBeFoundIn);
             var newVisibilityVal = $.trim(newVisibility.val());
-            var oldVisibilityVal = sakai_global.group.groupData["sakai:group-visible"];
+            var oldVisibilityVal = sakai_global.group.groupData['sakai:group-visible'];
             var oldVisibilityIndex = parseInt(newVisibility.find("option[value='" + sakai_global.group.groupData["sakai:group-visible"] + "']").attr("index"), 10);
             if (sakai_global.group.groupData['sakai:group-visible'] === newVisibilityVal || parseInt(newVisibility.attr('selectedIndex'), 10) > oldVisibilityIndex || newVisibilityVal === 'members-only' || oldVisibilityVal === 'public') {
                 $worldsettingsForm.submit();
