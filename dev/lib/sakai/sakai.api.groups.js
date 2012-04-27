84e007152ac1d6c8377eee179732012915d17233
James Sloane <jsloane@csu.edu.au>
2012-04-27 14:53:54 +1000

    SAKIII-3756 display error message if invalid group id

--- dev/lib/sakai/sakai.api.groups.js
+++ dev/lib/sakai/sakai.api.groups.js
@@ -186,7 +186,14 @@ define(
                 data: {data: $.toJSON(data)},
                 type: "POST",
                 success: function(_data, textStatus){
-                    callback(true, data);
+                    var success = true;
+                    if (_data && _data.results && _data.results[0] && _data.results[0].error) {
+                        success = false;
+                        if (_data.results[0].error === "Invalid group id") {
+                            data.errorMessage = "GROUP_CREATION_ERROR_INVALID_ID";
+                        }
+                    }
+                    callback(success, data);
                 },
                 error: function(){
                     callback(false, data);
