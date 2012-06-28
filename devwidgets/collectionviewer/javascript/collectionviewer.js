75dc7115b18eff4315ae354b3f6f55c6c20692ff
jsloane <jsloane@csu.edu.au>
2012-06-28 14:51:57 +1000

    SAKIII-5638 Remove all places that load > 20 items

--- devwidgets/collectionviewer/javascript/collectionviewer.js
+++ devwidgets/collectionviewer/javascript/collectionviewer.js
@@ -325,21 +325,18 @@ require(['jquery', 'sakai/sakai.api.core'], function($, sakai) {
          * @param {Function} callback Function to be executed on retrieval of the user profiles
          */
         var getMultipleUserData = function(data, callback) {
-            var batchRequests = [];
+            var usersToFetch = [];
             $.each(data.results, function(i, user) {
                 if (user['sakai:pool-content-created-for']) {
-                    batchRequests.push({
-                        'url': '/~' + user['sakai:pool-content-created-for'] + '/public/authprofile.profile.json',
-                        'method':'GET'
-                    });
+                    usersToFetch.push(user['sakai:pool-content-created-for']);
                 }
             });
-            sakai.api.Server.batch(batchRequests, function(success, results) {
-                if (success) {
-                    $.each(results.results, function(index, item) {
-                        item = $.parseJSON(item.body);
-                        var userid = item['rep:userId'];
-                        var displayName = sakai.api.User.getDisplayName(item);
+
+            sakai.api.User.getMultipleUsers(usersToFetch, function(fetchedUsers) {
+                $.each(data.results, function(index, item) {
+                    var userid = item['sakai:pool-content-created-for'];
+                    if (userid && fetchedUsers[userid]) {
+                        var displayName = sakai.api.User.getDisplayName(fetchedUsers[userid]);
                         data.results[index].ownerId = userid;
                         data.results[index].ownerDisplayName = displayName;
                         data.results[index].ownerDisplayNameShort = sakai.api.Util.applyThreeDots(displayName, 580, {
@@ -350,10 +347,10 @@ require(['jquery', 'sakai/sakai.api.core'], function($, sakai) {
                             max_rows: 1,
                             whole_word: false
                         }, 's3d-bold', true);
-                    });
-                    if ($.isFunction(callback)) {
-                        callback();
                     }
+                });
+                if ($.isFunction(callback)) {
+                    callback();
                 }
             });
         };
