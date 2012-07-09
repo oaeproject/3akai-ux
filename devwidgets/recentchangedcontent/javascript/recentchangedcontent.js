151f7978380f68c575bf1aff6d08278e2e548da8
jsloane <jsloane@csu.edu.au>
2012-07-09 11:52:06 +1000

    SAKIII-5918 changed widgets to use api search string function

--- devwidgets/recentchangedcontent/javascript/recentchangedcontent.js
+++ devwidgets/recentchangedcontent/javascript/recentchangedcontent.js
@@ -207,40 +207,11 @@ require(['jquery', 'sakai/sakai.api.core'], function($, sakai) {
         };
 
         /**
-         * This function will replace all
-         * @param {String} term The search term that needs to be converted.
-         */
-        var prepSearchTermForURL = function(term) {
-            // Filter out http:// as it causes the search feed to break
-            term = term.replace(/http:\/\//ig, '');
-            // taken this from search_main until a backend service can get related content
-            var urlterm = '';
-            var split = $.trim(term).split(/\s/);
-            if (split.length > 1) {
-                for (var i = 0; i < split.length; i++) {
-                    if (split[i]) {
-                        urlterm += split[i] + ' ';
-                        if (i < split.length - 1) {
-                            urlterm += 'OR ';
-                        }
-                    }
-                }
-            }
-            else {
-                urlterm = split[0];
-                if (urlterm === '') {
-                    urlterm = '*';
-                }
-            }
-            return urlterm;
-        };
-
-        /**
          * Fetches the related content
          */
         var getRelatedContent = function(contentData){
             var searchterm = contentData["sakai:pooled-content-file-name"].substring(0,400);
-            searchquery = prepSearchTermForURL(searchterm);
+            searchquery = sakai.api.Server.createSearchString(searchterm, false, 'OR');
 
             // get related content for contentData
             // return some search results for now
