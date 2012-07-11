34eed1d9aeec745064b1bd6b65d3b7d91a804a20
Bert Pareyn <pareyn.bert@gmail.com>
2012-07-11 11:21:49 -0700

    SAKIII-5944 Cannot share content from Content Permissions overlay
    Squashed commit of the following:
    
    commit 5f5d51c4f53dc1de7e6c1b5820d08581a9f4e8c9
    Merge: 067e2ba 4a7b64d
    Author: Christian Vuerings <vueringschristian@gmail.com>
    Date:   Wed Jul 11 11:21:48 2012 -0700
    
        Merge remote-tracking branch 'bp323/SAKIII-5944' into bp323-SAKIII-5944
    
    commit 4a7b64da4d85eebee4106585365eada70cc52cf9
    Author: Bert Pareyn <pareyn.bert@gmail.com>
    Date:   Tue Jul 10 15:55:37 2012 +0100
    
        SAKIII-5944 Cannot share content from Content Permissions overlay
    
    commit 13ed164446928d4ea1d3ed8839ec19025869447a
    Author: Christian Vuerings <vueringschristian@gmail.com>
    Date:   Mon Jul 9 15:24:19 2012 -0700
    
        SAKIII-5944 - Cannot share content from Content Permissions overlay
        https://jira.sakaiproject.org/browse/SAKIII-5944

--- devwidgets/newsharecontent/javascript/newsharecontent.js
+++ devwidgets/newsharecontent/javascript/newsharecontent.js
@@ -286,43 +286,45 @@ require(['jquery', 'sakai/sakai.api.core', 'underscore'], function($, sakai, _) 
                 }
                 sakai.api.Util.Forms.clearValidation($newsharecontent_form);
                 var idArr = $(this).attr("data-entityid");
-                if (idArr.length > 1 && !$.isArray(idArr)) {
-                    idArr = idArr.split(",");
-                }
-                var $this = $(this);
-                $newsharecontentContainer.css({
-                    'top':$this.offset().top + $this.height(),
-                    'left':$this.offset().left + $this.width() / 2 - 119
-                });
-                // Fetch data for content items
-                var batchRequests = [];
-                $.each(idArr, function(i, id) {
-                    batchRequests.push({
-                        "url": "/p/" + id + ".json",
-                        "method": "GET"
-                    });
-                });
-                sakai.api.Server.batch(batchRequests, function(success, data) {
-                    if (success && data) {
-                        if (data.results) {
-                            $.each(data.results, function(i, result){
-                                data.results[i].body = $.parseJSON(data.results[i].body);
-                            });
-                            contentObj = {
-                                data: data.results,
-                                shareUrl: sakai.api.Content.createContentURL(data.results[0].body)
-                            };
-                        } else if (data.url) {
-                            contentObj = {
-                                data: [data],
-                                shareUrl:  sakai.api.Content.createContentURL(data)
-                            };
-                        }
-                        if (window["addthis"]) {
-                            $newsharecontentContainer.jqmShow();
-                        }
+                if (idArr && idArr.length) {
+                    if (!$.isArray(idArr)) {
+                        idArr = idArr.split(',');
                     }
-                });
+                    var $this = $(this);
+                    $newsharecontentContainer.css({
+                        'top':$this.offset().top + $this.height(),
+                        'left':$this.offset().left + $this.width() / 2 - 119
+                    });
+                    // Fetch data for content items
+                    var batchRequests = [];
+                    $.each(idArr, function(i, id) {
+                        batchRequests.push({
+                            'url': '/p/' + id + '.json',
+                            'method': 'GET'
+                        });
+                    });
+                    sakai.api.Server.batch(batchRequests, function(success, data) {
+                        if (success && data) {
+                            if (data.results) {
+                                $.each(data.results, function(i, result){
+                                    data.results[i].body = $.parseJSON(data.results[i].body);
+                                });
+                                contentObj = {
+                                    data: data.results,
+                                    shareUrl: sakai.api.Content.createContentURL(data.results[0].body)
+                                };
+                            } else if (data.url) {
+                                contentObj = {
+                                    data: [data],
+                                    shareUrl:  sakai.api.Content.createContentURL(data)
+                                };
+                            }
+                            if (window['addthis']) {
+                                $newsharecontentContainer.jqmShow();
+                            }
+                        }
+                    });
+                }
             });
         };
 
