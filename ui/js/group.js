bd2c76b92dc832bda195b1ed866519e14c3ae28e
Bert Pareyn <pareyn.bert@gmail.com>
2012-12-19 16:17:02 +0000

    Fixes per comments on Github

--- ui/js/group.js
+++ ui/js/group.js
@@ -13,9 +13,6 @@
  * permissions and limitations under the License.
  */
 
-// TODO: Remove this once we have a better way of sharing data
-var sakai_global = sakai_global || {};
-
 require(['jquery', 'oae/api/oae.core'], function($, oae) {
 
     //  Get the group id from the URL. The expected URL is /group/<groupId>
@@ -24,12 +21,15 @@ require(['jquery', 'oae/api/oae.core'], function($, oae) {
         oae.api.util.redirect().redirectToLogin();
     }
 
+    // Variable used to cache the requested user's profile
+    var groupProfile = null;
+
     /**
      * Get the group's basic profile and set up the screen. If the groups
      * can't be found or is private to the current user, the appropriate
      * error page will be shown
      */
-    var getGroupdata = function() {
+    var getGroupProfile = function() {
         oae.api.group.getGroup(groupId, function(err, profile) {
             if (err && err.code === 404) {
                 oae.api.util.redirect().redirectTo404();
@@ -37,11 +37,11 @@ require(['jquery', 'oae/api/oae.core'], function($, oae) {
                 oae.api.util.redirect().redirectTo403();
             }
 
-            sakai_global.group = profile;
+            groupProfile = profile;
             renderEntity();
             setUpNavigation();
             // Set the browser title
-            oae.api.util.setBrowserTitle(sakai_global.group.name);
+            oae.api.util.setBrowserTitle(groupProfile.name);
             // We can now unhide the page
             oae.api.util.showPage();
         });
@@ -51,9 +51,9 @@ require(['jquery', 'oae/api/oae.core'], function($, oae) {
      * Render the group's profile picture and name
      */
     var renderEntity = function() {
-        oae.api.util.renderTemplate($('#oae_entity_template'), sakai_global.group, $('#oae_entity_container'));
-        $('#entity_group_permissions').on('click', function() {
-            $('body').trigger('init.grouppermissions.sakai');
+        oae.api.util.renderTemplate($('#oae_entity_template'), groupProfile, $('#oae_entity_container'));
+        $(document).on('click', '.group_permissions_trigger', function() {
+            $(document).trigger('init.grouppermissions.sakai', {'group': groupProfile});
         });
     };
     
@@ -63,7 +63,7 @@ require(['jquery', 'oae/api/oae.core'], function($, oae) {
     var setUpNavigation = function() {
         // Only render the left hand navigation if the group's profile
         // has already been retrieved
-        if (sakai_global.group) {
+        if (groupProfile) {
             // TODO: Replace this with more effective page configuration
             var pubdata = {
                 'structure0': {
@@ -116,7 +116,7 @@ require(['jquery', 'oae/api/oae.core'], function($, oae) {
                         }
                     ],
                     'id5244321': {
-                        'principalId': sakai_global.group.id
+                        'principalId': groupProfile.id
                     }
                 },
                 'id88785643': {
@@ -137,8 +137,8 @@ require(['jquery', 'oae/api/oae.core'], function($, oae) {
                         }
                     ],
                     'id032184831': {
-                        'principalId': sakai_global.group.id,
-                        'canManage': sakai_global.group.isManager
+                        'principalId': groupProfile.id,
+                        'canManage': groupProfile.isManager
                     }
                 },
                 'id1234354657': {
@@ -159,8 +159,8 @@ require(['jquery', 'oae/api/oae.core'], function($, oae) {
                         }
                     ],
                     'id7184318': {
-                        'principalId': sakai_global.group.id,
-                        'canManage': sakai_global.group.isManager
+                        'principalId': groupProfile.id,
+                        'canManage': groupProfile.isManager
                     }
                 }
             };
@@ -200,7 +200,7 @@ require(['jquery', 'oae/api/oae.core'], function($, oae) {
     // List to the left hand navigation ready event for navigation rendering
     $(window).on('lhnav.ready', setUpNavigation);  
 
-    getGroupdata();
+    getGroupProfile();
     setUpViewMode();
 
 });
