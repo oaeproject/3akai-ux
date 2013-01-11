3f43f6422ca18f9ce39582aaa912cb8160f24b07
Nicolaas Matthijs <nicolaas.matthijs@caret.cam.ac.uk>
2013-01-11 14:44:29 +0000

    Removed unused code

--- shared/vendor/js/jquery-plugins/jquery.timeago.js
+++ shared/vendor/js/jquery-plugins/jquery.timeago.js
@@ -136,8 +136,5 @@ require(['jquery'], function (jQuery) {
     return (new Date().getTime() - date.getTime());
   }
 
-  // fix for IE6 suckage
-  //document.createElement("abbr");
-  //document.createElement("time");
 })(jQuery);
 });
