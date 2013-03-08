8ca757d2d8ddbab7d1a0af1a83ae122a05d176d1
Nicolaas Matthijs <nicolaas.matthijs@caret.cam.ac.uk>
2013-03-08 18:39:00 +0000

    Finished library list options

--- shared/oae/js/jquery-plugins/jquery.list-options.js
+++ shared/oae/js/jquery-plugins/jquery.list-options.js
@@ -15,15 +15,27 @@
 
 define(['jquery'], function (jQuery) {
     (function() {
-        $(document).on('change', '.oae-list-selectall', function(ev) {
-            var checked = $(ev.target).is(':checked');
-            $('.oae-list:visible input[type="checkbox"]').attr('checked', checked);
-        });
 
+        /**
+         * Show or hide the list options when clicking the toggle next to the page title
+         */
         $(document).on('click', '.oae-list-options-toggle', function(ev) {
-            $('.oae-list-options').toggleClass('hide');
+            $('.oae-list-options-actions').toggleClass('hide');
             $(ev.target).find('i').toggleClass('icon-caret-down icon-caret-up');
         });
+
+        /**
+         * Select or deselect all elements when clicking the select all checkbox in the
+         * list options
+         */
+        $(document).on('change', '.oae-list-selectall', function(ev) {
+            var checked = $(this).is(':checked');
+            $('.oae-list input[type="checkbox"]').attr('checked', checked);
+        });
+
+        ////////////////
+        // VIEW MODES //
+        ////////////////
 
         /**
          * Switches the view mode of a list to list, expanded list or grid
@@ -34,26 +46,34 @@ define(['jquery'], function (jQuery) {
             $(el).addClass('active');
         };
 
-        $(document).on('click', '.search_view_grid', function() {
-            switchViewMode(this);
-            $('.oae-list:visible').addClass('grid');
-            $('.oae-list:visible').removeClass('expandedlist');
+        $(document).on('click', '.oae-list-options .btn-group button', function() {
+            $('.oae-list-options .btn-group button').removeClass('active');
+            $(this).addClass('active');
+            $('.oae-list:visible').removeClass('oae-list-grid oae-list-details oae-list-compact');
+            $('.oae-list:visible').addClass($(this).attr('data-type'));
         });
 
-        $(document).on('click', '.search_view_expandedlist', function() {
-            switchViewMode(this);
-            $('.oae-list:visible').removeClass('grid');
-            $('.oae-list:visible').addClass('expandedlist');
-        });
+        //$(document).on('click', '.search_view_grid', function() {
+        //    switchViewMode(this);
+        //    $('.oae-list:visible').addClass('grid');
+        //    $('.oae-list:visible').removeClass('expandedlist');
+        //});
 
-        $(document).on('click', '.search_view_list', function() {
-            switchViewMode(this);
-            $('.oae-list:visible').removeClass('expandedlist');
-            $('.oae-list:visible').removeClass('grid');
-        });
+        //$(document).on('click', '.search_view_expandedlist', function() {
+        //    switchViewMode(this);
+        //    $('.oae-list:visible').removeClass('grid');
+        //    $('.oae-list:visible').addClass('expandedlist');
+        //});
 
-        $(document).on('click', '.oae-list-item-right', function() {
-            $(this).parent().toggleClass('active');
-        });
+        //$(document).on('click', '.search_view_list', function() {
+        //    switchViewMode(this);
+        //    $('.oae-list:visible').removeClass('expandedlist');
+        //    $('.oae-list:visible').removeClass('grid');
+        //});
+
+        //$(document).on('click', '.oae-list-item-right', function() {
+        //    $(this).parent().toggleClass('active');
+        //});
+
     })();
 });
