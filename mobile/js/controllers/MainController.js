4f0712afa9d2de238fde7f8d4ce98ac87b72c78a
Mathieu Decoene <hello@mathieudecoene.be>
2013-04-09 11:08:15 +0100

    SASS setup

--- mobile/js/controllers/MainController.js
+++ mobile/js/controllers/MainController.js
@@ -125,7 +125,7 @@ define(
          */
         var showHideMenu = function(active) {
             if(active != null) _menu.setActive(active);
-            var val = (_menu.getActive()) ? '150px' : '0';
+            var val = (_menu.getActive()) ? '261px' : '0';
             $('#oae-mobile-viewport').css('left', val);
         };
 
