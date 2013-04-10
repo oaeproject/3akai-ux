0f15660222dfc01a7da2459a7c00f84b05545253
Bert Pareyn <pareyn.bert@gmail.com>
2013-04-10 18:12:32 +0100

    Implement the revision history widget

--- ui/js/content.js
+++ ui/js/content.js
@@ -96,7 +96,7 @@ require(['jquery','oae.core'], function($, oae) {
     });
 
     // Catches the `upload new version complete` event and refreshes the content profile
-    $(document).on('oae-uploadnewversion-complete', refreshContentProfile);
+    $(document).on('oae-contentprofile-refresh', refreshContentProfile);
 
     /**
      * Render the content's clip, containing the thumbnail, display name as well as the
