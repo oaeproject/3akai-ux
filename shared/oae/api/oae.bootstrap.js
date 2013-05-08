ae0e940c5890fa47947c324558841333f78e7610
Dieter Casier <casier.dieter@gmail.com>
2013-05-08 11:18:09 +0100

    Changed the link in bootstrap file oembed.oae-edited

--- shared/oae/api/oae.bootstrap.js
+++ shared/oae/api/oae.bootstrap.js
@@ -35,7 +35,7 @@ requirejs.config({
         'jquery.imgareaselect': 'vendor/js/jquery-plugins/imgareaselect/jquery.imgareaselect',
         'jquery.jeditable': 'vendor/js/jquery-plugins/jquery.jeditable.oae-edited',
         'jquery.notify': 'vendor/js/jquery-plugins/jquery.bootstrap.notify',
-        'jquery.oembed': 'vendor/js/jquery-plugins/jquery.oembed.sakai-edited',
+        'jquery.oembed': 'vendor/js/jquery-plugins/jquery.oembed.oae-edited',
         'jquery.parseurl': 'vendor/js/jquery-plugins/jquery.parseurl.oae-edited',
         'jquery.properties-parser': 'vendor/js/jquery-plugins/jquery.properties-parser',
         'jquery.serializeObject': 'vendor/js/jquery-plugins/jquery.serializeObject',
