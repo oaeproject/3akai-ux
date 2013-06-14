5260c64265b21ad6d8ff6d17790d823c53d63bb5
Bert Pareyn <pareyn.bert@gmail.com>
2013-06-14 15:47:36 +0100

    Fix untranslated keys test

--- tests/qunit/tests/js/untranslatedkeys.js
+++ tests/qunit/tests/js/untranslatedkeys.js
@@ -23,91 +23,113 @@ require(['jquery', 'oae.core', '../js/util.js', 'qunitjs'], function($, oae, uti
 
         /**
          * Checks whether all the keys found in the HTML string have a translation
+         *
          * @param {String} data HTML string to check for untranslated keys
          */
-        var checkKeys = function(data, widget, callback) {
-            if (regex.test(data)) {
-                while (regex.test(data)) {
+        var checkKeys = function(widgetData, html, isWidget) {
+            if (regex.test(html)) {
+                regex = new RegExp('__MSG__(.*?)__', 'gm');
+                while (regex.test(html)) {
+                    // Get the key from the match
                     var key = RegExp.lastMatch;
                     key = key.substring(7, key.length - 2);
-                    if (widget) {
-                        $.each(cachedWidgets, function(i, w) {
-                            if (w.id === widget) {
-                                if (w.i18n.default[key]) {
-                                    ok(true, key + ' translated in the \'' + widget + '\' widget bundle.');
-                                } else if (cachedWidgets[cachedWidgets.length - 1].i18n.default[key]) {
-                                    ok(true, key + ' translated in the default bundle.');
-                                } else {
-                                    ok(false, key + ' in \'' + widget + '\' not translated.');
+
+                    // Checks if the key has been found at least in one of the i18n files.
+                    var hasi18n = false;
+
+                    // If we're checking a widget check the widget bundles first
+                    if (isWidget) {
+                        // Check if the widget has i18n bundles
+                        if (_.keys(widgetData.widgetData[isWidget].i18n).length) {
+                            // For each bundle in the widget, check if it's available
+                            $.each(widgetData.widgetData[isWidget].i18n, function(i, widgetBundle) {
+                                if (widgetBundle[key]) {
+                                    hasi18n = true;
                                 }
+                            });
+                        }
+                    }
+
+                    // If the widget bundle has no translation or the check is not for a widget, check the main bundles
+                    if (!hasi18n) {
+                        $.each(widgetData.mainBundles, function(i, mainBundle) {
+                            if (mainBundle[key]) {
+                                hasi18n = true;
                             }
                         });
+                    }
+
+                    // If the key has been translated send an ok
+                    if (hasi18n) {
+                        ok(true, '\'' + key + '\' is translated.');
                     } else {
-                        if (cachedWidgets[cachedWidgets.length - 1].i18n.default[key]) {
-                            ok(true, key + ' translated in the default bundle.');
-                        } else {
-                            ok(false, key + ' in \'' + widget + '\' not translated.');
-                        }
+                        ok(false, '\'' + key + '\' is not translated.');
                     }
                 }
             } else {
-                ok(true, 'No keys in this file.');
+                ok(true, 'No keys to be translated.');
             }
-            callback();
-        };
-
-        var makeUntranslatedKeysTest = function(filename, widget) {
-            asyncTest(filename, function() {
-                var widgetId = '';
-                if (widget && widget.id) {
-                    widgetId = widget.id;
-                }
-                $.ajax({
-                    dataType: 'text',
-                    url: filename,
-                    success: function(data) {
-                        checkKeys(data, widgetId, function() {
-                            start();
-                        });
-                    }
-                });
-            });
         };
 
         /**
          * Initializes the Untranslated Keys module
+         *
          * @param  {Object}   widgets    Object containing the manifests of all widgets in node_modules/oae-core.
          */
-        var untranslatedKeysTest = function() {
-
-            // Test the widget HTML files
-            $.each(cachedWidgets, function(i, widget) {
-                if (widget.id !== 'default bundle') {
-                    makeUntranslatedKeysTest('/node_modules/oae-core/' + widget.id + '/' + widget.id + '.html', widget);
-                }
+        var untranslatedKeysTest = function(widgetData) {
+            // Test the widget HTML files for untranslated keys
+            $.each(widgetData.widgetData, function(i, widget) {
+                asyncTest(i + '.html', function() {
+                    checkKeys(widgetData, widget.html, widget.id);
+                    start();
+                });
             });
 
-            // Test the core HTML files
-            var coreHTML = ['errors/accessdenied',
-                            'errors/notfound',
-                            'content',
-                            'group',
-                            'index',
-                            'me',
-                            'search',
-                            'user'];
-            $.each(coreHTML, function(ii, coreHTMLFile) {
-                makeUntranslatedKeysTest('/ui/' + coreHTMLFile + '.html');
+            // Test the core HTML files for untranslated keys
+            $.each(widgetData.mainHTML, function(ii, mainHTML) {
+                asyncTest(ii + '.html', function() {
+                    checkKeys(widgetData, mainHTML, false);
+                    start();
+                });
             });
+
+            // Test the macro HTML files for untranslated keys
+            $.each(widgetData.macroHTML, function(iii, macroHTML) {
+                asyncTest(iii + '.html', function() {
+                    checkKeys(widgetData, macroHTML, false);
+                    start();
+                });
+            });
+
+            // Test the widget JS files for untranslated keys
+            $.each(widgetData.widgetData, function(j, widget) {
+                asyncTest(j + '.js', function() {
+                    checkKeys(widgetData, widget.js, widget.id);
+                    start();
+                });
+            });
+
+            // Test the main JS files for untranslated keys
+            $.each(widgetData.mainJS, function(jj, mainJS) {
+                asyncTest(jj + '.js', function() {
+                    checkKeys(widgetData, mainJS, false);
+                    start();
+                });
+            });
+
+            // Test the API files for untranslated keys
+            $.each(widgetData.apiJS, function(jjj, apiJS) {
+                asyncTest(jjj + '.js', function() {
+                    checkKeys(widgetData, apiJS, false);
+                    start();
+                });
+            });
+
         };
 
-        util.loadWidgets(function(widgets) {
-            util.loadWidgetBundles(widgets, function(widgetBundles) {
-                cachedWidgets = widgetBundles;
-                untranslatedKeysTest();
-            });
-        });
+        util.loadWidgets(untranslatedKeysTest);
 
         QUnit.load();
+        QUnit.start();
     }
 );
