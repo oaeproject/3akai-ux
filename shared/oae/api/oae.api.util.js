5b589b483983ad2efa5eb38369b306dd9fba7fe8
Bert Pareyn <pareyn.bert@gmail.com>
2013-06-26 13:45:00 +0100

    Set max-lengths on input and textarea elements

--- shared/oae/api/oae.api.util.js
+++ shared/oae/api/oae.api.util.js
@@ -339,6 +339,21 @@ define(['exports', 'require', 'jquery', 'underscore', 'oae.api.config', 'jquery.
          * @api private
          */
         var init = function() {
+            // Don't allow fields to have more than 1000 characters
+            $.validator.addMethod('maxlength-short', function(value, element) {
+                return $.trim(value.length) <= 1000;
+            });
+
+            // Don't allow fields to have more than 10.000 characters
+            $.validator.addMethod('maxlength-medium', function(value, element) {
+                return $.trim(value.length) <= 10000;
+            });
+
+            // Don't allow fields to have more than 100.000 characters
+            $.validator.addMethod('maxlength-long', function(value, element) {
+                return $.trim(value.length) <= 100000;
+            });
+
             // Don't allow spaces in the field
             $.validator.addMethod('nospaces', function(value, element) {
                 return this.optional(element) || (value.indexOf(' ') === -1);
