81c100e1ae345017dd456502518cbc7447f87238
Bert Pareyn <pareyn.bert@gmail.com>
2013-08-05 11:42:23 +0100

    Create the link preview test

--- tests/casperjs/util/include/content.js
+++ tests/casperjs/util/include/content.js
@@ -13,7 +13,7 @@ var contentUtil = function() {
     /**
      * Creates a file through the UI and returns the URL to it
      *
-     * @param  {[String]}     file       Optional URL to the file to create
+     * @param  {[String]}   file       Optional URL to the file to create
      * @param  {Function}   callback   Standard callback function
      */
     var createFile = function(file, callback) {
@@ -42,7 +42,38 @@ var contentUtil = function() {
         });
     };
 
+    /**
+     * Creates a link through the UI and returns the URL to it
+     *
+     * @param  {[String]}   link       Optional URL to the link to create
+     * @param  {Function}   callback   Standard callback function
+     */
+    var createLink = function(link, callback) {
+        var linkToCreate = link || 'http://www.oaeproject.org';
+        var contentUrl = null;
+
+        casper.thenOpen('http://test.oae.com/me', function() {
+            casper.waitForSelector('#me-clip-container .oae-clip-content > button', function() {
+                casper.click('.oae-trigger-createlink');
+                casper.wait(1000, function() {
+                    casper.sendKeys('#createlink-link-dump', 'http://www.oaeproject.org');
+                    casper.evaluate(function() {
+                        document.getElementById('createlink-next').removeAttribute('disabled');
+                    });
+                    casper.click('#createlink-next');
+                    casper.click('button#createlink-create');
+                    casper.waitForSelector('#oae-notification-container .alert', function() {
+                        contentUrl = casper.getElementAttribute('#oae-notification-container .alert h4 + a', 'href');
+                        casper.echo('Created link at ' + contentUrl);
+                        callback(contentUrl);
+                    });
+                });
+            });
+        });
+    };
+
     return {
-        'createFile': createFile
+        'createFile': createFile,
+        'createLink': createLink
     };
 };
