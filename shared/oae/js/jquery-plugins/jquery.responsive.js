2daa779c2b884ceed28c0de2d9ac79a81d299b22
Bert Pareyn <pareyn.bert@gmail.com>
2013-12-05 16:48:20 +0000

    Add in responsive nav animation, improvements to the search page search bar

--- shared/oae/js/jquery-plugins/jquery.responsive.js
+++ shared/oae/js/jquery-plugins/jquery.responsive.js
@@ -17,11 +17,45 @@ define(['jquery'], function (jQuery) {
     (function($) {
 
         /**
-         * Toggle the left hand navigation
+         * Toggle the left hand navigation with animation
          */
         $(document).on('click', '.oae-lhnavigation-toggle', function(ev) {
-            $('.oae-lhnavigation > ul').toggleClass('hidden-xs hidden-sm');
-            $('.oae-page').toggleClass('oae-page-expanded');
+            // If the left hand navigation is open close it
+            if ($('.oae-page').hasClass('oae-page-expanded')) {
+                // Animate the width and opacity to 0
+                $('.oae-lhnavigation').animate({
+                    'opacity': 0,
+                    'width': 0
+                }, 250);
+                // Animate the padding of the page to 25 pixels
+                $('.oae-page').animate({
+                    'padding-left': '25px'
+                }, 250, function() {
+                    // Add the bootstrap and OAE helper classes
+                    $('.oae-lhnavigation > ul').addClass('hidden-xs hidden-sm');
+                    $('.oae-page').removeClass('oae-page-expanded');
+                });
+            // If the left hand navigation is closed open it
+            } else {
+                // First set the opacity and width to 0 before animating it
+                $('.oae-lhnavigation').css({
+                    'opacity': 0,
+                    'width': 0
+                });
+                // Remove the bootstrap responsive hidden classes
+                $('.oae-lhnavigation > ul').removeClass('hidden-xs hidden-sm');
+                // Animate the opacity and width
+                $('.oae-lhnavigation').animate({
+                    'opacity': 1,
+                    'width': '200px'
+                }, 250);
+                // Animate the padding of the page to 200px (width of the left hand nav)
+                $('.oae-page').animate({
+                    'padding-left': '200px'
+                }, 250, function() {
+                    $('.oae-page').addClass('oae-page-expanded');
+                });
+            }
         });
 
     })(jQuery);
