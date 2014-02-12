1b0492d3675d5e2069fda4d72f35042a7f0450c5
Nicolaas Matthijs <nicolaas.matthijs@caret.cam.ac.uk>
2014-02-12 13:29:29 +0000

    Documentation follow-up for Travis fix

--- tools/travis/checkHilaryStart.sh
+++ tools/travis/checkHilaryStart.sh
@@ -9,26 +9,28 @@ URL="http://admin.oae.com/api/me"
 STATUS=false
 ATTEMPT=0
 
-# Execute curl request to `/api/me` and verify the response and
-# verify that the server is up by checking if a 200 status code came back
+# Execute curl request to `/api/me` and check for a 200 status code
+# to verify that Hilary has started up successfully
 checkHilaryStartup() {
-    # Request `/api/me`
-    STATUS="$(curl -sL -w '%{http_code}' $URL -o /dev/null)"
+
     # Increment the attempt counter
     ATTEMPT="$(($ATTEMPT+1))"
+
+    # Request `/api/me`
+    STATUS="$(curl -sL -w '%{http_code}' $URL -o /dev/null)"
+
     # Check the status code
     if [ $STATUS -eq 200 ]
     then
         echo "Hilary started. Status code "$STATUS
     else
-        # If the status code differs from 200 60 times, Hilary hasn't successfully
-        # started in the 2 minute time frame and the script fails
+        # If Hilary hasn't started up after 2 minutes, we back off and fail
         if [ $ATTEMPT -eq 60 ]
         then
-            echo "Hilary failed to start after 2 minutes. Exiting."
+            echo "Hilary failed to start after 2 minutes. Exiting"
             exit 1
         else
-            echo "Hilary not started yet. Status code "$STATUS "Retrying in 2 seconds"
+            echo "Hilary not started yet. Status code "$STATUS". Retrying in 2 seconds"
             sleep 2
             checkHilaryStartup
         fi
