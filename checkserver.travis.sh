42c5391ec289ef5be64ae7d24aee88cda53ec6e8
Bert Pareyn <pareyn.bert@gmail.com>
2014-02-12 13:02:32 +0000

    Follow up on Hilary start up script for Travis

--- checkserver.travis.sh
+++ checkserver.travis.sh
@@ -1,30 +1,39 @@
 #!/bin/sh
-# Verify that Hilary has started up and is ready to
-# start responsding to requests. Once /api/me returns
-# a 200 status code Travis can continue.
-echo "Check Hilary startup status."
+# Verify that Hilary started up successfully and is ready
+# to start responding to requests. Once `/api/me` returns
+# a 200 status code, Travis can continue.
+echo "Check Hilary startup status"
 
 # Variables used to check the Hilary start up status
 URL="http://admin.oae.com/api/me"
-ME=false
+STATUS=false
+ATTEMPT=0
 
-# Execute curl request to /api/me and verify the response
-checkServerStatus() {
-    ME="$(curl -sL -w '%{http_code}' $URL -o /dev/null)"
-    verifyServerStatus
-}
-
-# Verify that the server is up by checking if a 200 status code came back
-verifyServerStatus() {
-    if [ $ME -eq 200 ]
+# Execute curl request to `/api/me` and verify the response and
+# verify that the server is up by checking if a 200 status code came back
+checkHilaryStartup() {
+    # Request `/api/me`
+    STATUS="$(curl -sL -w '%{http_code}' $URL -o /dev/null)"
+    # Increment the attempt counter
+    ATTEMPT="$(($ATTEMPT+1))"
+    # Check the status code
+    if [ $STATUS -eq 200 ]
     then
-        echo "Hilary started. Status code "$ME.
+        echo "Hilary started. Status code "$STATUS
     else
-        echo "Hilary not started yet. Status code "$ME "Retrying in 2 seconds."
-        sleep 2
-        checkServerStatus
+        # If the status code differs from 200 60 times, Hilary hasn't successfully
+        # started in the 2 minute time frame and the script fails
+        if [ $ATTEMPT -eq 60 ]
+        then
+            echo "Hilary failed to start after 2 minutes. Exiting."
+            exit 1
+        else
+            echo "Hilary not started yet. Status code "$STATUS "Retrying in 2 seconds"
+            sleep 2
+            checkHilaryStartup
+        fi
     fi
 }
 
 # Initialize the first status check
-checkServerStatus
+checkHilaryStartup
