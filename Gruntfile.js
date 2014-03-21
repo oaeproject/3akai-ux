2ba5f673c6df0a536eeceb9ec4e9524d0009d466
Stephen Thomas <stephen@sathomas.me>
2014-03-21 10:07:48 -0400

    (Issue #3556) Add reference to r.js pull request that supports saner CSS optimization options

--- Gruntfile.js
+++ Gruntfile.js
@@ -93,6 +93,7 @@ module.exports = function(grunt) {
                     'optimize': 'uglify',
                     'preserveLicenseComments': false,
                     'optimizeCss': 'standard',
+                    // TODO: Replace the following with a saner value if/when `r.js` merges PR https://github.com/jrburke/r.js/pull/653
                     'cssImportIgnore': '//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,300,600,700&subset=latin,cyrillic-ext,latin-ext,greek-ext',
                     'inlineText': true,
                     'useStrict': false,
