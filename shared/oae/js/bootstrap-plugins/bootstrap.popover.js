dafa6d522f2040a634b54acde24858ab91b2ec34
Bert Pareyn <pareyn.bert@gmail.com>
2014-03-25 15:08:21 +0000

    WIP on bringing native bootstrap popovers back

--- shared/oae/js/bootstrap-plugins/bootstrap.popover.js
+++ shared/oae/js/bootstrap-plugins/bootstrap.popover.js
@@ -15,37 +15,62 @@
 
 define(['jquery'], function($) {
 
+    var $currentPopover = null;
+    var $prevPopover = null;
+
     /**
-     * Close popovers and remove them from the DOM
+     * Hide the open popover and reset the `$currentPopover` to null as none will be open
      */
-    var closePopovers = function() {
-        var $popover = $('.popover');
-        $($popover.prev()).popover('destroy');
-        $popover.detach();
+    var hidePopover = function($popover) {
+        $popover.popover('hide');
+        $popover = null;
     };
 
     /**
-     * Close any popovers that are open when an element other than the popover or one of
-     * its children is clicked
+     * Assign the current open popover to the `$currentPopover` variable to be able to
+     * reference it later.
+     *
+     * @param  {Event}    ev    Standard Bootstrap `shown` event
+     */
+    $(document).on('shown.bs.popover', function(ev) {
+        $prevPopover = $currentPopover;
+        $currentPopover = $(ev.target);
+        console.log($prevPopover, $currentPopover);
+    });
+
+    /**
+     * Reset the `$currentPopover` when a popover is closed
+     */
+    $(document).on('hidden.bs.popover', function() {
+        $currentPopover = null;
+    });
+
+    /**
+     * Hide the popover when the user clicks anywhere else in the document
      *
      * @param  {Event}    ev    Standard jQuery click event
      */
-    $('body').on('click', function(ev) {
-        if ($('.popover').length && !$(ev.target).parents('.popover').length) {
-            closePopovers();
-            ev.stopPropagation();
+    $(document).on('click', function(ev) {
+        //console.log($currentPopover, !$.contains($currentPopover[0], ev.target), ev.target !== $currentPopover[0], !$.contains($('.popover')[0], ev.target));
+        // If the popover was clicked or the trigger ignore the click
+        if ($currentPopover && !$.contains($currentPopover[0], ev.target) && ev.target !== $currentPopover[0] && !$.contains($('.popover')[0], ev.target)) {
+            console.log('hide current popover');
+            hidePopover($currentPopover);
+        } else if ($prevPopover && $prevPopover[0] !== $currentPopover) {
+            console.log('hide prev popover');
+            hidePopover($prevPopover);
         }
     });
 
     /**
-     * Catch the keypress event for `escape` and close any popovers that are open
+     * Catch the keypress event for `escape` and hide the open popover
      *
      * @param  {Event}    ev    Standard jQuery click event
      */
     $(document).on('keyup', function(ev) {
         var keyCode = parseInt(ev.which, 10);
-        if (keyCode === 27 && $('.popover').length) {
-            closePopovers();
+        if (keyCode === 27 && $currentPopover) {
+            hidePopover();
         }
     });
 });
