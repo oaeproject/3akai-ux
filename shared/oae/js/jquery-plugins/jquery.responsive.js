5ff332d42d1a16bac4cd360e05f6fba199ce92be
Branden Visser <mrvisser@gmail.com>
2014-03-27 10:44:18 -0400

    (Issue 3403) Switching lhnav transition to use translate

--- shared/oae/js/jquery-plugins/jquery.responsive.js
+++ shared/oae/js/jquery-plugins/jquery.responsive.js
@@ -15,17 +15,13 @@
 
 /**
  * Utility plugin that handles the responsive left hand navigation interactions.
- *     - Ability to open and close the navigation using the `.oae-lhnavigation-toggle` class on desktop and mobile.
- *     - When a `page link` is clicked on a mobile device the navigation will close (does not apply on desktop). The navigation stays visible when an `action button` is clicked as this doesn't show a new page.
- *     - Toggling the visiblity of the navigation uses css transitions to transition '.oae-lhnavigation' and '.oae-page' to their expanded state on desktop and mobile.
- *     - '.oae-lhnav-collapsing' is set to enable css transitions on the elements. It is removed once the transitions have ended.
- *     - '.oae-lhnav-expanded' is responsible for setting the required properties needed by '.oae-lhnavigation' and '.oae-page' to be shown in their expanded state.
+ *  - Ability to open and close the navigation using the `.oae-lhnavigation-toggle` class on desktop and mobile
+ *  - When a `page link` is clicked on a mobile device the navigation will close (does not apply on desktop). The navigation stays visible when an `action button` is clicked as this doesn't show a new page
+ *  - Toggling the visiblity of the navigation uses css transitions to transition `.oae-page` to its expanded state on desktop and mobile
  */
 
 define(['jquery', 'underscore', 'oae.api.util'], function (jQuery, _, oaeUtil) {
     (function($) {
-        // Time (in ms) it takes for the left hand nav opening/closing animation to finish.
-        var LHNAVIGATION_ANIMATION_TIME = 250;
 
         /**
          * Determine whether or not the left-hand nav is expanded
@@ -37,49 +33,34 @@ define(['jquery', 'underscore', 'oae.api.util'], function (jQuery, _, oaeUtil) {
         };
 
         /**
-         * Toggle the left hand navigation. Throttle the function to prevent it from being triggered during animation.
-         *
-         * @param  {Boolean}   showNav   True when the left hand navigation should be shown, false when the left hand navigation should be hidden.
+         * Toggle the left hand navigation between its expanded and collapsed state
          */
-        var toggleLhNav = _.throttle(function(showNav) {
-            var $lhNav = $('.oae-lhnavigation').addClass('oae-lhnav-collapsing').toggleClass('oae-lhnav-expanded', showNav);
-            if (showNav) {
-                // Remove the bootstrap responsive hidden classes to show the left hand
-                // navigation when animating on smaller screens
-                $lhNav.children('ul').removeClass('hidden-xs hidden-sm');
+        var toggleLhNav = function() {
+            $('.oae-lhnavigation').toggleClass('oae-lhnav-expanded');
+        };
+
+        /**
+         * Close the left hand navigation when the user selects items that are marked with
+         * the attribute `data-close-nav`
+         */
+        $(document).on('click', '.oae-lhnavigation > ul li[data-close-nav]', function() {
+            if (isLhNavExpanded()) {
+                toggleLhNav();
             }
-
-            setTimeout(function() {
-                $lhNav.removeClass('oae-lhnav-collapsing');
-                if (!showNav) {
-                    // Add the bootstrap and OAE helper classes once the closing animation is finished
-                    $lhNav.children('ul').addClass('hidden-xs hidden-sm');
-                }
-            }, LHNAVIGATION_ANIMATION_TIME);
-        }, LHNAVIGATION_ANIMATION_TIME + 20);
-
-        /**
-         * Close the left hand navigation when the user selects items that should be closed
-         * when clicked.
-         */
-        $(document).on('click', '.oae-lhnavigation > ul li[data-close-nav]', function() {toggleLhNav(false);});
-
-        /**
-         * Toggle the left hand navigation with animation. The left hand navigation can only
-         * be toggled in small and extra small viewports.
-         */
-        $(document).on('click', '.oae-lhnavigation-toggle', function(ev) {
-            // Open the left hand navigation if it's closed, otherwise close it.
-            toggleLhNav(!$('.oae-lhnavigation').hasClass('oae-lhnav-expanded'));
         });
 
         /**
+         * Clicking the toggle will expand / collapse the lhnavigation
+         */
+        $(document).on('click', '.oae-lhnavigation-toggle', toggleLhNav);
+
+        /**
          * Close the left hand navigation when anything within the main page, except for the
-         * `oae-lhnavigation-toggle`, receives focus whilst the left hand navigation is still open.
+         * `oae-lhnavigation-toggle`, receives focus whilst the left hand navigation is still open
          */
         $(document).on('focusin', '.oae-page', function(ev) {
             if (isLhNavExpanded() && !$(ev.target).hasClass('oae-lhnavigation-toggle')) {
-                toggleLhNav(false);
+                toggleLhNav();
             }
         });
 
