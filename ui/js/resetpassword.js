d812acd67af471df8358d55d5bdf812e9f76b0ff
taybson <taybson@gmail.com>
2015-02-19 16:14:42 -0500

    get reseting password fixed

--- ui/js/resetpassword.js
+++ ui/js/resetpassword.js
@@ -17,74 +17,46 @@ require(['jquery','oae.core'], function($, oae) {
 
     // Set the page title
     oae.api.util.setBrowserTitle('Reset Password');
-	
-   
-	
-    /**
-     * Get Secret: A start point for reseting passord
-     */
-	var getSecret = function(){
-		$(document).on('submit','#resetpassword-input-username', function(event){
-			
-			// disable redirect to other page
-			event.preventDefault();
-			
-			var username = $.trim($('#resetpassword-username', $(this)).val());
-			
-	        $.ajax({
-	            'url': '/api/auth/local/reset/init/'+username,
-	            'type': 'GET',
-	            'data': null,
-	            'success': function(data) {
-	                //callback(null, data);
-					oae.api.util.notification('Congratulations!','A Email has been sent to your Email address.');
-	            },
-	            'error': function(data) {
-	                oae.api.util.notification('Sorry!','The Email has not been sent out!');
-	            }
-	        });
-		});
-	};
-	
+
     /**
      * ResetPassword: Last Step for reseting passord
      */
-	var resetPassword = function(username,secret){
-		$(document).on('submit','#resetpassword-input-password', function(event){
-			
-			// disable redirect to other page
-			event.preventDefault();
-			
-			var password = $.trim($('#resetpassword-new-password', $(this)).val());
-			
-	        $.ajax({
-	            'url': '/api/auth/local/reset/change/' + username + '/' + secret,
-	            'type': 'POST',
-	            'data': {'newPassword':password},
-	            'success': function(data) {
-	                //callback(null, data);
-					oae.api.util.notification('Congratulations!','Your password has been succesfully changed!');
-					
-					// wait for 3 seconds and redirect the user to mainpage
-					setTimeout(
-					  function() 
-					  	{
-					    	//redirect the user to setting page
-							window.location.href = "/me";
-							
-					  	}, 3000);
-	            	},
-	            'error': function(data) {
-	                oae.api.util.notification('Sorry!','The password has not been changed!');
-	            }
-	        });
-		});
-	};
+	  var resetPassword = function(username,secret){
+  		$(document).on('submit','#resetpassword-input-password', function(event){
+  			
+  		// disable redirect to other page
+  		event.preventDefault();
+  			
+  		var password = $.trim($('#resetpassword-new-password', $(this)).val());
+  		
+          $.ajax({
+              'url': '/api/auth/local/reset/change/' + username,
+              'type': 'POST',
+              'data': {'newPassword':password, 'secret':secret},
+              'success': function(data) {
+                  //callback(null, data);
+  				        oae.api.util.notification('Congratulations!','Your password has been succesfully changed!');
+  				
+          				// wait for 3 seconds and redirect the user to mainpage
+          				setTimeout(
+              				  function() 
+              				  	{
+              				    	//redirect the user to setting page
+              						  window.location.href = "/me";
+              						
+              				  	}, 3000);
+                	     },
+              'error': function(data) {
+                  oae.api.util.notification('Sorry!','The password has not been changed!');
+              }
+          });
+  		});
+    };
 	
     /**
      * Select a page to display: A start point for reseting passord
      */
-	var displayHTML = function(){
+	  var displayHTML = function(){
 		
 		// get the path of url
 		var urlPath = location.pathname;
@@ -94,21 +66,12 @@ require(['jquery','oae.core'], function($, oae) {
 		
 		// get the length of urlArray
 		var urlLength = urlArray.length;
-		
-		// short url means there is not token
-		if(urlLength < 3){
-			$('#resetpassword-input-password').remove();
-			getSecret();
-		
-		// long url means there might be token, but the token might be invalid. We leave the security concern to later steps. 
-		}else{
-			$('#resetpassword-input-username').remove();
-			var username = urlArray[2];
-			var secret = urlArray[3];
-			resetPassword(username,secret); 
-		}
+    
+		var username = urlArray[2];
+		var secret = urlArray[3];
+    
+		resetPassword(username,secret); 
 	};
 	
 	displayHTML();
-    
 });
