6168a19a47aae102f5db0bd46454756b9139400c
Simon Gaeremynck <gaeremyncks@gmail.com>
2015-04-13 13:14:50 +0200

    Email verification: Sign in when anonymous logic

--- ui/js/emailverification.js
+++ ui/js/emailverification.js
@@ -20,18 +20,49 @@ require(['jquery','oae.core'], function($, oae) {
         return oae.api.util.redirect().me();
     }
 
-    // Verify the email address
     var token = $.url().param('token');
-    oae.api.user.verifyEmail(token, function(err) {
-        if (err) {
-            $('#emailverification-failure').show();
-            return;
+
+    // The user needs to be logged in before we can verify their token
+    if (oae.data.me.anon) {
+        // When only a single external institutional authentication strategy (`cas`, `googleApps`, `shibboleth`)
+        // is enabled, the Sign In button should send the user directly to the sign in page. Alternatively, the sign
+        // in dropdown in the top navigation should be opened when clicking the Sign In button
+        var enabledStrategies = oae.api.authentication.getEnabledStrategies();
+        var singleInstitutionalAuth = null;
+        if (_.keys(enabledStrategies).length === 1 && _.contains(['cas', 'googleApps', 'shibboleth'], _.keys(enabledStrategies)[0])) {
+            singleInstitutionalAuth = _.values(enabledStrategies)[0];
         }
 
-        // Show a confirmation message
-        $('#emailverification-success').show();
+        // After signing in, the user should be redirected back to the current email confirmation page
+        var redirectUrl = '/email-verification?token=' + token;
 
-        // Give the user some time to read the message and redirect them to /me
-        setTimeout(oae.api.util.redirect().me, 5000);
-    });
+        oae.api.util.template().render($('#error-signin-template'), {
+            'singleInstitutionalAuth': singleInstitutionalAuth,
+            'redirectUrl': redirectUrl
+        }, $('#error-signin-container'));
+
+        // When local authentication, any external non-institutional authentication strategies (`facebook`, `google`, `twitter`)
+        // or more than 1 external institutional authentication strategies are enabled, the sign in dropdown in the top navigation
+        // widget should be opened when clicking the `Sign In` button
+        if (!singleInstitutionalAuth) {
+            $(document).on('click', '#error-signin-dropdown', function() {
+                $('#topnavigation-signin').click();
+                return false;
+            });
+        }
+    } else {
+        // Verify the email address
+        oae.api.user.verifyEmail(token, function(err) {
+            if (err) {
+                $('#emailverification-failure').show();
+                return;
+            }
+
+            // Show a confirmation message
+            $('#emailverification-success').show();
+
+            // Give the user some time to read the message and redirect them to /me
+            setTimeout(oae.api.util.redirect().me, 5000);
+        });
+    }
 });
