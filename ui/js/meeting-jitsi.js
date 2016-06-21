68c93cb6e13617a1f284c5408a4e9f61ca91c2be
Jimmy Kasprzak <jimmykasprzak@gmail.com>
2016-06-21 10:20:35 +0200

    Add the set up of the meeting-jitsi page.

--- ui/js/meeting-jitsi.js
+++ ui/js/meeting-jitsi.js
@@ -7,11 +7,67 @@ require(['jquery', 'oae.core'], function ($, oae) {
     // Variable used to cache the meeting's base URL
     var baseUrl = '/meeting-jitsi/' + $.url().segment(2) + '/' + $.url().segment(3);
 
-    var getMeetingProfile = function () {
-        oae.api.meetingJitsi.getMeeting(meetingId, function (err, meeting) {
+    /**
+     * Set up the context. 
+     */
+    var setUpContext = function () {
+        $(document).on('oae.context.get', function (e, widgetId) {
+            console.log('widgetId : ', widgetId);
+            console.log('meetingProfile : ', meetingProfile);
+            if (widgetId)
+                $(document).trigger('oae.context.send.' + widgetId, meetingProfile);
+            else
+                $(document).trigger('oae.context.send', meetingProfile);
+        });
+    };
 
-            console.log('err : ', err);
-            console.log('profile : ', meeting);
+    /**
+     * Set up the left hand navigation with the content space page structure.
+     * The content left hand navigation item will not be shown to the user and
+     * is only used to load the correct content preview widget
+     */
+    var setUpNavigation = function () {
+        var lhNavPages = [{
+            'id': 'meeting',
+            'title': meetingProfile.displayName,
+            'icon': 'fa-video-camera',
+            'closeNav': true,
+            'class': 'hide',
+            'layout': [{
+                'width': 'col-md-12',
+                'widgets': [
+                    {
+                        'id': 'meeting-jitsi',
+                        'name': 'meeting-jitsi',
+                        'settings': meetingProfile
+                    },
+                    {
+                        'name': 'comments'
+                    }
+                ]
+            }]
+        }];
+
+        $(window).trigger('oae.trigger.lhnavigation', [lhNavPages, [], baseUrl]);
+        $(window).on('oae.ready.lhnavigation', function () {
+            $(window).trigger('oae.trigger.lhnavigation', [lhNavPages, [], baseUrl]);
+        });
+    };
+
+    /**
+     * Render the meeting clip
+     */
+    var setUpClip = function () {
+        oae.api.util.template().render($('#meeting-jitsi-clip-template'), {
+            'meeting': meetingProfile,
+            'displayOptions': {
+                'addLink:': false
+            }
+        }, $('#meeting-jitsi-clip-container'));
+    };
+
+    var getMeetingProfile = function () {
+        oae.api.meetingJitsi.getMeeting(meetingId, function (err, profile) {
 
             if (err) {
                 if (err.code === 401) 
@@ -20,23 +76,18 @@ require(['jquery', 'oae.core'], function ($, oae) {
                     oae.api.util.redirect().notfound();
                 return;
             }
-
-            /**
+            
             // Cache the meeting profile data
             meetingProfile = profile;
-
             // Render the entity information
             setUpClip();
-
             // Set up the page
             setUpNavigation();
-
             // Set up the context event exchange
             setUpContext();
-
             // We can now unhide the page
             oae.api.util.showPage();
-
+            /**
             // Set up the meeting push notifications
             setUpPushNotifications();
              */
