c441f91f13e36b4dc27dbc37be9bc86e0237878c
Claire Lozano <claire.lozano@live.fr>
2017-02-23 12:21:17 +0100

    config enable/disable jitsi

--- ui/js/index.js
+++ ui/js/index.js
@@ -103,6 +103,17 @@ require(['jquery','oae.core'], function($, oae) {
     };
 
     /**
+     * Meeting
+     */
+    var setUpMeeting = function() {
+        activateMeeting = oae.api.config.getValue('oae-jitsi', 'activate', 'meeting');
+        if(activateMeeting === "yes"){
+            oae.api.util.template().render($('#activate-meeting-template'), {
+            }, $('#activate-meeting-container'));            
+        }
+    };
+
+    /**
      * Set up the left hand navigation with the me space page structure
      */
     var setUpNavigation = function() {
@@ -147,12 +158,6 @@ require(['jquery','oae.core'], function($, oae) {
                     'title': oae.api.i18n.translate('__MSG__DISCUSSION__'),
                     'closeNav': true,
                     'class': 'oae-trigger-creatediscussion'
-                },
-                {
-                    'icon': 'fa-video-camera',
-                    'title': oae.api.i18n.translate('__MSG__MEETING__'),
-                    'closeNav': true,
-                    'class': 'oae-trigger-createmeeting-jitsi'
                 }
             ]
         }];
@@ -258,28 +263,40 @@ require(['jquery','oae.core'], function($, oae) {
                         ]
                     }
                 ]
-            },
-            {
-                  'id': 'meetings-jitsi',
-                  'title': oae.api.i18n.translate('__MSG__MY_MEETINGS__'),
-                  'icon': 'fa-video-camera',
-                  'closeNav': true,
-                  'layout': [
-                      {
-                          'width': 'col-md-12',
-                          'widgets': [
-                              {
-                                  'name': 'meetings-jitsi-library',
-                                  'settings': {
-                                      'context': oae.data.me,
-                                      'canManage': true
-                                  }
-                              }
-                          ]
-                      }
-                  ]
             }
         ];
+
+        // If Jitsi config value equals to "yes", then display meetings on navigation and on the left hand navigation pages
+        activateMeeting = oae.api.config.getValue('oae-jitsi', 'activate', 'meeting');
+        if(activateMeeting === "yes"){
+            lhNavActions[1].children.push({
+                'icon': 'fa-video-camera',
+                'title': oae.api.i18n.translate('__MSG__MEETING__'),
+                'closeNav': true,
+                'class': 'oae-trigger-createmeeting-jitsi'
+            });
+
+            lhNavPages.push({
+                'id': 'meetings-jitsi',
+                'title': oae.api.i18n.translate('__MSG__MY_MEETINGS__'),
+                'icon': 'fa-video-camera',
+                'closeNav': true,
+                'layout': [
+                    {
+                        'width': 'col-md-12',
+                        'widgets': [
+                            {
+                                'name': 'meetings-jitsi-library',
+                                'settings': {
+                                    'context': oae.data.me,
+                                    'canManage': true
+                                }
+                            }
+                        ]
+                    }
+                ]
+            });
+        }
 
         $(window).trigger('oae.trigger.lhnavigation', [lhNavPages, lhNavActions, '/']);
         $(window).on('oae.ready.lhnavigation', function() {
@@ -325,6 +342,7 @@ require(['jquery','oae.core'], function($, oae) {
 
         // Logged in users also have clips and left-hand navigation
         setUpClip();
+        setUpMeeting();
         setUpNavigation();
 
         // Show user preferences if, e.g., it's deep-linked via the URL
