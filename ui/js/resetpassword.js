788ffc8c9cd0c9b0b3820b4942caad892a191252
taybson <taybson@gmail.com>
2017-05-15 22:09:36 -0700

    makes changes to ui and translation. Some clean ups.

--- ui/js/resetpassword.js
+++ ui/js/resetpassword.js
@@ -19,59 +19,58 @@ require(['jquery','oae.core'], function($, oae) {
     oae.api.util.setBrowserTitle('Reset Password');
 
     /**
-     * ResetPassword: Last Step for reseting passord
+     * ResetPassword: Last Step for reseting password
      */
-	  var resetPassword = function(username,secret){
-  		$(document).on('submit','#resetpassword-input-password', function(event){
-  			
-  		// disable redirect to other page
-  		event.preventDefault();
-  			
-  		var password = $.trim($('#resetpassword-new-password', $(this)).val());
-  		
-          $.ajax({
-              'url': '/api/auth/local/reset/change/' + username,
-              'type': 'POST',
-              'data': {'newPassword':password, 'secret':secret},
-              'success': function(data) {
-                  //callback(null, data);
-  				        oae.api.util.notification('Congratulations!','Your password has been succesfully changed!');
-  				
-          				// wait for 3 seconds and redirect the user to mainpage
-          				setTimeout(
-              				  function() 
-              				  	{
-              				    	//redirect the user to setting page
-              						  window.location.href = "/me";
-              						
-              				  	}, 3000);
-                	     },
-              'error': function(data) {
-                  oae.api.util.notification('Sorry!','The password has not been changed!');
-              }
-          });
-  		});
+    var resetPassword = function(username,secret){
+  	$(document).on('submit','#resetpassword-input-password', function(event){
+  	    
+  	    // disable redirect to other page
+  	    event.preventDefault();
+  	    
+  	    var password = $.trim($('#resetpassword-new-password', $(this)).val());
+  	    
+            $.ajax({
+		'url': '/api/auth/local/reset/change/' + username,
+		'type': 'POST',
+		'data': {'newPassword':password, 'secret':secret},
+		'success': function(data) {
+                    //callback(null, data);
+  		    oae.api.util.notification(oae.api.i18n.translate('__MSG__CONGRATULATIONS__'), oae.api.i18n.translate('__MSG__YOUR_PASSWORD_HAS_BEEN_UPDATED__'));
+  		    
+          	    // wait for 3 seconds and redirect the user to mainpage
+          	    setTimeout(
+              		function() {
+              		    //redirect the user to setting page
+              		    window.location.href = "/";
+              		    
+              		}, 3000);
+                },
+		'error': function(data) {
+                    oae.api.util.notification(oae.api.i18n.translate('__MSG__SORRY__'), oae.api.i18n.translate('__MSG__COULDNT_UPDATE_PASSWORD__'));
+		}
+            });
+  	});
     };
 	
     /**
      * Select a page to display: A start point for reseting passord
      */
-	  var displayHTML = function(){
-		
-		// get the path of url
-		var urlPath = location.pathname;
-		
-		// get value of url in a array
-		var urlArray = urlPath.split("/");
-		
-		// get the length of urlArray
-		var urlLength = urlArray.length;
-    
-		var username = urlArray[2];
-		var secret = urlArray[3];
-    
-		resetPassword(username,secret); 
-	};
+    var displayHTML = function(){
 	
-	displayHTML();
+	// get the path of url
+	var urlPath = location.pathname;
+	
+	// get value of url in a array
+	var urlArray = urlPath.split("/");
+	
+	// get the length of urlArray
+	var urlLength = urlArray.length;
+	
+	var username = urlArray[2];
+	var secret = urlArray[3];
+	
+	resetPassword(username,secret); 
+    };
+    
+    displayHTML();
 });
