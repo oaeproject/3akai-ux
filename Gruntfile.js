f9b51bf6b8981eb725854480b128bd3e9be8054e
Salla Karppinen <sk@researchresearch.com>
2017-06-29 11:48:50 +0100

    Fixes issue #4168

--- Gruntfile.js
+++ Gruntfile.js
@@ -120,7 +120,7 @@ module.exports = function(grunt) {
                         'name': 'oae.core',
                         'exclude': ['jquery']
                     }],
-                    'fileExclusionRegExp': /^(\.|<%= target %>|tests|tools|grunt|optimist|properties-parser|readdirp|underscore$|shelljs$|oae-release-tools|robots\.txt)/,
+                    'fileExclusionRegExp': /^(\.|<%= target %>|tests|tools|grunt|optimist|properties-parser|readdirp|mkdirp|underscore$|shelljs$|oae-release-tools|es6-promise|cssstyle|resolve|nwmatcher|strip-json-comments|robots\.txt)/,
                     'logLevel': 2
                 }
             }
